56dea3c0c9a8099bbcd0ae1a5b502cc03fdc02dc
dpressel <dpressel@gmail.com>
2016-10-14 15:45:30 -0400

    Initial add of Keras version of fine-tuning classifier.

---
+++ classify/keras/cnn-sentence-fine.py
@@ -0,0 +1,124 @@
+from keras.models import Model, load_model
+from keras.layers import Dense, Activation, Convolution1D, Embedding, Input, merge, GlobalMaxPooling1D, Dropout
+from keras.utils.visualize_util import plot, model_to_dot
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+import numpy as np
+from keras.utils import np_utils
+import w2v
+import time
+import json
+import argparse
+from data import buildVocab
+from data import loadTemporalIndices
+from utils import revlut
+import os.path
+import os
+# Use the functional API since we support parallel convolutions
+def create(embeddings, nc, filtsz, cmotsz, hsz, maxlen, pdrop):
+    x = Input(shape=(maxlen,), dtype='int32', name='input')
+
+    vocab_size = embeddings.weights.shape[0]
+    embedding_dim = embeddings.dsz
+    
+    lut = Embedding(input_dim=vocab_size, output_dim=embedding_dim, weights=[embeddings.weights], input_length=maxlen)
+    
+    embed = lut(x)
+
+    mots = []
+    for i, fsz in enumerate(filtsz):
+        conv = Convolution1D(cmotsz, fsz, activation='relu', input_length=maxlen)(embed)
+        gmp = GlobalMaxPooling1D()(conv)
+        mots.append(gmp)
+
+    joined = merge(mots, mode='concat')
+    cmotsz_all = cmotsz * len(filtsz)
+    drop1 = Dropout(pdrop)(joined)
+
+    input_dim = cmotsz_all
+    last_layer = drop1
+
+    if hsz > 0:
+        proj = Dense(hsz, input_dim=cmotsz_all, activation='relu')(drop1)
+        drop2 = Dropout(pdrop)(proj)
+        input_dim = hsz
+        last_layer = drop2
+
+    dense = Dense(output_dim=nc, input_dim=input_dim, activation='softmax')(last_layer)
+    model = Model(input=[x], output=[dense])
+#    plot(model, 'model.png')
+    return model
+
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('--eta', help='Initial learning rate', default=0.001, type=float)
+parser.add_argument('--embed', help='Word2Vec embeddings file', required=True)
+parser.add_argument('--train', help='Training file', required=True)
+parser.add_argument('--valid', help='Validation file')
+parser.add_argument('--test', help='Test file', required=True)
+
+
+parser.add_argument('--optim', help='Optim method', default='adam', choices=['adam', 'adagrad', 'adadelta', 'sgd'])
+parser.add_argument('--dropout', help='Dropout probability', default=0.5, type=float)
+parser.add_argument('--unif', help='Initializer bounds for embeddings', default=0.25)
+parser.add_argument('--epochs', help='Number of epochs', default=25, type=int)
+parser.add_argument('--batchsz', help='Batch size', default=50, type=int)
+parser.add_argument('--mxlen', help='Max length', default=100, type=int)
+parser.add_argument('--patience', help='Patience', default=10, type=int)
+parser.add_argument('--cmotsz', help='Hidden layer size', default=100, type=int)
+parser.add_argument('--hsz', help='Projection layer size', default=-1, type=int)
+parser.add_argument('--filtsz', help='Filter sizes', nargs='+', default=[3,4,5], type=int)
+parser.add_argument('--clean', help='Do cleaning', action='store_true')
+parser.add_argument('--chars', help='Use characters instead of words', action='store_true')
+parser.add_argument('--valsplit', help='Validation split if no valid set', default=0.15, type=float)
+parser.add_argument('--outdir', help='Output directory', default='./train')
+
+args = parser.parse_args()
+
+if os.path.exists(args.outdir) is False:
+    print('Creating path: %s' % (args.outdir))
+    os.makedirs(args.outdir)
+vocab = buildVocab([args.train, args.test, args.valid], args.clean, args.chars)
+
+embeddings = w2v.Word2VecModel(args.embed, vocab, args.unif)
+
+
+mxfiltsz = np.max(args.filtsz)
+f2i = {}
+X_train, y_train, f2i = loadTemporalIndices(args.train, embeddings.vocab, f2i, args.clean, args.chars, args.mxlen, mxfiltsz)
+print('Loaded  training data')
+
+valsplit = args.valsplit
+valdata = None
+if args.valid is not None:
+    print('Using provided validation data')
+    valsplit = 0
+    X_valid, y_valid, f2i = loadTemporalIndices(args.valid, embeddings.vocab, f2i, args.clean, args.chars, args.mxlen, mxfiltsz)
+    valdata = [X_valid, y_valid]
+
+X_test, y_test, f2i = loadTemporalIndices(args.test, embeddings.vocab, f2i, args.clean, args.chars, args.mxlen, mxfiltsz)
+print('Loaded test data')
+
+nc = len(f2i)
+model = create(embeddings, nc, args.filtsz, args.cmotsz, args.hsz, args.mxlen, args.dropout)
+
+model.compile(args.optim, 'categorical_crossentropy' , metrics=['accuracy'])
+
+y_train = np_utils.to_categorical(y_train, nc)
+y_test = np_utils.to_categorical(y_test, nc)
+if args.valid is not None:
+    valdata[1] = np_utils.to_categorical(valdata[1], nc)
+
+early_stopping = EarlyStopping(monitor='val_loss', patience=args.patience, verbose=1, mode='auto')
+
+checkpoint = ModelCheckpoint('%s/cnn-sentence-fine.model' % args.outdir, monitor='val_loss', verbose=1, save_best_only=True, save_weights_only=False, mode='auto')
+model.fit(X_train, y_train, args.batchsz, args.epochs, verbose=1, callbacks=[checkpoint, early_stopping], validation_split=valsplit, validation_data=valdata, shuffle=True)
+
+print('=====================================================')
+print('Evaluating best model on test data:')
+print('=====================================================')
+model = load_model('%s/cnn-sentence-fine.model' % args.outdir)
+start_time = time.time()
+
+score = model.evaluate(X_test, y_test, args.batchsz, verbose=1)
+duration = time.time() - start_time
+
+print('Test (Loss %.4f) (Acc = %.4f) (%.3f sec)' % (score[0], score[1], duration))
---
+++ classify/keras/data.py
@@ -0,0 +1,122 @@
+import w2v
+import numpy as np
+from collections import Counter
+import re
+import math
+import codecs
+
+REPLACE = { "'s": " 's ",
+            "'ve": " 've ",
+            "n't": " n't ",
+            "'re": " 're ",
+            "'d": " 'd ",
+            "'ll": " 'll ",
+            ",": " , ",
+            "!": " ! ",
+        }
+          
+  
+def splits(text):
+    return filter(lambda s: len(s) != 0, re.split('\s+', text))
+
+def doClean(l):
+    l = re.sub(r"[^A-Za-z0-9(),!?\'\`]", " ", l)
+    for k,v in REPLACE.items():
+        l = l.replace(k, v)
+    return l.strip()
+
+def labelSent(line, clean, chars):
+    labelText = re.split('[\t\s]+', line)
+    label = labelText[0]
+    text = labelText[1:]
+    if chars is True:
+        text = ' '.join([ch for ch in ''.join(text)])
+    if clean is True:
+        text = ' '.join([doClean(w.lower()) for w in text]).replace('  ', ' ')
+    else:
+        text = ' '.join(text).replace('  ', ' ')
+    return label, text
+
+def numLines(filename):
+    lines = 0
+    with codecs.open(filename, encoding='utf-8', mode='r') as f:
+        for line in f:
+            lines = lines + 1
+    return lines
+
+def buildVocab(files, clean=False, chars=False):
+    vocab = Counter()
+    for file in files:
+        if file is None:
+            continue
+        with codecs.open(file, encoding='utf-8', mode='r') as f:
+            for line in f:
+                _, text = labelSent(line, clean, chars)
+                for w in splits(text):
+                    vocab[w] += 1
+    return vocab
+
+def loadTemporalIndices(filename, index, f2i, clean=False, chars=False, mxlen=1000, mxfiltsz=5):
+    ts = []
+    PAD = index['<PADDING>']
+    mxsiglen = mxlen - mxfiltsz
+    labelIdx = len(f2i)
+    
+    halffiltsz = int(math.floor(mxfiltsz / 2))
+    labelIdx = len(f2i)
+
+    n = numLines(filename)
+    x = np.empty((n, mxlen), dtype=int)
+    x.fill(PAD)
+    y = np.zeros(n, dtype=int)
+ 
+    with codecs.open(filename, encoding='utf-8', mode='r') as f:
+        for offset, line in enumerate(f):
+            label, text = labelSent(line, clean, chars)
+            if not label in f2i:
+                f2i[label] = labelIdx
+                labelIdx += 1
+
+            y[offset] = f2i[label]
+            toks = splits(text)
+            mx = min(len(toks), mxsiglen)
+            toks = toks[:mx]
+            for j in range(len(toks)):
+                w = toks[j]
+                key = index.get(w, PAD)
+                x[offset][j+halffiltsz] = key
+    return x, y, f2i    
+
+def loadTemporalEmb(filename, w2v, f2i, clean=False, chars=False, mxlen=1000, mxfiltsz=5):
+    ts = []
+    vsz = w2v.vsz
+    dsz = w2v.dsz
+        
+    mxsiglen = mxlen - mxfiltsz
+    halffiltsz = int(math.floor(mxfiltsz / 2))
+    labelIdx = len(f2i)
+
+    n = numLines(filename)
+
+    n = numLines(filename)
+    x = np.zeros((n, mxlen, dsz), dtype=np.float32)
+    y = np.zeros(n, dtype=int)
+
+    with codecs.open(filename, encoding='utf-8', mode='r') as f:
+
+        for offset, line in enumerate(f):
+            label, text = labelSent(line, clean, chars)
+            if not label in f2i:
+                f2i[label] = labelIdx
+                labelIdx += 1
+
+            y[offset] = f2i[label]
+            toks = splits(text)
+            mx = min(len(toks), mxsiglen)
+            toks = toks[:mx]
+            for j in range(len(toks)):
+                w = toks[j]
+                z = w2v.lookup(w, False)
+                x[offset][j + halffiltsz] = z
+            i = i + 1
+    return x, y, f2i
