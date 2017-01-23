Fixded vsz+1 bug found in upgrade, added gradient clipping

seq2seq/tf/model.py
@@ -12,10 +12,10 @@ class Seq2SeqBase:
     def makeCell(self, hsz, nlayers, rnntype):
         st = False if nlayers > 1 else True
         
-        cell = tf.nn.rnn_cell.BasicLSTMCell(hsz, state_is_tuple=st) if rnntype == 'lstm' else tf.nn.rnn_cell.GRUCell(hsz)
+        cell = tf.contrib.rnn.BasicLSTMCell(hsz, state_is_tuple=st) if rnntype == 'lstm' else tf.contrib.rnn.GRUCell(hsz)
         
         if nlayers > 1:
-            cell = tf.nn.rnn_cell.MultiRNNCell([cell] * nlayers, state_is_tuple=st)
+            cell = tf.contrib.rnn.MultiRNNCell([cell] * nlayers, state_is_tuple=st)
         return cell
 
     def save(self, sess, outdir, base):
@@ -77,13 +77,13 @@ class Seq2SeqBase:
 
     def createLoss(self):
 
-        tsparse = tf.unpack(tf.transpose(self.tgt, perm=[1, 0]))
+        tsparse = tf.unstack(tf.transpose(self.tgt, perm=[1, 0]))
 
         with tf.name_scope("Loss"):
 
             log_perp_list = []
             error_list = []
-            totalSz = 0
+            total_list = []
             # For each t in T
             for preds_i, target_i in zip(self.preds, tsparse):
 
@@ -93,21 +93,24 @@ class Seq2SeqBase:
                 best_i = tf.cast(tf.argmax(preds_i, 1), tf.int32)
                 err = tf.cast(tf.not_equal(best_i, target_i), tf.float32)
                 # Gives back (B, V)
-                xe = tf.nn.sparse_softmax_cross_entropy_with_logits(preds_i, target_i)
+                xe = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=preds_i, labels=target_i)
 
                 log_perp_list.append(xe * mask)
                 error_list.append(err * mask)
-                totalSz += tf.reduce_sum(mask)
+                total_list.append(tf.reduce_sum(mask))
                 
             log_perps = tf.add_n(log_perp_list)
             error_all = tf.add_n(error_list)
-            log_perps /= totalSz
+            totalsz = tf.add_n(total_list)
+            log_perps /= totalsz
 
             cost = tf.reduce_sum(log_perps)
             all_error = tf.reduce_sum(error_all)
 
-            batchSz = tf.cast(tf.shape(tsparse[0])[0], tf.float32)
-            return cost/batchSz, all_error, totalSz
+            batchsz = tf.cast(tf.shape(tsparse[0])[0], tf.float32)
+            avg_cost = cost/batchsz
+            #checks = tf.add_check_numerics_ops()
+            return avg_cost, all_error, totalsz #, checks
 
     def step(self, sess, src, dst):
         """
@@ -155,16 +158,16 @@ class Seq2SeqModel(Seq2SeqBase):
             rnn_dec = self.makeCell(hsz, nlayers, rnntype)
 
             # Primitive will wrap RNN and unroll in time
-            rnn_enc_seq, final_encoder_state = tf.nn.rnn(rnn_enc, embed_in_seq, scope='rnn_enc', dtype=tf.float32)
+            rnn_enc_seq, final_encoder_state = tf.contrib.rnn.static_rnn(rnn_enc, embed_in_seq, scope='rnn_enc', dtype=tf.float32)
             # Provides the link between the encoder final state and the decoder
-            rnn_dec_seq, _ = tf.nn.rnn(rnn_dec, embed_out_seq, initial_state=final_encoder_state, scope='rnn_dec', dtype=tf.float32)
+            rnn_dec_seq, _ = tf.contrib.rnn.static_rnn(rnn_dec, embed_out_seq, initial_state=final_encoder_state, scope='rnn_dec', dtype=tf.float32)
 
         with tf.name_scope("output"):
             # Leave as a sequence of (T, B, W)
 
-            W = tf.Variable(tf.truncated_normal([hsz, embed2.vsz],
+            W = tf.Variable(tf.truncated_normal([hsz, embed2.vsz + 1],
                                                 stddev = 0.1), name="W")
-            b = tf.Variable(tf.constant(0.0, shape=[1, embed2.vsz]), name="b")
+            b = tf.Variable(tf.constant(0.0, shape=[1, embed2.vsz + 1]), name="b")
 
             self.preds = [(tf.matmul(rnn_dec_i, W) + b) for rnn_dec_i in rnn_dec_seq]
             self.probs = [tf.nn.softmax(pred, name="probs") for pred in self.preds]
@@ -209,7 +212,7 @@ class Seq2SeqLib(Seq2SeqBase):
                 print('With attention')
                 rnn_dec_seq, _ = attn_rnn_seq2seq(embed_in_seq, embed_out_seq, cell)
             else:
-                rnn_dec_seq, _ = tf.nn.seq2seq.basic_rnn_seq2seq(embed_in_seq, embed_out_seq, cell)
+                rnn_dec_seq, _ = tf.contrib.legacy_seq2seq.basic_rnn_seq2seq(embed_in_seq, embed_out_seq, cell)
             
         with tf.name_scope("output"):
             # Leave as a sequence of (T, B, W)

seq2seq/tf/seq2seq.py
@@ -118,16 +118,16 @@ if FLAGS.attn:
 # Use library-backed seq2seq routine
 seq2seq = Seq2SeqLib() if FLAGS.seq2seqlib else Seq2SeqModel()
 
+#with tf.device('/cpu:0'):
 with tf.Graph().as_default():
     sess = tf.Session()
     with sess.as_default():
         seq2seq.params(embed1, embed2, FLAGS.mxlen, FLAGS.hsz, FLAGS.layers, FLAGS.attn, FLAGS.rnntype)
 
         trainer = Trainer(seq2seq, FLAGS.optim, FLAGS.eta)
-        train_writer = tf.train.SummaryWriter(FLAGS.outdir + "/train", sess.graph)
-        init = tf.initialize_all_variables()
+        train_writer = tf.summary.FileWriter(FLAGS.outdir + "/train", sess.graph)
+        init = tf.global_variables_initializer()
         sess.run(init)
-
         trainer.prepare(tf.train.Saver())
 
         err_min = 1

seq2seq/tf/train.py
@@ -16,10 +16,12 @@ class Trainer:
         else:
             self.optimizer = tf.train.GradientDescentOptimizer(eta)
 
-        self.train_op = self.optimizer.minimize(self.loss, global_step=self.global_step)
+        gvs = self.optimizer.compute_gradients(self.loss)
+        capped_gvs = [(tf.clip_by_value(grad, -1., 1.), var) for grad, var in gvs]
+        self.train_op = self.optimizer.apply_gradients(capped_gvs, global_step=self.global_step)
 
-        self.loss_summary = tf.scalar_summary("loss", self.loss)
-        self.summary_op = tf.merge_all_summaries()
+        self.loss_summary = tf.summary.scalar("loss", self.loss)
+        self.summary_op = tf.summary.merge_all()
 
     def checkpoint(self, sess, outdir, name):
         self.model.saver.save(sess, outdir + "/train/" + name, global_step=self.global_step)
@@ -44,6 +46,7 @@ class Trainer:
         
             _, step, summary_str, lossv, errv, totv = sess.run([self.train_op, self.global_step, self.summary_op, self.loss, self.errs, self.tot], feed_dict=feed_dict)
             summary_writer.add_summary(summary_str, step)
+            #print(lossv, errv, totv)
             total_loss += lossv
             total_err += errv
             total += totv

seq2seq/tf/utils.py
@@ -9,22 +9,22 @@ def attn_rnn_seq2seq(encoder_inputs,
                      dtype=tf.float32,
                      scope=None):
     with tf.variable_scope(scope or "attention_rnn_seq2seq"):
-        encoder_outputs, enc_state = tf.nn.rnn(cell, encoder_inputs, dtype=dtype)
+        encoder_outputs, enc_state = tf.contrib.rnn.static_rnn(cell, encoder_inputs, dtype=dtype)
         top_states = [tf.reshape(e, [-1, 1, cell.output_size])
                       for e in encoder_outputs]
-        attention_states = tf.concat(1, top_states)
+        attention_states = tf.concat_v2(values=top_states, axis=1)
     
-    return tf.nn.seq2seq.attention_decoder(decoder_inputs,
-                                           enc_state,
-                                           attention_states,
-                                           cell,
-                                           num_heads=num_heads)
+    return tf.contrib.legacy_seq2seq.attention_decoder(decoder_inputs,
+                                                       enc_state,
+                                                       attention_states,
+                                                       cell,
+                                                       num_heads=num_heads)
 
 def tensorToSeq(tensor):
-    return tf.unpack(tf.transpose(tensor, perm=[1, 0, 2]))
+    return tf.unstack(tf.transpose(tensor, perm=[1, 0, 2]))
 
 def seqToTensor(sequence):
-    return tf.transpose(tf.pack(sequence), perm=[1, 0, 2])
+    return tf.transpose(tf.stack(sequence), perm=[1, 0, 2])
 
 def revlut(lut):
     return {v: k for k, v in lut.items()}