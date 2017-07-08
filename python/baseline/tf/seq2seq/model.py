94ed923bfcd2bbe0b62c3ce0a6fd2119f9e616a6
Dan Pressel <dpressel@gmail.com>
2017-07-08 08:30:26 -0400

    Fixed an issue, start to refactor towards enc-dec

--- python/baseline/tf/seq2seq/model.py
+++ python/baseline/tf/seq2seq/model.py
@@ -4,129 +4,107 @@ from google.protobuf import text_format
 from tensorflow.python.platform import gfile
 from baseline.tf.tfy import *
 from baseline.w2v import RandomInitVecModel
-
-class Seq2SeqBaseTf:
-
-    def save(self, model_base):
-        pass
-
-    def __init__(self):
-        pass
-
-    def step(self, src, src_len, dst, dst_len):
-        """
-        Generate probability distribution over output V for next token
-        """
-        feed_dict = {self.src: src, self.tgt: dst, self.pkeep: 1.0}
-        return self.sess.run(self.probs, feed_dict=feed_dict)
+import tensorflow.contrib.seq2seq as tfcontrib_seq2seq
+from baseline.model import EncoderDecoder
 
 
-    def make_cell(self, hsz, nlayers, rnntype):
-        
-        if nlayers > 1:
-            cell = tf.contrib.rnn.MultiRNNCell([new_rnn_cell(hsz, rnntype, True) for _ in range(nlayers)], state_is_tuple=True)
-            return cell
-        return new_rnn_cell(hsz, rnntype, False)
-
-    def create_loss(self):
-        
-        targets = tf.unstack(tf.transpose(self.tgt[:,1:], perm=[1, 0]))
-        predictions = tf.unstack(self.preds)
-        bests = tf.unstack(self.best)
-
-        with tf.name_scope("Loss"):
-
-            log_perp_list = []
-            total_list = []
-            # For each t in T
-            for preds_i, best_i, target_i in zip(predictions, bests, targets):
-                # Mask against (B)
-                mask = tf.cast(tf.sign(target_i), tf.float32)
-                # self.preds_i = (B, V)
-                #best_i = tf.cast(tf.argmax(preds_i, 1), tf.int32)
-                err = tf.cast(tf.not_equal(tf.cast(best_i, tf.int32), target_i), tf.float32)
-                # Gives back (B, V)
-                xe = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=preds_i, labels=target_i)
-
-                log_perp_list.append(xe * mask)
-                total_list.append(tf.reduce_sum(mask))
-                
-            log_perps = tf.add_n(log_perp_list)
-            totalsz = tf.add_n(total_list)
-            log_perps /= totalsz
-
-            cost = tf.reduce_sum(log_perps)
-
-            batchsz = tf.cast(tf.shape(targets[0])[0], tf.float32)
-            avg_cost = cost/batchsz
-            return avg_cost
-
-
-class Seq2SeqModelTf_v1_1(Seq2SeqBaseTf):
-
-    def create_loss(self):
-
-        targets = tf.transpose(self.tgt[:,1:], perm=[1, 0])
-        targets = targets[0:self.mx_tgt_len,:]
-        target_lens = self.tgt_len - 1
-        with tf.name_scope("Loss"):
-            losses = tf.nn.sparse_softmax_cross_entropy_with_logits(
-                logits=self.preds, labels=targets)
-
-            loss_mask = tf.sequence_mask(
-                tf.to_int32(target_lens), tf.to_int32(tf.shape(targets)[0]))
-            losses = losses * tf.transpose(tf.to_float(loss_mask), [1, 0])
+def _temporal_cross_entropy_loss(logits, labels, label_lengths, mx_seq_length):
+    """Do cross-entropy loss accounting for sequence lengths
     
-            losses = tf.reduce_sum(losses)
-            losses /= tf.cast(tf.reduce_sum(target_lens), tf.float32)
-            return losses
+    :param logits: a `Tensor` with shape `[batch, timesteps, vocab]`
+    :param labels: an integer `Tensor` with shape `[batch, timesteps]`
+    :param label_lengths: The actual length of the target text.  Assume right-padded
+    :param mx_seq_length: The maximum length of the sequence
+    :return: 
+    """
+
+    # The labels actual length is 100, and starts with <GO>
+    labels = tf.transpose(labels, perm=[1, 0])
+    # TxB loss mask
+    # Logits == mxlen=10*batchsz=100
+    # Labels == mxlen=9,batchsz=100
+    labels = labels[0:mx_seq_length, :]
+    timesteps = tf.to_int32(tf.shape(labels)[0])
+    # The labels no longer include <GO> so go is not useful.  This means that if the length was 100 before, the length
+    # of labels is now 99 (and that is the max allowed)
+
+    #logits = logits[0:mx_seq_length, :, :]
+    with tf.name_scope("Loss"):
+        losses = tf.nn.sparse_softmax_cross_entropy_with_logits(
+            logits=logits, labels=labels)
+
+        # BxT loss mask
+        loss_mask = tf.to_float(tf.sequence_mask(tf.to_int32(label_lengths), timesteps))
+        # TxB losses * TxB loss_mask
+        losses = losses * tf.transpose(loss_mask, [1, 0])
+
+        losses = tf.reduce_sum(losses)
+        losses /= tf.cast(tf.reduce_sum(label_lengths), tf.float32)
+        return losses
+
+
+class Seq2SeqModel(EncoderDecoder):
+
+    def create_loss(self):
+        # We do not want to count <GO> in our assessment, we do want to count <EOS>
+        return _temporal_cross_entropy_loss(self.preds[:-1, :, :], self.tgt[:, 1:], self.tgt_len - 1, self.mx_tgt_len - 1)
 
     def __init__(self):
+        super(Seq2SeqModel, self).__init__()
         pass
 
-    def params(self, sess, embed1, embed2, mxlen, hsz, nlayers=1, attn=False, rnntype='lstm', predict=False):
-        self.sess = sess
+    @staticmethod
+    def create(src_vocab, dst_vocab, **kwargs):
+
+        model = Seq2SeqModel()
+        hsz = int(kwargs['hsz'])
+        attn = bool(kwargs.get('attn', False))
+        nlayers = int(kwargs.get('layers', 1))
+        rnntype = kwargs.get('rnntype', 'lstm')
+        mxlen = kwargs.get('mxlen', 100)
+        predict = kwargs.get('predict', False)
+        model.sess = kwargs.get('sess', tf.Session())
+
         # These are going to be (B,T)
-        self.src = tf.placeholder(tf.int32, [None, mxlen], name="src")
-        self.tgt = tf.placeholder(tf.int32, [None, mxlen], name="tgt")
-        self.pkeep = tf.placeholder(tf.float32, name="pkeep")
+        model.src = tf.placeholder(tf.int32, [None, mxlen], name="src")
+        model.tgt = tf.placeholder(tf.int32, [None, mxlen], name="tgt")
+        model.pkeep = tf.placeholder(tf.float32, name="pkeep")
 
-        self.src_len = tf.placeholder(tf.int32, [None], name="src_len")
-        self.tgt_len = tf.placeholder(tf.int32, [None], name="tgt_len")
-        self.mx_tgt_len = tf.placeholder(tf.int32, name="mx_tgt_len")
+        model.src_len = tf.placeholder(tf.int32, [None], name="src_len")
+        model.tgt_len = tf.placeholder(tf.int32, [None], name="tgt_len")
+        model.mx_tgt_len = tf.placeholder(tf.int32, name="mx_tgt_len")
 
-        self.vocab1 = embed1.vocab
-        self.vocab2 = embed2.vocab
+        model.vocab1 = src_vocab.vocab
+        model.vocab2 = dst_vocab.vocab
 
-        self.mxlen = mxlen
-        self.hsz = hsz
-        self.nlayers = nlayers
-        self.rnntype = rnntype
-        self.attn = attn
+        model.mxlen = mxlen
+        model.hsz = hsz
+        model.nlayers = nlayers
+        model.rnntype = rnntype
+        model.attn = attn
 
-        GO = self.vocab2['<GO>']
-        EOS = self.vocab2['<EOS>']
-        vsz = embed2.vsz + 1
+        GO = model.vocab2['<GO>']
+        EOS = model.vocab2['<EOS>']
+        vsz = dst_vocab.vsz + 1
 
-        assert embed1.dsz == embed2.dsz
-        self.dsz = embed1.dsz
+        assert src_vocab.dsz == dst_vocab.dsz
+        model.dsz = src_vocab.dsz
 
         with tf.name_scope("LUT"):
-            Wi = tf.Variable(tf.constant(embed1.weights, dtype=tf.float32), name="Wi")
-            Wo = tf.Variable(tf.constant(embed2.weights, dtype=tf.float32), name="Wo")
+            Wi = tf.Variable(tf.constant(src_vocab.weights, dtype=tf.float32), name="Wi")
+            Wo = tf.Variable(tf.constant(dst_vocab.weights, dtype=tf.float32), name="Wo")
 
-            embed_in = tf.nn.embedding_lookup(Wi, self.src)
+            embed_in = tf.nn.embedding_lookup(Wi, model.src)
             
         with tf.name_scope("Recurrence"):
-            rnn_enc_tensor, final_encoder_state = self.encode(embed_in, self.src)
-            #print(final_encoder_state[0], final_encoder_state[1])
+            rnn_enc_tensor, final_encoder_state = model.encode(embed_in, model.src)
             batch_sz = tf.shape(rnn_enc_tensor)[0]
 
-            with tf.variable_scope("dec") as vs:
+            with tf.variable_scope("dec"):
                 proj = dense_layer(vsz)
-                rnn_dec_cell = self._attn_cell(rnn_enc_tensor) #[:,:-1,:])
+                rnn_dec_cell = model._attn_cell(rnn_enc_tensor) #[:,:-1,:])
 
-                if self.attn is True:
+                if model.attn is True:
                     initial_state = rnn_dec_cell.zero_state(dtype=tf.float32, batch_size=batch_sz)
                 else:
                     initial_state = final_encoder_state
@@ -134,21 +112,22 @@ class Seq2SeqModelTf_v1_1(Seq2SeqBaseTf):
                 if predict is True:
                     helper = tf.contrib.seq2seq.GreedyEmbeddingHelper(Wo, tf.fill([batch_sz], GO), EOS)
                 else:
-                    helper = tf.contrib.seq2seq.TrainingHelper(inputs=tf.nn.embedding_lookup(Wo, self.tgt), sequence_length=self.tgt_len)
+                    helper = tf.contrib.seq2seq.TrainingHelper(inputs=tf.nn.embedding_lookup(Wo, model.tgt), sequence_length=model.tgt_len)
                 decoder = tf.contrib.seq2seq.BasicDecoder(cell=rnn_dec_cell, helper=helper, initial_state=initial_state, output_layer=proj)
-                final_outputs, final_decoder_state, _ = tf.contrib.seq2seq.dynamic_decode(decoder, impute_finished=True, output_time_major=True, maximum_iterations=self.mxlen)
-                self.preds = final_outputs.rnn_output
+                final_outputs, final_decoder_state, _ = tf.contrib.seq2seq.dynamic_decode(decoder, impute_finished=True, output_time_major=True, maximum_iterations=model.mxlen)
+                model.preds = final_outputs.rnn_output
                 best = final_outputs.sample_id
 
         with tf.name_scope("Output"):
-            self.best = tf.identity(best, name='best')
-            self.probs = tf.map_fn(lambda x: tf.nn.softmax(x, name='probs'), self.preds)
-        return self
+            model.best = tf.identity(best, name='best')
+            model.probs = tf.map_fn(lambda x: tf.nn.softmax(x, name='probs'), model.preds)
+        return model
 
     def _attn_cell(self, rnn_enc_tensor):
         cell = new_multi_rnn_cell(self.hsz, self.rnntype, self.nlayers)
         if self.attn:
-            attn_mech = tf.contrib.seq2seq.LuongAttention(self.hsz, rnn_enc_tensor, self.src_len) 
+            attn_mech = tfcontrib_seq2seq.BahdanauAttention(self.hsz, rnn_enc_tensor, self.src_len)
+            #attn_mech = tfcontrib_seq2seq.LuongAttention(self.hsz, rnn_enc_tensor, self.src_len)
             cell = tf.contrib.seq2seq.AttentionWrapper(cell, attn_mech, self.hsz, name='dyn_attn_cell')
         return cell
 
@@ -178,7 +157,6 @@ class Seq2SeqModelTf_v1_1(Seq2SeqBaseTf):
 
         with open(model_base + '-2.vocab', 'w') as f:
             json.dump(self.vocab2, f)     
-        
 
     def save(self, model_base):
         self.save_md(model_base)
@@ -222,13 +200,5 @@ class Seq2SeqModelTf_v1_1(Seq2SeqBaseTf):
 
 
 def create_model(embedding1, embedding2, **kwargs):
-    hsz = int(kwargs['hsz'])
-    attn = bool(kwargs.get('attn', False))
-    layers = int(kwargs.get('layers', 1))
-    rnntype = kwargs.get('rnntype', 'lstm')
-    mxlen = kwargs.get('mxlen', 100)
-    predict = kwargs.get('predict', False)
-    sess = kwargs.get('sess', tf.Session())
-    enc_dec = Seq2SeqModelTf_v1_1()
-    enc_dec.params(sess, embedding1, embedding2, mxlen, hsz, layers, attn, rnntype, predict)
+    enc_dec = Seq2SeqModel.create(embedding1, embedding2, **kwargs)
     return enc_dec
