abc21202f70331a1cdd523fab18be04da402ef18
Brian <blester125@gmail.com>
2018-05-04 15:18:38 -0400

    Replace python-magic with home rolled magic number checks

---
+++ python/mead/mime_type.py
@@ -0,0 +1,42 @@
+import re
+from binascii import hexlify
+from functools import partial
+
+class MN(object):
+    GZIP = b'1f8b'
+    TAR = b'7573746172'
+    TAR_START = 257
+    ZIP = b'504b0304'
+
+def check_mn(b, mn=None, start=0):
+    if hexlify(b[start:start+20])[:len(mn)] == mn:
+        return True
+    return False
+
+check_gzip = partial(check_mn, mn=MN.GZIP)
+check_tar = partial(check_mn, mn=MN.TAR, start=MN.TAR_START)
+check_zip = partial(check_mn, mn=MN.ZIP)
+
+class RE(object):
+    HTML = re.compile(b"(<!doctype html>|<html.*?>)")
+    BIN = re.compile(b"\d+? \d+?$", re.MULTILINE)
+
+def check_re(b, regex=None):
+    return True if regex.match(b) else False
+
+check_html = partial(check_re, regex=RE.HTML)
+check_bin = partial(check_re, regex=RE.BIN)
+
+def mime_type(file_name):
+    b = open(file_name, 'rb').read(1024)
+    if check_gzip(b):
+        return "application/gzip"
+    if check_tar(b):
+        return "application/x-tar"
+    if check_zip(b):
+        return "application/zip"
+    if check_html(b):
+        return "text/html"
+    if check_bin(b):
+        return "application/w2v"
+    return "text/plain"
