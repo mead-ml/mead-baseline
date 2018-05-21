mead-ml/mead-baseline

commit 7174db2d8d61422ab2778b2540994a1510eb412b

Refactored Embeddings loaders and added tests (#50)

python/baseline/w2v.py
@@ -5,55 +5,44 @@ from baseline.utils import export
 __all__ = []
 exporter = export(__all__)
 
+
+def norm_weights(word_vectors):
+    norms = np.linalg.norm(word_vectors, axis=1, keepdims=True)
+    norms = (norms == 0) + norms
+    return word_vectors / norms
+
 @exporter
 class EmbeddingsModel(object):
     def __init__(self):
         super(EmbeddingsModel, self).__init__()
 
-    def get_vsz(self):
-        pass
-
     def get_dsz(self):
-        pass
+        return self.dsz
+
+    def get_vsz(self):
+        return self.vsz
 
     def lookup(self, word, nullifabsent=True):
-        pass
+        if word in self.vocab:
+            return self.weights[self.vocab[word]]
+        if nullifabsent:
+            return None
+        return self.nullv
+
 
 @exporter
-class Word2VecModel(EmbeddingsModel):
+class PretrainedEmbeddingsModel(EmbeddingsModel):
 
-    def __init__(self, filename, known_vocab=None, unif_weight=None, keep_unused=False, normalize=False):
-        super(Word2VecModel, self).__init__()
+    def __init__(self, filename, known_vocab=None, unif_weight=None, keep_unused=False, normalize=False, **kwargs):
+        super(PretrainedEmbeddingsModel, self).__init__()
         uw = 0.0 if unif_weight is None else unif_weight
         self.vocab = {}
-        idx = 0
+        idx = 1
 
-        with io.open(filename, "rb") as f:
-            header = f.readline()
-            vsz, self.dsz = map(int, header.split())
-
-            self.nullv = np.zeros(self.dsz, dtype=np.float32)
-            self.vocab["<PAD>"] = idx
-            idx += 1
-
-            word_vectors = [self.nullv]
-            width = 4 * self.dsz
-
-            for i in range(vsz):
-                word = Word2VecModel._readtospc(f)
-                raw = f.read(width)
-                if keep_unused is False and word not in known_vocab:
-                    continue
-
-                # Otherwise add it to the list and remove from knownvocab
-                if known_vocab and word in known_vocab:
-                    known_vocab[word] = 0
-
-                vec = np.fromstring(raw, dtype=np.float32)
-                word_vectors.append(vec)
-
-                self.vocab[word] = idx
-                idx += 1
+        word_vectors, self.dsz, known_vocab, idx = self._read_vectors(filename, idx, known_vocab, keep_unused)
+        self.nullv = np.zeros(self.dsz, dtype=np.float32)
+        word_vectors = [self.nullv] + word_vectors
+        self.vocab["<PAD>"] = 0
 
         if known_vocab is not None:
             unknown = {v: cnt for v,cnt in known_vocab.items() if cnt > 0}
@@ -62,19 +51,41 @@ class Word2VecModel(EmbeddingsModel):
                 self.vocab[v] = idx
                 idx += 1
 
-        if normalize is True:
-            for i in range(len(word_vectors)):
-                norm = np.linalg.norm(word_vectors[i])
-                word_vectors[i] = word_vectors[i] if norm == 0.0 else word_vectors[i]/norm
-
         self.weights = np.array(word_vectors)
+        if normalize is True:
+            self.weights = norm_weights(self.weights)
+
         self.vsz = self.weights.shape[0] - 1
 
-    def get_dsz(self):
-        return self.dsz
+    def _read_vectors(self, filename, idx, known_vocab, keep_unused):
+        pass
 
-    def get_vsz(self):
-        return self.vsz
+
+@exporter
+class Word2VecModel(PretrainedEmbeddingsModel):
+
+    def __init__(self, filename, **kwargs):
+        super(Word2VecModel, self).__init__(filename, **kwargs)
+
+    def _read_vectors(self, filename, idx, known_vocab, keep_unused):
+        word_vectors = []
+        with io.open(filename, "rb") as f:
+            header = f.readline()
+            vsz, dsz = map(int, header.split())
+            width = 4 * dsz
+            for i in range(vsz):
+                word = Word2VecModel._readtospc(f)
+                raw = f.read(width)
+                if keep_unused is False and word not in known_vocab:
+                    continue
+                if known_vocab and word in known_vocab:
+                    known_vocab[word] = 0
+                vec = np.fromstring(raw, dtype=np.float32)
+                word_vectors.append(vec)
+                self.vocab[word] = idx
+                idx += 1
+        return word_vectors, dsz, known_vocab, idx
+
 
     @staticmethod
     def _readtospc(f):
@@ -88,23 +99,14 @@ class Word2VecModel(EmbeddingsModel):
         s = s.decode('utf-8')
         return s.strip()
 
-    def lookup(self, word, nullifabsent=True):
-        if word in self.vocab:
-            return self.weights[self.vocab[word]]
-        if nullifabsent:
-            return None
-        return self.nullv
-
 
 @exporter
-class GloVeModel(EmbeddingsModel):
+class GloVeModel(PretrainedEmbeddingsModel):
 
-    def __init__(self, filename, known_vocab=None, unif_weight=None, keep_unused=False, normalize=False):
-        super(GloVeModel, self).__init__()
-        uw = 0.0 if unif_weight is None else unif_weight
-        self.vocab = {}
-        idx = 1
+    def __init__(self, filename, **kwargs):
+        super(GloVeModel, self).__init__(filename, **kwargs)
 
+    def _read_vectors(self, filename, idx, known_vocab, keep_unused):
         word_vectors = []
         with io.open(filename, "r", encoding="utf-8") as f:
             for line in f:
@@ -112,46 +114,14 @@ class GloVeModel(EmbeddingsModel):
                 word = values[0]
                 if keep_unused is False and word not in known_vocab:
                     continue
-
-                # Otherwise add it to the list and remove from knownvocab
                 if known_vocab and word in known_vocab:
                     known_vocab[word] = 0
                 vec = np.asarray(values[1:], dtype=np.float32)
                 word_vectors.append(vec)
                 self.vocab[word] = idx
                 idx += 1
-            self.dsz = vec.shape[0]
-            self.nullv = np.zeros(self.dsz, dtype=np.float32)
-            word_vectors = [self.nullv] + word_vectors
-            self.vocab["<PAD>"] = 0
-
-        if known_vocab is not None:
-            unknown = {v: cnt for v, cnt in known_vocab.items() if cnt > 0}
-            for v in unknown:
-                word_vectors.append(np.random.uniform(-uw, uw, self.dsz))
-                self.vocab[v] = idx
-                idx += 1
-
-        if normalize is True:
-            for i in range(len(word_vectors)):
-                norm = np.linalg.norm(word_vectors[i])
-                word_vectors[i] = word_vectors[i] if norm == 0.0 else word_vectors[i]/norm
-
-        self.weights = np.array(word_vectors)
-        self.vsz = self.weights.shape[0] - 1
-
-    def get_dsz(self):
-        return self.dsz
-
-    def get_vsz(self):
-        return self.vsz
-
-    def lookup(self, word, nullifabsent=True):
-        if word in self.vocab:
-            return self.weights[self.vocab[word]]
-        if nullifabsent:
-            return None
-        return self.nullv
+        dsz = vec.shape[0]
+        return word_vectors, dsz, known_vocab, idx
 
 
 @exporter
@@ -179,16 +149,3 @@ class RandomInitVecModel(EmbeddingsModel):
 
         self.nullv = np.zeros(self.dsz, dtype=np.float32)
         self.weights[0] = self.nullv
-
-    def get_dsz(self):
-        return self.dsz
-
-    def get_vsz(self):
-        return self.vsz
-
-    def lookup(self, word, nullifabsent=True):
-        if word in self.vocab:
-            return self.weights[self.vocab[word]]
-        if nullifabsent:
-            return None
-        return self.nullv

test/test_parallel_conv.py
@@ -1,108 +1,110 @@
-import random
-from unittest.mock import patch, MagicMock
-import numpy as np
-import tensorflow as tf
-from baseline.tf.tfy import parallel_conv, char_word_conv_embeddings, char_word_conv_embeddings_var_fm
+try:
+    import random
+    from mock import patch, MagicMock
+    import numpy as np
+    import tensorflow as tf
+    from baseline.tf.tfy import parallel_conv, char_word_conv_embeddings, char_word_conv_embeddings_var_fm
 
 
-class ParallelConvTest(tf.test.TestCase):
+    class ParallelConvTest(tf.test.TestCase):
 
-    def setUp(self):
-        tf.reset_default_graph()
-        self.batchsz = random.randint(5, 65)
-        self.seqsz = random.randint(5, 11)
-        self.embedsz = random.randint(100, 301)
-        self.num_filt = random.randint(2, 6)
-        self.filtsz = set()
-        while len(self.filtsz) != self.num_filt:
-            self.filtsz.add(random.randint(2, 7))
-        self.filtsz = list(self.filtsz)
-        self.motsz = random.randint(128, 257)
-        self.nfeat_factor = random.randint(1, 4)
-        self.max_feat = random.randint(100, 301)
-        self.input = np.random.uniform(size=(self.batchsz, self.seqsz, self.embedsz)).astype(np.float32)
-        self.p = tf.placeholder(tf.float32, shape=(None, self.seqsz, self.embedsz))
+        def setUp(self):
+            tf.reset_default_graph()
+            self.batchsz = random.randint(5, 65)
+            self.seqsz = random.randint(5, 11)
+            self.embedsz = random.randint(100, 301)
+            self.num_filt = random.randint(2, 6)
+            self.filtsz = set()
+            while len(self.filtsz) != self.num_filt:
+                self.filtsz.add(random.randint(2, 7))
+            self.filtsz = list(self.filtsz)
+            self.motsz = random.randint(128, 257)
+            self.nfeat_factor = random.randint(1, 4)
+            self.max_feat = random.randint(100, 301)
+            self.input = np.random.uniform(size=(self.batchsz, self.seqsz, self.embedsz)).astype(np.float32)
+            self.p = tf.placeholder(tf.float32, shape=(None, self.seqsz, self.embedsz))
 
-    def test_output_batch_shape_int_arg(self):
-        conv = parallel_conv(self.p, self.filtsz, self.embedsz, self.motsz)
-        with self.test_session() as sess:
-            sess.run(tf.global_variables_initializer())
-            self.assertEqual(conv.eval({self.p: self.input}).shape[0], self.batchsz)
-
-    def test_output_batch_shape_list_arg(self):
-        motsz = [self.motsz] * len(self.filtsz)
-        conv = parallel_conv(self.p, self.filtsz, self.embedsz, motsz)
-        with self.test_session() as sess:
-            sess.run(tf.global_variables_initializer())
-            self.assertEqual(conv.eval({self.p: self.input}).shape[0], self.batchsz)
-
-    def test_output_feature_shape_int_arg(self):
-        conv = parallel_conv(self.p, self.filtsz, self.embedsz, self.motsz)
-        with self.test_session() as sess:
-            sess.run(tf.global_variables_initializer())
-            self.assertEqual(conv.eval({self.p: self.input}).shape[1], self.motsz * self.num_filt)
-
-    def test_output_feature_shape_list_arg(self):
-        motsz = [self.nfeat_factor * fsz for fsz in self.filtsz]
-        conv = parallel_conv(self.p, self.filtsz, self.embedsz, motsz)
-        with self.test_session() as sess:
-            sess.run(tf.global_variables_initializer())
-            self.assertEqual(conv.eval({self.p: self.input}).shape[1], sum(motsz))
-
-    def test_shape_available_int(self):
-        """The previous tests test the shape of the actual output tensor. This
-        tests the output shape information available when building the graph.
-        When tf.squeeze is used on a tensor with a None dimension all shape info
-        is lost because it is unknown if the None dimension is 1 and should be
-        squeezed out. This cased error later because layers needed shape information
-        to set the right size. This test makes sure that needed size information
-        is present. This test would have caught the break in the classify method.
-        """
-        conv = parallel_conv(self.p, self.filtsz, self.embedsz, self.motsz)
-        conv_shape = conv.get_shape().as_list()
-        self.assertEqual(conv_shape, [None, self.motsz * len(self.filtsz)])
-
-    def test_shape_available_list(self):
-        """Same as the `test_shape_available_int` commnet."""
-        motsz = [self.nfeat_factor * fsz for fsz in self.filtsz]
-        conv = parallel_conv(self.p, self.filtsz, self.embedsz, motsz)
-        conv_shape = conv.get_shape().as_list()
-        self.assertEqual(conv_shape, [None, sum(motsz)])
-
-    def test_conv_called(self):
-        with patch('baseline.tf.tfy.tf.nn.conv2d') as conv_mock:
-            conv_mock.return_value = tf.zeros((self.batchsz, 1, self.seqsz, self.motsz))
+        def test_output_batch_shape_int_arg(self):
             conv = parallel_conv(self.p, self.filtsz, self.embedsz, self.motsz)
-            self.assertEqual(conv_mock.call_count, self.num_filt)
+            with self.test_session() as sess:
+                sess.run(tf.global_variables_initializer())
+                self.assertEqual(conv.eval({self.p: self.input}).shape[0], self.batchsz)
 
-    def test_list_and_number_args_equal(self):
-        with tf.variable_scope("TEST"):
-            conv1 = parallel_conv(self.p, self.filtsz, self.embedsz, self.motsz)
-        with tf.variable_scope("TEST", reuse=True):
-            conv2 = parallel_conv(self.p, self.filtsz, self.embedsz, [self.motsz] * len(self.filtsz))
-        with self.test_session() as sess:
-            sess.run(tf.global_variables_initializer())
-            np.testing.assert_allclose(conv1.eval({self.p: self.input}), conv2.eval({self.p: self.input}))
+        def test_output_batch_shape_list_arg(self):
+            motsz = [self.motsz] * len(self.filtsz)
+            conv = parallel_conv(self.p, self.filtsz, self.embedsz, motsz)
+            with self.test_session() as sess:
+                sess.run(tf.global_variables_initializer())
+                self.assertEqual(conv.eval({self.p: self.input}).shape[0], self.batchsz)
 
-    @patch('baseline.tf.tfy.parallel_conv')
-    @patch('baseline.tf.tfy.skip_conns')
-    def test_char_word_call_correct(self, skip_mock, conv_mock):
-        conv_ret = MagicMock()
-        conv_mock.return_value = conv_ret
-        _ = char_word_conv_embeddings(self.p, self.filtsz, self.embedsz, self.motsz)
-        conv_mock.assert_called_once_with(self.p, self.filtsz, self.embedsz, self.motsz, tf.nn.tanh)
-        skip_mock.assert_called_once_with(conv_ret, self.motsz * len(self.filtsz), 1)
+        def test_output_feature_shape_int_arg(self):
+            conv = parallel_conv(self.p, self.filtsz, self.embedsz, self.motsz)
+            with self.test_session() as sess:
+                sess.run(tf.global_variables_initializer())
+                self.assertEqual(conv.eval({self.p: self.input}).shape[1], self.motsz * self.num_filt)
 
-    @patch('baseline.tf.tfy.parallel_conv')
-    @patch('baseline.tf.tfy.highway_conns')
-    def test_char_word_var_fm_call_correct(self, skip_mock, conv_mock):
-        conv_ret = MagicMock()
-        conv_mock.return_value = conv_ret
-        nfeats = [min(self.nfeat_factor * fsz, self.max_feat) for fsz in self.filtsz]
-        _ = char_word_conv_embeddings_var_fm(self.p, self.filtsz, self.embedsz, self.nfeat_factor, max_feat=self.max_feat)
-        conv_mock.assert_called_once_with(self.p, self.filtsz, self.embedsz, nfeats, tf.nn.tanh)
-        skip_mock.assert_called_once_with(conv_ret, sum(nfeats), 2)
+        def test_output_feature_shape_list_arg(self):
+            motsz = [self.nfeat_factor * fsz for fsz in self.filtsz]
+            conv = parallel_conv(self.p, self.filtsz, self.embedsz, motsz)
+            with self.test_session() as sess:
+                sess.run(tf.global_variables_initializer())
+                self.assertEqual(conv.eval({self.p: self.input}).shape[1], sum(motsz))
 
+        def test_shape_available_int(self):
+            """The previous tests test the shape of the actual output tensor. This
+            tests the output shape information available when building the graph.
+            When tf.squeeze is used on a tensor with a None dimension all shape info
+            is lost because it is unknown if the None dimension is 1 and should be
+            squeezed out. This cased error later because layers needed shape information
+            to set the right size. This test makes sure that needed size information
+            is present. This test would have caught the break in the classify method.
+            """
+            conv = parallel_conv(self.p, self.filtsz, self.embedsz, self.motsz)
+            conv_shape = conv.get_shape().as_list()
+            self.assertEqual(conv_shape, [None, self.motsz * len(self.filtsz)])
+
+        def test_shape_available_list(self):
+            """Same as the `test_shape_available_int` commnet."""
+            motsz = [self.nfeat_factor * fsz for fsz in self.filtsz]
+            conv = parallel_conv(self.p, self.filtsz, self.embedsz, motsz)
+            conv_shape = conv.get_shape().as_list()
+            self.assertEqual(conv_shape, [None, sum(motsz)])
+
+        def test_conv_called(self):
+            with patch('baseline.tf.tfy.tf.nn.conv2d') as conv_mock:
+                conv_mock.return_value = tf.zeros((self.batchsz, 1, self.seqsz, self.motsz))
+                conv = parallel_conv(self.p, self.filtsz, self.embedsz, self.motsz)
+                self.assertEqual(conv_mock.call_count, self.num_filt)
+
+        def test_list_and_number_args_equal(self):
+            with tf.variable_scope("TEST"):
+                conv1 = parallel_conv(self.p, self.filtsz, self.embedsz, self.motsz)
+            with tf.variable_scope("TEST", reuse=True):
+                conv2 = parallel_conv(self.p, self.filtsz, self.embedsz, [self.motsz] * len(self.filtsz))
+            with self.test_session() as sess:
+                sess.run(tf.global_variables_initializer())
+                np.testing.assert_allclose(conv1.eval({self.p: self.input}), conv2.eval({self.p: self.input}))
+
+        @patch('baseline.tf.tfy.parallel_conv')
+        @patch('baseline.tf.tfy.skip_conns')
+        def test_char_word_call_correct(self, skip_mock, conv_mock):
+            conv_ret = MagicMock()
+            conv_mock.return_value = conv_ret
+            _ = char_word_conv_embeddings(self.p, self.filtsz, self.embedsz, self.motsz)
+            conv_mock.assert_called_once_with(self.p, self.filtsz, self.embedsz, self.motsz, tf.nn.tanh)
+            skip_mock.assert_called_once_with(conv_ret, self.motsz * len(self.filtsz), 1)
+
+        @patch('baseline.tf.tfy.parallel_conv')
+        @patch('baseline.tf.tfy.highway_conns')
+        def test_char_word_var_fm_call_correct(self, skip_mock, conv_mock):
+            conv_ret = MagicMock()
+            conv_mock.return_value = conv_ret
+            nfeats = [min(self.nfeat_factor * fsz, self.max_feat) for fsz in self.filtsz]
+            _ = char_word_conv_embeddings_var_fm(self.p, self.filtsz, self.embedsz, self.nfeat_factor, max_feat=self.max_feat)
+            conv_mock.assert_called_once_with(self.p, self.filtsz, self.embedsz, nfeats, tf.nn.tanh)
+            skip_mock.assert_called_once_with(conv_ret, sum(nfeats), 2)
+except ImportError:
+    pass
 
 if __name__ == "__main__":
     tf.test.main()

python/install_dev.sh
@@ -1,4 +1,14 @@
+package=${1:-"baseline"}
+test=${2:-"test"}
+
 rm -rf *.egg*
-cp setup_$1.py setup.py
-pip install -e .
+cp setup_$package.py setup.py
+pip install -e .[test]
 rm setup.py
+
+if [ $package = "baseline" ]; then
+    if [ $test = "test" ]; then
+        cd ../test
+        pytest
+    fi
+fi

python/setup_baseline.py
@@ -81,6 +81,9 @@ def main():
             'six',
             'requests',
         ],
+        extras_require={
+            'test': ['pytest', 'mock']
+        },
         entry_points={
             'console_scripts': [
                 'mead-train = mead.trainer:main',

test/test_embeddings.py
@@ -1,0 +1,182 @@
+import os
+import math
+import random
+import string
+from functools import partial
+import pytest
+import numpy as np
+from baseline.w2v import *
+from baseline.w2v import norm_weights
+
+loc = os.path.dirname(os.path.realpath(__file__))
+GLOVE_FILE = os.path.join(loc, "test_data", "glove_test.txt")
+W2V_FILE = os.path.join(loc, "test_data", "w2v_test.bin")
+
+def random_model():
+    """Randomly pick a model for testing."""
+    models = [GloVeModel, Word2VecModel]
+    model = random.choice(models)
+    if model == GloVeModel:
+        return partial(GloVeModel, GLOVE_FILE)
+    elif model == Word2VecModel:
+        return partial(Word2VecModel, W2V_FILE)
+
+def test_glove_vsz():
+    gold_vsz = 10
+    wv = GloVeModel(GLOVE_FILE, keep_unused=True)
+    assert wv.get_vsz() == gold_vsz
+
+def test_glove_dsz():
+    gold_dsz = 50
+    wv = GloVeModel(GLOVE_FILE, keep_unused=True)
+    assert wv.get_dsz() == gold_dsz
+
+def test_w2v_vsz():
+    gold_vsz = 10
+    wv = Word2VecModel(W2V_FILE, keep_unused=True)
+    assert wv.get_vsz() == gold_vsz
+
+def test_w2v_dsz():
+    gold_dsz = 300
+    wv = Word2VecModel(W2V_FILE, keep_unused=True)
+    assert wv.get_dsz() == gold_dsz
+
+def test_rand_vsz():
+    ref_wv = random_model()(keep_unused=True)
+    vocab = ref_wv.vocab
+    gold_dsz = ref_wv.get_dsz()
+    wv = RandomInitVecModel(gold_dsz, vocab)
+    assert wv.get_vsz() == len(vocab) - 1
+
+def test_rand_dsz():
+    ref_wv = random_model()(keep_unused=True)
+    vocab = ref_wv.vocab
+    gold_dsz = ref_wv.get_dsz()
+    wv = RandomInitVecModel(gold_dsz, vocab)
+    assert wv.get_dsz() == gold_dsz
+
+def test_random_vector_range():
+    gold_weight = 0.4
+    wv = RandomInitVecModel(
+        300, {k: 1 for k in list(string.ascii_letters)},
+        unif_weight=gold_weight
+    )
+    assert np.min(wv.weights) >= -gold_weight
+    assert np.max(wv.weights) <= gold_weight
+
+def test_valid_lookup():
+    wv = random_model()(keep_unused=True)
+    key = '<PAD>'
+    while key == '<PAD>':
+        key = random.choice(list(wv.vocab.keys()))
+    res = wv.lookup(key)
+    assert res is not None
+    assert res.shape == (wv.get_dsz(),)
+    with pytest.raises(AssertionError):
+        np.testing.assert_allclose(res, np.zeros((wv.get_dsz(),)))
+
+def test_nullv_lookup():
+    wv = random_model()(keep_unused=True)
+    key = "zzzzzzzzzzz"
+    assert key not in wv.vocab
+    res = wv.lookup(key, nullifabsent=False)
+    np.testing.assert_allclose(res, np.zeros((wv.get_dsz())))
+
+def test_none_lookup():
+    wv = random_model()(keep_unused=True)
+    key = "zzzzzzzzzzz"
+    assert key not in wv.vocab
+    res = wv.lookup(key)
+    assert res is None
+
+def test_normalize_e2e():
+    wv = random_model()(normalize=True, keep_unused=True)
+    norms = np.linalg.norm(wv.weights, axis=1)
+    for norm in norms:
+        assert norm == 0 or np.allclose(norm, 1, rtol=1e-4)
+
+def test_normalize():
+    wv = random_model()(keep_unused=True)
+    normed = norm_weights(wv.weights)
+    norms = np.linalg.norm(normed, axis=1)
+    for norm in norms:
+        assert norm == 0 or np.allclose(norm, 1, rtol=1e-4)
+
+def test_vocab_truncation():
+    model = random_model()
+    wv = model(keep_unused=True)
+    gold = wv.vocab
+    keys = list(gold.keys())
+    removed = '<PAD>'
+    while removed == '<PAD>':
+        removed = random.choice(keys)
+    gold.pop(removed)
+    wv = model(known_vocab=gold)
+    assert set(gold.keys()) == set(wv.vocab.keys())
+    assert removed not in wv.vocab
+
+def test_vocab_not_truncated():
+    model = random_model()
+    wv = model(keep_unused=True)
+    gold = wv.vocab
+    keys = list(gold.keys())
+    removed = '<PAD>'
+    while removed == '<PAD>':
+        removed = random.choice(keys)
+    gold.pop(removed)
+    assert set(keys) != set(gold)
+    wv = model(known_vocab=gold, keep_unused=True)
+    assert set(keys) == set(wv.vocab.keys())
+    assert removed in wv.vocab
+
+def test_extra_vocab():
+    model = random_model()
+    wv = model(keep_unused=True)
+    vocab = wv.vocab
+    extra_keys = ["AAAAAAAAAAAA", "ZZZZZZZZZZ"]
+    for key in extra_keys:
+        vocab[key] = 12
+    wv = model(known_vocab=vocab)
+    for key in extra_keys:
+        assert key in wv.vocab
+    for key in extra_keys:
+        np.testing.assert_allclose(wv.lookup(key), np.zeros((wv.get_dsz(),)))
+
+def test_extra_vocab_weights():
+    """Test that words not in the pretrained vocab are initialized correctly."""
+    weight = 0.5
+    model = random_model()
+    wv = model(keep_unused=True, unif_weight=weight)
+    vocab = wv.vocab
+    extra_keys = ["AAAAAAAAAAAA", "ZZZZZZZZZZ"]
+    for key in extra_keys:
+        vocab[key] = 12
+    wv = model(known_vocab=vocab)
+    for key in extra_keys:
+        vec = wv.lookup(key)
+        assert np.min(vec) >= -weight
+        assert np.max(vec) <= weight
+
+def test_rand_no_counts():
+    """Test that unattested word are not removed without counts."""
+    vocab = {
+        "A": 0,
+        "B": 0,
+        "C": 13,
+        "D": 55
+    }
+    wv = RandomInitVecModel(random.randint(1, 301), vocab, counts=False)
+    assert wv.get_vsz() == len(vocab) - 1
+
+def test_rand_counts():
+    """Test that unattested word are removed with counts."""
+    vocab = {
+        "A": 0,
+        "B": 0,
+        "C": 13,
+        "D": 55
+    }
+    wv = RandomInitVecModel(12, vocab, counts=True)
+    gold = {"C", "D"}
+    for g in gold:
+        assert g in wv.vocab

commit 5c13b59eba7b7d63415e12ead05bc5b0925087ae

Fix regression introduced by #50

python/baseline/w2v.py
@@ -11,6 +11,7 @@ def norm_weights(word_vectors):
     norms = (norms == 0) + norms
     return word_vectors / norms
 
+
 @exporter
 class EmbeddingsModel(object):
     def __init__(self):
@@ -45,7 +46,7 @@ class PretrainedEmbeddingsModel(EmbeddingsModel):
         self.vocab["<PAD>"] = 0
 
         if known_vocab is not None:
-            unknown = {v: cnt for v,cnt in known_vocab.items() if cnt > 0}
+            unknown = {v: cnt for v, cnt in known_vocab.items() if cnt > 0}
             for v in unknown:
                 word_vectors.append(np.random.uniform(-uw, uw, self.dsz))
                 self.vocab[v] = idx
@@ -64,8 +65,8 @@ class PretrainedEmbeddingsModel(EmbeddingsModel):
 @exporter
 class Word2VecModel(PretrainedEmbeddingsModel):
 
-    def __init__(self, filename, **kwargs):
-        super(Word2VecModel, self).__init__(filename, **kwargs)
+    def __init__(self, filename, known_vocab=None, unif_weight=None, keep_unused=False, normalize=False, **kwargs):
+        super(Word2VecModel, self).__init__(filename, known_vocab, unif_weight, keep_unused, normalize, **kwargs)
 
     def _read_vectors(self, filename, idx, known_vocab, keep_unused):
         word_vectors = []
@@ -86,7 +87,6 @@ class Word2VecModel(PretrainedEmbeddingsModel):
                 idx += 1
         return word_vectors, dsz, known_vocab, idx
 
-
     @staticmethod
     def _readtospc(f):
 
@@ -103,8 +103,8 @@ class Word2VecModel(PretrainedEmbeddingsModel):
 @exporter
 class GloVeModel(PretrainedEmbeddingsModel):
 
-    def __init__(self, filename, **kwargs):
-        super(GloVeModel, self).__init__(filename, **kwargs)
+    def __init__(self, filename, known_vocab=None, unif_weight=None, keep_unused=False, normalize=False, **kwargs):
+        super(GloVeModel, self).__init__(filename, known_vocab, unif_weight, keep_unused, normalize, **kwargs)
 
     def _read_vectors(self, filename, idx, known_vocab, keep_unused):
         word_vectors = []

test/test_parallel_conv.py
@@ -103,7 +103,7 @@ try:
             _ = char_word_conv_embeddings_var_fm(self.p, self.filtsz, self.embedsz, self.nfeat_factor, max_feat=self.max_feat)
             conv_mock.assert_called_once_with(self.p, self.filtsz, self.embedsz, nfeats, tf.nn.tanh)
             skip_mock.assert_called_once_with(conv_ret, sum(nfeats), 2)
-except ImportError:
+except ImportError as e:
     pass
 
 if __name__ == "__main__":