37d27c7bc9500fa226699b1560a3791182685afe
schoudhury <schoudhury@interactions.com>
2018-08-10 19:59:30 +0000

    allow external directories in script

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -1,8 +1,8 @@
 usage() {
-    echo "Usage: $0 [-g <GPU number>] [-n <container name>] [-t <container type: tf/pytorch>]" 1>&2; exit 1;
+    echo "Usage: $0 [-g <GPU number>] [-n <container name>] [-t <container type: tf/pytorch>] [-e <external-mount-directories>]" 1>&2; exit 1;
 }
 
-while getopts ":g:n:t:" x; do
+while getopts ":g:n:t:e:" x; do
     case "${x}" in
         g)
             GPU_NUM=${OPTARG}
@@ -13,6 +13,8 @@ while getopts ":g:n:t:" x; do
         t)  
             CON_TYPE=${OPTARG}
             ;; 
+        e)
+            EXTERNAL_MOUNTS+=("$OPTARG");;
         *)
             usage
             ;;
@@ -26,14 +28,20 @@ if [[ -z "${GPU_NUM// }" || -z "${CON_NAME//}" || -z "${CON_TYPE//}" ]]; then
     exit 1
 fi
 echo "using GPU: "${GPU_NUM}", container name: "${CON_NAME}, container type: "${CON_TYPE}"
+echo "external mount directories ${EXTERNAL_MOUNTS[@]}"
 
 CON_BUILD=baseline-${CON_TYPE}
-
 if [ -e $HOME/.bl-data ]; then
     CACHE_MOUNT="-v $HOME/.bl-data:$HOME/.bl-data"
 else
     CACHE_MOUNT=""
 fi
 
-NV_GPU=${GPU_NUM} nvidia-docker run -e LANG=C.UTF-8 --rm --name=${CON_NAME} --net=host -v /data/embeddings:/data/embeddings:ro -v /data/datasets:/data/datasets:ro -v /data/model-store:/data/model-store -v /data/model-checkpoints:/data/model-checkpoints ${CACHE_MOUNT} -it ${CON_BUILD} bash
+NUM_EXTERNAL=${#EXTERNAL_MOUNTS[@]}
+for ((i=0;i<NUM_EXTERNAL;i++)); do
+    BASENAME=`basename ${EXTERNAL_MOUNTS[i]}` 
+    EXTERNAL_MOUNTS[i]="-v ${EXTERNAL_MOUNTS[i]}:/$BASENAME"
+done
+
+NV_GPU=${GPU_NUM} nvidia-docker run -e LANG=C.UTF-8 --rm --name=${CON_NAME} --net=host -v /data/embeddings:/data/embeddings:ro -v /data/datasets:/data/datasets:ro -v /data/model-store:/data/model-store -v /data/model-checkpoints:/data/model-checkpoints ${EXTERNAL_MOUNTS[@]} ${CACHE_MOUNT} -it ${CON_BUILD} bash
 
--- docs/docker.md
+++ docs/docker.md
@@ -12,6 +12,6 @@ We assume all necessary files (datasests, embeddings etc.) are stored in `/data/
 
 For convenience, we also provide a [build script](../docker/build_docker.sh), and a [run script](../docker/run_docker.sh). The usual pipeline is this:
 
-- build a docker image: `./build_docker.sh tf` or `./build_docker.sh pyt` 
+- build a docker image: `./build_docker.sh tf` or `./build_docker.sh pytorch` 
 
-- run the container: `./run_docker.sh -g <gpu_number> -n <container_name> -t <image_type>`, eg. `./run_docker.sh -g 0 -n test -t tf`.
+- run the container: `./run_docker.sh -g <gpu_number> -n <container_name> -t <image_type> -e <external directories to mount>`, eg. `./run_docker.sh -g 0 -n test -t tf -e ~/test` or `./run_docker.sh -g 0 -n test -t pytorch -e ~/test ~/test1/`. An external directory `/a/b/c` is mounted as `/c` in the docker.
