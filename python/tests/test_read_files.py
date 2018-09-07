450f2cb586449f5e86f30f71fb6fff60a0e8425b
Brian <blester125@gmail.com>
2018-09-07 11:41:24 -0400

    Added tests

---
+++ python/tests/test_read_files.py
@@ -0,0 +1,67 @@
+import mock
+import pytest
+
+from baseline.utils import read_config_file, read_json, read_yaml
+
+@pytest.fixture
+def gold_data():
+    return {
+        "a": 1,
+        "b": {
+            "c": 2,
+        },
+    }
+
+
+def test_read_json(gold_data):
+    data = read_json("test_data/test_json.json")
+    assert data == gold_data
+
+def test_read_json_default_value():
+    gold_default = {}
+    data = read_json("test_data/not_there.json")
+    assert data == gold_default
+
+def test_read_json_given_default():
+    gold_default = "default"
+    data = read_json("test_data/not_there.json", gold_default)
+    assert data == gold_default
+
+def test_read_json_strict():
+    with pytest.raises(FileNotFoundError):
+        read_json("test_data/not_there.json", strict=True)
+
+def test_read_yaml(gold_data):
+    pytest.importorskip('yaml')
+    data = read_yaml("test_data/test_yaml.yml")
+    assert data == gold_data
+
+def test_read_yaml_default_value():
+    pytest.importorskip('yaml')
+    gold_default = {}
+    data = read_yaml("test_data/not_there.yml")
+    assert data == gold_default
+
+def test_read_yaml_given_default():
+    pytest.importorskip('yaml')
+    gold_default = "default"
+    data = read_yaml("test_data/not_there.yml", gold_default)
+    assert data == gold_default
+
+def test_read_yaml_strict():
+    pytest.importorskip('yaml')
+    with pytest.raises(FileNotFoundError):
+        read_yaml("test_data/not_there.yml", strict=True)
+
+def test_read_config_json_dispatch():
+    file_name = 'example.json'
+    with mock.patch('baseline.utils.read_json') as read_patch:
+        read_config_file(file_name)
+    read_patch.assert_called_once_with(file_name, strict=True)
+
+def test_read_config_ymal_dispatch():
+    pytest.importorskip('yaml')
+    file_name = 'example.yml'
+    with mock.patch('baseline.utils.read_yaml') as read_patch:
+        read_config_file(file_name)
+    read_patch.assert_called_once_with(file_name, strict=True)
