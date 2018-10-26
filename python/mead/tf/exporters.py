20b09ef2b1bc1ddf5756751eb65425ee4ea5181d
Matt Barta <mtbarta@gmail.com>
2018-10-26 14:57:43 -0400

    exporters save an assets json file

--- python/mead/tf/exporters.py
+++ python/mead/tf/exporters.py
@@ -1,13 +1,15 @@
 import baseline
 import os
+import datetime
 from tensorflow.python.framework.errors_impl import NotFoundError
 import mead.exporters
 from mead.exporters import register_exporter
 from baseline.tf.embeddings import *
-from baseline.utils import export, read_json, ls_props, Offsets
+from baseline.utils import export, read_json, ls_props, Offsets, write_json
 from collections import namedtuple
 
 FIELD_NAME = 'text/tokens'
+ASSET_FILE_NAME = b'model.assets'
 
 __all__ = []
 exporter = export(__all__)
@@ -15,6 +17,49 @@ exporter = export(__all__)
 
 SignatureOutput = namedtuple("SignatureOutput", ("classes", "scores"))
 
+def create_bundle(builder, output_path, assets=None):
+    """Creates the output files for an exported model.
+
+    :builder the tensorflow saved_model builder.
+    :output_path the path to export a model. this includes the model_version name.
+    :assets a dictionary of assets to save alongside the model.
+    """
+    builder.save()
+
+    if assets:
+        asset_file = os.path.join(output_path, ASSET_FILE_NAME)
+        write_json(assets, asset_file)
+
+def create_assets(basename, sig_input, sig_output, sig_name, lengths_key=None):
+    """Save required variables for running an exported model from baseline's services.
+
+    :basename the base model name. e.g. tagger-26075
+    :sig_input the input dictionary
+    :sig_output the output namedTuple
+    :lengths_key the lengths_key from the model.
+        used to translate batch output. Exported models will return a flat list,
+        and it needs to be reshaped into per-example lists. We use this key to tell
+        us which feature holds the sequence legnths.
+
+    """
+    inputs = [k for k in sig_input]
+    outputs =  sig_output._fields
+
+    model_name = basename.split("/")[-1]
+    assets = {
+        'inputs': inputs,
+        'outputs': outputs,
+        'signature_name': sig_name,
+        'metadata': {
+            'exported_model': model_name,
+            'exported_time': str(datetime.datetime.utcnow()),
+        }
+    }
+
+    if lengths_key:
+        assets['lengths_key'] = lengths_key
+
+    return assets
 
 @exporter
 class TensorFlowExporter(mead.exporters.Exporter):
@@ -43,12 +88,21 @@ class TensorFlowExporter(mead.exporters.Exporter):
         with tf.Graph().as_default():
             config_proto = tf.ConfigProto(allow_soft_placement=True)
             with tf.Session(config=config_proto) as sess:
-                sig_input, sig_output, sig_name = self._create_rpc_call(sess, basename)
+                sig_input, sig_output, sig_name, assets = self._create_rpc_call(sess, basename)
                 output_path = os.path.join(tf.compat.as_bytes(output_dir), tf.compat.as_bytes(str(model_version)))
                 print('Exporting trained model to %s' % output_path)
-                builder = self._create_saved_model_builder(sess, output_path, sig_input, sig_output, sig_name)
-                builder.save()
-                print('Successfully exported model to %s' % output_dir)
+
+                try:
+                    builder = self._create_saved_model_builder(sess, output_path, sig_input, sig_output, sig_name)
+                    create_bundle(builder, output_path, assets)
+                    print('Successfully exported model to %s' % output_dir)
+                except AssertionError as e:
+                    # model already exists
+                    raise e
+                except Exception as e:
+                    # export process broke.
+                    # TODO(MB): we should remove the directory, if one has been saved already.
+                    raise e
 
     def _create_saved_model_builder(self, sess, output_path, sig_input, sig_output, sig_name):
         """
@@ -149,7 +203,9 @@ class ClassifyTensorFlowExporter(TensorFlowExporter):
 
         sig_input = predict_tensors
         sig_output = SignatureOutput(classes, values)
-        return sig_input, sig_output, 'predict_text'
+        sig_name = 'predict_text'
+        assets = create_assets(basename, sig_input, sig_output, sig_name, model.lengths_key)
+        return sig_input, sig_output, sig_name, assets
 
 
 @exporter
@@ -197,13 +253,13 @@ class TaggerTensorFlowExporter(TensorFlowExporter):
             indices, _ = tf.contrib.crf.crf_decode(model.probs, model.A, lengths)
             indices = indices[:, 1:]
 
-        # list_of_labels = [''] * len(labels)
-        # for label, idval in labels.items():
-        #     list_of_labels[idval] = label
+        list_of_labels = [''] * len(labels)
+        for label, idval in labels.items():
+            list_of_labels[idval] = label
 
-        # class_tensor = tf.constant(list_of_labels)
-        # table = tf.contrib.lookup.index_to_string_table_from_tensor(class_tensor)
-        # classes = table.lookup(tf.to_int64(indices))
+        class_tensor = tf.constant(list_of_labels)
+        table = tf.contrib.lookup.index_to_string_table_from_tensor(class_tensor)
+        classes = table.lookup(tf.to_int64(indices))
         self._restore_checkpoint(sess, basename)
 
         return model, indices, values
@@ -221,9 +277,10 @@ class TaggerTensorFlowExporter(TensorFlowExporter):
                 raise Exception('Unknown attribute in signature: {}'.format(v))
 
         sig_input = predict_tensors
-        print(sig_input.keys())
         sig_output = SignatureOutput(classes, values)
-        return sig_input, sig_output, 'tag_text'
+        sig_name = 'tag_text'
+        assets = create_assets(basename, sig_input, sig_output, sig_name, model.lengths_key)
+        return sig_input, sig_output, sig_name, assets
 
 
 @exporter
@@ -310,7 +367,9 @@ class Seq2SeqTensorFlowExporter(TensorFlowExporter):
                 raise Exception('Unknown attribute in signature: {}'.format(v))
 
         sig_input = predict_tensors
-        print(sig_input)
         sig_output = SignatureOutput(classes, values)
-        return sig_input, sig_output, 'suggest_text'
+        sig_name, 'suggest_text'
+        assets = create_assets(basename, sig_input, sig_output, sig_name, self.length_key)
+
+        return sig_input, sig_output, sig_name, assets
         
