11f89832c64df71f0b81254802ad07bef3478a1b
Amy <ahemmeter@interactions.com>
2018-11-15 08:25:54 -0500

    updating for v1

--- python/addons/classify_sepcnn.py
+++ python/addons/classify_sepcnn.py
@@ -1,12 +1,12 @@
-from baseline.keras.classify import SequentialWordClassifierBase
+from baseline.keras.classify import GraphWordClassifierBase
 
 from keras.layers import (MaxPooling1D,
                           Dropout,
                           SeparableConv1D,
                           GlobalAveragePooling1D)
 
-
-class SepConvWordClassifier(SequentialWordClassifierBase):
+@register_model(task='classify', name='sepcnn')
+class SepConvWordClassifier(GraphWordClassifierBase):
     """
     Model defined in https://developers.google.com/machine-learning/guides/text-classification/step-4
     """
@@ -32,20 +32,20 @@ class SepConvWordClassifier(SequentialWordClassifierBase):
         poolsz = kwargs.get('poolsz', 2)
 
         for _ in range(blocks - 1):
-            self.impl.add(Dropout(rate=pdrop))
-            self.impl.add(SeparableConv1D(filters=cmotsz, kernel_size=filtsz, activation='relu',
+            drop1 = Dropout(rate=pdrop)(embed)
+            Sep1 = SeparableConv1D(filters=cmotsz, kernel_size=filtsz, activation='relu',
                                           bias_initializer='random_uniform', depthwise_initializer='random_uniform',
-                                          padding='same'))
-            self.impl.add(SeparableConv1D(filters=cmotsz, kernel_size=filtsz, activation='relu',
+                                          padding='same')(drop1)
+            Sep2 = SeparableConv1D(filters=cmotsz, kernel_size=filtsz, activation='relu',
                                           bias_initializer='random_uniform', depthwise_initializer='random_uniform',
-                                          padding='same'))
-            self.impl.add(MaxPooling1D(pool_size=poolsz))
+                                          padding='same')(Sep1)
+            maxpool = MaxPooling1D(pool_size=poolsz)(Sep2)
 
-        self.impl.add(SeparableConv1D(filters=cmotsz*2, kernel_size=filtsz, activation='relu',
+            Sep3 = SeparableConv1D(filters=cmotsz*2, kernel_size=filtsz, activation='relu',
                                       bias_initializer='random_uniform', depthwise_initializer='random_uniform',
-                                      padding='same'))
-        self.impl.add(GlobalAveragePooling1D())
-        self.impl.add(Dropout(rate=pdrop))
+                                      padding='same')(maxpool)
+            global_average_pooling = GlobalAveragePooling1D()(Sep3)
+            drop2 = Dropout(rate=pdrop)(global_average_pooling)
 
 
 def create_model(embeddings, labels, **kwargs):
