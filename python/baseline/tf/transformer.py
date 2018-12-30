908b21b7d391c84a28313ab64335ab9398e40904
dpressel <dpressel@gmail.com>
2018-12-29 21:53:30 -0500

    add all transformer layers and a CRF layer

--- python/baseline/tf/transformer.py
+++ python/baseline/tf/transformer.py
@@ -1,6 +1,14 @@
 import tensorflow as tf
 from baseline.tf.tfy import tf_activation, get_shape_as_list, layer_norm, time_distributed_projection
-from baseline.tf.layers import TRAIN_FLAG, FFN, split_heads, combine_heads, TransformerEncoder
+from baseline.tf.layers import (TRAIN_FLAG,
+                                FFN,
+                                split_heads,
+                                combine_heads,
+                                TransformerEncoder,
+                                TransformerEncoderStack,
+                                TransformerDecoder,
+                                TransformerDecoderStack)
+
 
 
 
@@ -63,41 +71,46 @@ def transformer_encoder(x, src_mask, scope, num_heads, pdrop, scale=True, activa
 
 
 def transformer_decoder(src, tgt, src_mask, tgt_mask, scope, num_heads, pdrop, scale=True, activation_type='relu', d_ff=None):
-    with tf.variable_scope(scope):
-        d_model = get_shape_as_list(tgt)[-1]
-        if d_ff is None:
-            d_ff = 4*d_model
 
-        tgt = layer_norm(tgt, 'ln_1')
+    d_model = get_shape_as_list(tgt)[-1]
+    return TransformerDecoder(d_model, num_heads, pdrop, scale, activation_type, d_ff)((src, tgt), TRAIN_FLAG(), (src_mask, tgt_mask))
 
-        q, k, v = self_attention_qkv(tgt, d_model)
-        self_attn = multi_headed_attention(q, k, v, 'self_attn', d_model, num_heads, pdrop, scale=scale, mask=tgt_mask)
-        tgt = tgt + tf.layers.dropout(self_attn, pdrop, training=TRAIN_FLAG())
-        tgt = layer_norm(tgt, 'ln_2')
 
-        q, k, v = low_order_projection_qkv(tgt, src, src, d_model)
-        # Mask at zeros???
-        src_attn = multi_headed_attention(q, k, v, "dual_attn", d_model, num_heads, pdrop, scale=scale, mask=src_mask)
-        tgt = tgt + tf.layers.dropout(src_attn, pdrop, training=TRAIN_FLAG())
-
-        tgt = layer_norm(tgt, 'ln_3')
-        m = ffn(tgt, 'ffn', pdrop, d_ff=d_ff, activation_type=activation_type)
-        h = tgt + tf.layers.dropout(m, pdrop, training=TRAIN_FLAG())
-        return h
+#def transformer_decoder(src, tgt, src_mask, tgt_mask, scope, num_heads, pdrop, scale=True, activation_type='relu', d_ff=None):
+#    with tf.variable_scope(scope):
+#        d_model = get_shape_as_list(tgt)[-1]
+#        if d_ff is None:
+#            d_ff = 4*d_model
+#        tgt = layer_norm(tgt, 'ln_1')
+#        q, k, v = self_attention_qkv(tgt, d_model)
+#        self_attn = multi_headed_attention(q, k, v, 'self_attn', d_model, num_heads, pdrop, scale=scale, mask=tgt_mask)
+#        tgt = tgt + tf.layers.dropout(self_attn, pdrop, training=TRAIN_FLAG())
+#        tgt = layer_norm(tgt, 'ln_2')
+#        q, k, v = low_order_projection_qkv(tgt, src, src, d_model)
+#        # Mask at zeros???
+#        src_attn = multi_headed_attention(q, k, v, "dual_attn", d_model, num_heads, pdrop, scale=scale, mask=src_mask)
+#        tgt = tgt + tf.layers.dropout(src_attn, pdrop, training=TRAIN_FLAG())
+#        tgt = layer_norm(tgt, 'ln_3')
+#        m = ffn(tgt, 'ffn', pdrop, d_ff=d_ff, activation_type=activation_type)
+#        h = tgt + tf.layers.dropout(m, pdrop, training=TRAIN_FLAG())
+#        return h
 
 
 def transformer_encoder_stack(x, src_mask, num_heads, pdrop, scale=True, layers=1, activation_type='relu', scope='TransformerEncoder', d_ff=None):
-    with tf.variable_scope(scope):
-        for i in range(layers):
-            x = transformer_encoder(x, src_mask, 'encoder-{}'.format(i), num_heads, pdrop, scale, activation_type, d_ff)
-    return layer_norm(x, 'ln_out')
+    d_model = get_shape_as_list(x)[-1]
+
+    return TransformerEncoderStack(d_model, num_heads, pdrop, scale, layers, activation_type, d_ff=d_ff, name=scope)(x, TRAIN_FLAG(), src_mask)
 
 
-def transformer_decoder_stack(src, tgt, src_mask, tgt_mask, num_heads, pdrop, scale=True, layers=1, activation_type='relu', scope='TransformerEncoder', d_ff=None):
-    with tf.variable_scope(scope):
-        for i in range(layers):
-            x = transformer_decoder(src, tgt, src_mask, tgt_mask, 'decoder-{}'.format(i), num_heads, pdrop, scale, activation_type, d_ff)
-    return layer_norm(x, 'ln_out')
+def transformer_decoder_stack(src, tgt, src_mask, tgt_mask, num_heads, pdrop, scale=True, layers=1, activation_type='relu', scope='TransformerDecoder', d_ff=None):
+    d_model = get_shape_as_list(tgt)[-1]
+    return TransformerDecoderStack(d_model, num_heads, pdrop, scale, layers, activation_type, d_ff=d_ff, name=scope)((src, tgt), TRAIN_FLAG(), (src_mask, tgt_mask))
+
+#def transformer_decoder_stack(src, tgt, src_mask, tgt_mask, num_heads, pdrop, scale=True, layers=1, activation_type='relu', scope='TransformerEncoder', d_ff=None):
+#    with tf.variable_scope(scope):
+#        for i in range(layers):
+#            x = transformer_decoder(src, tgt, src_mask, tgt_mask, 'decoder-{}'.format(i), num_heads, pdrop, scale, activation_type, d_ff)
+#    return layer_norm(x, 'ln_out')
 
 
 def subsequent_mask(size):
