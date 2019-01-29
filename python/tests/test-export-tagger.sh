ce36e67157b313bc6c5ce5ac1480b1be9d84f088
sagnik <schoudhury@interactions.com>
2019-01-29 17:46:36 -0500

    testing tagger export

--- python/tests/test-export-tagger.sh
+++ python/tests/test-export-tagger.sh
@@ -1,9 +1,11 @@
+#!/usr/bin/env bash
 BASELINE_DIR=${HOME}/dev/work/baseline
-TAGGER_MODEL=tagger-26231.zip
+TAGGER_MODEL=tagger-24429.zip
 TEST_FILE=eng.testb.small
 TEST_LOAD=$TEST_FILE.load
 TEST_SERVE=$TEST_FILE.serve
 TEST_SERVE_PREPROC=$TEST_FILE.serve_preproc
+NUM_FEATURES=3
 SLEEP=5
 
 docker stop tfserving
@@ -18,7 +20,7 @@ echo "running test for tagger"
 echo "------------------------"
 
 echo "downloading trained tagger model and test file"
-wget https://www.dropbox.com/s/bwjbmrcalwtjz4l/tagger-26231.zip?dl=1 -O $TAGGER_MODEL
+wget https://www.dropbox.com/s/86xzsofbgk3dzyj/tagger-24429.zip?dl=1 -O $TAGGER_MODEL
 wget https://www.dropbox.com/s/pp3pcq5q5ko1df6/eng.testb.small?dl=1 -O $TEST_FILE
 
 echo "tagging by loading the model"
@@ -29,7 +31,7 @@ echo "exporting w/o preproc"
 MDIR=models/conll
 rm -rf $MDIR
 mkdir -p $MDIR
-mead-export --config $BASELINE_DIR/python/mead/config/conll-single-tf.json --model $TAGGER_MODEL --is_remote false --output_dir $MDIR
+mead-export --config $BASELINE_DIR/python/mead/config/conll.json --model $TAGGER_MODEL --is_remote false --output_dir $MDIR
 sleep $SLEEP
 
 echo "running tf serving"
@@ -45,7 +47,7 @@ echo "exporting with preproc"
 MDIR=models-preproc/conll
 rm -rf $MDIR
 mkdir -p $MDIR
-mead-export --config $BASELINE_DIR/python/mead/config/conll-single-tf.json --model $TAGGER_MODEL --is_remote false --output_dir $MDIR --modules preproc-exporters --exporter_type preproc
+mead-export --config $BASELINE_DIR/python/mead/config/conll.json --model $TAGGER_MODEL --is_remote false --output_dir $MDIR --modules preproc-exporters preprocessors --exporter_type preproc
 sleep $SLEEP
 
 echo "running tf serving"
@@ -61,9 +63,11 @@ docker stop tfserving
 docker rm tfserving
 
 #remove prints coming from baseline
-sed -i -e 1,4d $TEST_LOAD 
-sed -i -e 1,3d $TEST_SERVE
-sed -i -e 1,3d $TEST_SERVE_PREPROC
+NUM_LINES_TO_REMOVE_LOAD=`expr "$NUM_FEATURES" + 2`
+NUM_LINES_TO_REMOVE_SERVE=`expr "$NUM_FEATURES" + 1`
+sed -i -e 1,${NUM_LINES_TO_REMOVE_LOAD}d $TEST_LOAD
+sed -i -e 1,${NUM_LINES_TO_REMOVE_SERVE}d $TEST_SERVE
+sed -i -e 1,${NUM_LINES_TO_REMOVE_SERVE}d $TEST_SERVE_PREPROC
 
 DIFF=$(diff ${TEST_LOAD} ${TEST_SERVE})
     if [ "$DIFF" != "" ]
@@ -81,4 +85,3 @@ DIFF=$(diff ${TEST_SERVE} ${TEST_SERVE_PREPROC})
 
 echo "tagger exporting successful"
 exit 0
-
