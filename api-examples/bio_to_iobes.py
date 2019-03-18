2641b3e95c0e12973bda090ad32d71d9178a9720
sagnik <schoudhury@interactions.com>
2019-03-18 15:58:45 -0400

    allow multiple fields in the driver program

--- api-examples/bio_to_iobes.py
+++ api-examples/bio_to_iobes.py
@@ -9,12 +9,15 @@ parser.add_argument('--train_file', help='Training file relative name', default=
 parser.add_argument('--valid_file', help='Validation file relative name', default='eng.testa')
 parser.add_argument('--test_file', help='Test file relative name', default='eng.testb')
 parser.add_argument('--suffix', help='Suffix to append', default='.iobes')
-parser.add_argument('--fields', help='fields to convert to iobes', default=[-1], nargs='+', required=False)
+parser.add_argument('--fields', help='fields to convert to iobes if you have multiple features', default=[-1], nargs='+', required=False)
 
 args = parser.parse_args()
 
 
-convert_bio_to_iobes(os.path.join(args.io_dir, args.train_file), os.path.join(args.io_dir, args.train_file + args.suffix))
-convert_bio_to_iobes(os.path.join(args.io_dir, args.valid_file), os.path.join(args.io_dir, args.valid_file + args.suffix))
-convert_bio_to_iobes(os.path.join(args.io_dir, args.test_file), os.path.join(args.io_dir, args.test_file + args.suffix))
+convert_bio_to_iobes(os.path.join(args.io_dir, args.train_file),
+                     os.path.join(args.io_dir, args.train_file + args.suffix), fields=[int(x) for x in args.fields])
+convert_bio_to_iobes(os.path.join(args.io_dir, args.valid_file),
+                     os.path.join(args.io_dir, args.valid_file + args.suffix), fields=[int(x) for x in args.fields])
+convert_bio_to_iobes(os.path.join(args.io_dir, args.test_file),
+                     os.path.join(args.io_dir, args.test_file + args.suffix), fields=[int(x) for x in args.fields])
 
