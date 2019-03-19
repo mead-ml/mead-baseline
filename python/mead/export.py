faef0da71a6632bf4f3725380e57fb943ccff5d1
Brian <blester125@gmail.com>
2019-03-19 11:09:07 -0400

    update export.py

--- python/mead/export.py
+++ python/mead/export.py
@@ -1,8 +1,6 @@
 import argparse
+from baseline.utils import read_config_file, unzip_model
 import mead
-from baseline.utils import unzip_model
-from baseline.utils import read_config_file
-from baseline.utils import str2bool
 from mead.exporters import create_exporter
 from mead.utils import convert_path, configure_logger, get_export_params
 
@@ -25,16 +23,16 @@ def main():
     parser.add_argument('--datasets', help='json library of dataset labels', default='config/datasets.json', type=convert_path)
     parser.add_argument('--logging', help='json file for logging', default='config/logging.json', type=convert_path)
     parser.add_argument('--task', help='task to run', choices=['classify', 'tagger', 'seq2seq', 'lm'])
-    parser.add_argument('--exporter_type', help='exporter type', default=None)
+    parser.add_argument('--exporter_type', help="exporter type (default 'default')", default=None)
     parser.add_argument('--return_labels', help='if true, the exported model returns actual labels else '
-                                                'the indices for labels vocab', default=None)
+                                                'the indices for labels vocab (default False)', default=None)
     parser.add_argument('--model', help='model name', required=True, type=unzip_model)
     parser.add_argument('--model_version', help='model_version', default=None)
-    parser.add_argument('--output_dir', help='output dir', default=None)
+    parser.add_argument('--output_dir', help="output dir (default './models')", default=None)
     parser.add_argument('--project', help='Name of project, used in path first', default=None)
     parser.add_argument('--name', help='Name of the model, used second in the path', default=None)
     parser.add_argument('--beam', help='beam_width', default=30, type=int)
-    parser.add_argument('--is_remote', help='if True, separate items for remote server and client. If False bundle everything together', default=True, type=str2bool)
+    parser.add_argument('--is_remote', help='if True, separate items for remote server and client. If False bundle everything together (default True)', default=None)
 
     args = parser.parse_args()
     configure_logger(args.logging)
@@ -50,19 +48,20 @@ def main():
 
     task = mead.Task.get_task_specific(task_name, args.settings)
 
-    output_dir, project, name, model_version, exporter_type, return_labels = get_export_params(
+    output_dir, project, name, model_version, exporter_type, return_labels, is_remote = get_export_params(
         config_params.get('export', {}),
         args.output_dir,
         args.project, args.name,
         args.model_version,
         args.exporter_type,
         args.return_labels,
+        args.is_remote,
     )
     task.read_config(config_params, args.datasets, exporter_type=exporter_type)
     feature_exporter_field_map = create_feature_exporter_field_map(config_params['features'])
     exporter = create_exporter(task, exporter_type, return_labels=return_labels,
                                feature_exporter_field_map=feature_exporter_field_map)
-    exporter.run(args.model, output_dir, project, name, model_version, remote=args.is_remote)
+    exporter.run(args.model, output_dir, project, name, model_version, remote=is_remote)
 
 
 if __name__ == "__main__":
