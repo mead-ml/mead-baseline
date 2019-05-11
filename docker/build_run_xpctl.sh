062d1e0b5033d1ba3be38eb19a31e7fcef202092
sagnik <schoudhury@interactions.com>
2019-05-11 19:50:07 -0400

    added docker capability

---
+++ docker/build_run_xpctl.sh
@@ -0,0 +1,78 @@
+usage() {
+    echo "Usage: $0
+    [-u|--user|--dbuser <db user> (required)]
+    [--pass|--dbpass <db password> (required)]
+    [--dbhost <db host> (default=localhost)]
+    [--dbport <db port> (default=27017)]
+    [-b|--backend <backend> (default=mongo)]
+    [-p|--port <port to run xpctl server> (default=5310)]" 1>&2; exit 1;
+}
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -u|--user|--dbuser)
+    DB_USER="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    --pass|--dbpass)
+    DB_PASSWORD="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    --dbhost)
+    DB_HOST="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    --dbport)
+    DB_PORT="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -b|--backend)
+    BACKEND="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -p|--port)
+    PORT="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -h|--help)
+    HELP="$2"
+    #usage
+    shift # past argument
+    ;;
+    *)    # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+DB_HOST=${DB_HOST:-localhost}
+DB_PORT=${DB_PORT:-27017}
+BACK_END=${BACK_END:-mongo}
+PORT=${PORT:5310}
+
+CON_BUILD=xpctlserver
+docker build \
+--network=host \
+--build-arg backend=${BACK_END} \
+--build-arg user=${DB_USER} \
+--build-arg passwd=${DB_PASSWORD} \
+--build-arg dbhost=${DB_HOST} \
+--build-arg dbport=${DB_PORT} \
+--build-arg port=${PORT} \
+-t ${CON_BUILD}-${BACK_END} \
+-f Dockerfile \
+../
+
+docker run -e LANG=C.UTF-8 --rm --name=${CON_BUILD} --network=host -it ${CON_BUILD}-${BACK_END}
--- python/xpctl/swagger_server/__main__.py
+++ python/xpctl/swagger_server/__main__.py
@@ -23,14 +23,27 @@ def main():
     parser = argparse.ArgumentParser(description='NLU unit')
     parser.add_argument('--ll', help='Log level', type=str, default='INFO')
     parser.add_argument('--backend', help='backend', type=str, default='mongo')
-    parser.add_argument('--cred', help='credential for backend', default=os.path.expanduser('~/xpctlcred-mongo-local.json'))
+    parser.add_argument('--cred', help='credential for backend',
+                        default=os.path.expanduser('~/xpctlcred-mongo-local.json'))
+    parser.add_argument('--user', help='user for backend')
+    parser.add_argument('--passwd', help='user for backend')
+    parser.add_argument('--dbhost', help='host for backend')
+    parser.add_argument('--dbport', help='port for backend')
     parser.add_argument('--port', help='port', default='5310')
     args = parser.parse_args()
     
     logging.basicConfig(level=get_logging_level(args.ll))
     
     app = connexion.App(__name__, specification_dir='./swagger/')
-    d = read_config_file(args.cred)
+    if args.user is not None:
+        d = {
+            'user': args.user,
+            'passwd': args.passwd,
+            'dbhost': args.dbhost,
+            'dbport': args.dbport
+        }
+    else:
+        d = read_config_file(args.cred)
     d.update({'dbtype': args.backend})
     app.app.backend = ExperimentRepo().create_repo(**d)
     app.app.json_encoder = encoder.JSONEncoder
