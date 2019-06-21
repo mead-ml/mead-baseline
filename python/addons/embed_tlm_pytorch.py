e88b6a1b8917a640440d7cec7d0855f9b8769cd3
dpressel <dpressel@gmail.com>
2019-06-20 20:44:21 -0400

    transformer LM embedding w TransformerEncoderStack
    This makes it easy to reload outputs from api-example
    transformer training and use for downstream embeddings
    or fine-tuning

---
+++ python/addons/embed_tlm_pytorch.py
@@ -0,0 +1,174 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from collections import Counter
+
+
+from baseline.utils import read_json
+from baseline.pytorch.transformer import TransformerEncoderStack
+from baseline.pytorch.embeddings import PositionalLookupTableEmbeddings
+from baseline.embeddings import register_embeddings
+from baseline.pytorch.embeddings import PyTorchEmbeddings
+from baseline.vectorizers import register_vectorizer, AbstractVectorizer
+from pytorch_pretrained_bert.tokenization import BertTokenizer
+from pytorch_pretrained_bert.modeling import BertModel
+from baseline.pytorch.torchy import *
+
+
+@register_vectorizer(name='tlm-subwords')
+class WordPieceVectorizer1D(AbstractVectorizer):
+    """Define a Baseline Vectorizer that can do WordPiece with BERT tokenizer
+
+    If you use tokens=subword, this vectorizer is used, and so then there is
+    a dependency on bert_pretrained_pytorch
+    """
+
+    def __init__(self, **kwargs):
+        """Loads a BertTokenizer using bert_pretrained_pytorch
+
+        :param kwargs:
+        """
+        super(WordPieceVectorizer1D, self).__init__(kwargs.get('transform_fn'))
+        from pytorch_pretrained_bert import BertTokenizer
+        self.max_seen = 128
+        handle = kwargs.get('embed_file')
+        self.tokenizer = BertTokenizer.from_pretrained(handle, do_lower_case=False)
+        self.mxlen = kwargs.get('mxlen', -1)
+
+    def count(self, tokens):
+        seen = 0
+        counter = Counter()
+        for tok in self.iterable(tokens):
+            counter[tok] += 1
+            seen += 1
+        self.max_seen = max(self.max_seen, seen)
+        return counter
+
+    def iterable(self, tokens):
+        for tok in tokens:
+            if tok == '<unk>':
+                yield '[UNK]'
+            elif tok == '<EOS>':
+                yield '[SEP]'
+            else:
+                for subtok in self.tokenizer.tokenize(tok):
+                    yield subtok
+
+    def _next_element(self, tokens, vocab):
+        for atom in self.iterable(tokens):
+            value = vocab.get(atom)
+            if value is None:
+                value = vocab['[UNK]']
+            yield value
+
+    def run(self, tokens, vocab):
+        if self.mxlen < 0:
+            self.mxlen = self.max_seen
+        vec1d = np.zeros(self.mxlen, dtype=np.long)
+        for i, atom in enumerate(self._next_element(tokens, vocab)):
+            if i == self.mxlen:
+                i -= 1
+                break
+            vec1d[i] = atom
+        valid_length = i + 1
+        return vec1d, valid_length
+
+    def get_dims(self):
+        return self.mxlen,
+
+
+@register_embeddings(name='tlm-words-embed')
+class TransformerLMEmbeddings(PyTorchEmbeddings):
+    """Support embeddings trained with the TransformerLanguageModel class
+
+    This method supports either subword or word embeddings, not characters
+
+    """
+    def __init__(self, name, **kwargs):
+        super(TransformerLMEmbeddings, self).__init__(name)
+        self.vocab = read_json(kwargs.get('vocab_file'))
+        self.vsz = len(self.vocab)
+        layers = int(kwargs.get('layers', 8))
+        num_heads = int(kwargs.get('num_heads', 10))
+        pdrop = kwargs.get('dropout', 0.0)
+        self.d_model = int(kwargs.get('dsz', kwargs.get('d_model', 410)))
+        d_ff = int(kwargs.get('d_ff', 2100))
+        x_embedding = PositionalLookupTableEmbeddings('pos', vsz=self.vsz, dsz=self.d_model)
+        self.init_embed({'x': x_embedding})
+        self.transformer = TransformerEncoderStack(num_heads, d_model=self.d_model, pdrop=pdrop, scale=True, layers=layers, d_ff=d_ff)
+
+    def embed(self, input):
+        embedded = self.embeddings['x'](input)
+        #all_embeddings = []
+        #for k in self.embeddings.keys():
+        #    embedding = self.embeddings[k]
+        #    all_embeddings.append(embedding.encode(input[k]))
+        #embedded = torch.cat(all_embeddings, -1)
+        embedded_dropout = self.embed_dropout(embedded)
+        if self.embeddings_proj:
+            embedded_dropout = self.embeddings_proj(embedded_dropout)
+        return embedded_dropout
+
+    def init_embed(self, embeddings, **kwargs):
+        pdrop = float(kwargs.get('dropout', 0.5))
+        self.embed_dropout = nn.Dropout(pdrop)
+        self.embeddings = EmbeddingsContainer()
+        input_sz = 0
+        for k, embedding in embeddings.items():
+
+            self.embeddings[k] = embedding
+            input_sz += embedding.get_dsz()
+
+        projsz = kwargs.get('projsz')
+        if projsz:
+            self.embeddings_proj = pytorch_linear(input_sz, projsz)
+            print('Applying a transform from {} to {}'.format(input_sz, projsz))
+            return projsz
+        else:
+            self.embeddings_proj = None
+        return input_sz
+
+    def forward(self, x):
+        input_mask = torch.zeros(x.shape, device=x.device, dtype=torch.long).masked_fill(x != 0, 1).unsqueeze(1).unsqueeze(1)
+        embedding = self.embed(x)
+        z = self.get_output(self.transformer(embedding, mask=input_mask))
+        return z
+
+    def get_output(self, z):
+        return z.detach()
+
+    def get_vocab(self):
+        return self.vocab
+
+    def get_vsz(self):
+        return self.vsz
+
+    def get_dsz(self):
+        return self.d_model
+
+    @classmethod
+    def load(cls, embeddings, **kwargs):
+        c = cls("tlm-words-embed", **kwargs)
+        c.load_state_dict(torch.load(embeddings), strict=False)
+        return c
+
+
+def _mean_pool(embeddings):
+    return torch.mean(embeddings, 1, False)
+
+
+def _max_pool(embeddings):
+    return torch.max(embeddings, 1, False)[0]
+
+
+@register_embeddings(name='tlm-words-embed-pooled')
+class TransformerLMPooledEmbeddings(TransformerLMEmbeddings):
+
+    def __init__(self, name, **kwargs):
+        super(TransformerLMPooledEmbeddings, self).__init__(name=name, **kwargs)
+        pooling = kwargs.get('pooling', 'mean')
+
+        self.pooling_op = _max_pool if pooling == 'max' else _mean_pool
+
+    def get_output(self, z):
+        return self.pooling_op(z)
