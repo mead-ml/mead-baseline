49998a4a688f568a5afabe96d7cda99a73155127
Brian <blester125@gmail.com>
2019-08-20 11:43:17 -0400

    chore: move hpctl to mead-ml

--- python/hpctl/tests/__init__.py
+++
@@ -1,9 +0,0 @@
-import random
-import string
-
-chars = list(string.ascii_letters + string.digits)
-
-def r_str(length=None, min_=5, max_=20):
-    if length is None:
-        length = random.randint(min_, max_)
-    return ''.join([random.choice(chars) for _ in range(length)])
