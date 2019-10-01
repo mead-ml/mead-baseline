f3a37f6dbeea1e40fc2b4d1223dd53d2cf7a5935
wliu <wliu@interactions.com>
2019-10-01 10:28:43 -0400

    wordpiece1d in custom_vectorizer independent of embed_bert

--- python/addons/custom_vectorizers.py
+++ python/addons/custom_vectorizers.py
@@ -1,13 +1,42 @@
 import numpy as np
 from collections import Counter
 from baseline.utils import Offsets
-from embed_bert import WordpieceTokenizer, WordPieceVectorizer1D
+# from embed_bert import WordpieceTokenizer, WordPieceVectorizer1D
 from baseline.vectorizers import register_vectorizer, AbstractVectorizer
 
 
-@register_vectorizer(name='wordpiece1dlm')
-class WordPieceVectorizer1DLM(WordPieceVectorizer1D):
-    # override iterable function for continuous sequence data (typically for lm)
+@register_vectorizer(name='wordpiece1d')
+class WordPieceVectorizer1D(AbstractVectorizer):
+    """Define a Baseline Vectorizer that can do WordPiece with BERT tokenizer
+
+    this vectorizer has a dependency on bert_pretrained_pytorch
+    """
+
+    def __init__(self, pytorch_pretrained_bert=None, **kwargs):
+        """Loads a BertTokenizer using bert_pretrained_pytorch
+
+        :param kwargs:
+        """
+        super(WordPieceVectorizer1D, self).__init__(kwargs.get('transform_fn'))
+        from pytorch_pretrained_bert import BertTokenizer
+        self.max_seen = 128
+        handle = kwargs.get('embed_file', 'bert-base-cased')
+        self.tokenizer = BertTokenizer.from_pretrained(handle, do_lower_case=False)
+        self.mxlen = kwargs.get('mxlen', -1)
+
+    @property
+    def vocab(self):
+        return self.tokenizer.vocab
+
+    def count(self, tokens):
+        seen = 0
+        counter = Counter()
+        for tok in self.iterable(tokens):
+            counter[tok] += 1
+            seen += 1
+        self.max_seen = max(self.max_seen, seen)
+        return counter
+
     def iterable(self, tokens):
         for tok in tokens:
             if tok == '<unk>':
@@ -15,16 +44,37 @@ class WordPieceVectorizer1DLM(WordPieceVectorizer1D):
             elif tok == '<EOS>':
                 yield '[SEP]'
             else:
-                for subtok in self.wordpiece_tok.tokenize(tok):
+                for subtok in self.tokenizer.tokenize(tok):
                     yield subtok
+
+    def _next_element(self, tokens, vocab):
+        for atom in self.iterable(tokens):
+            value = vocab.get(atom)
+            if value is None:
+                value = vocab['[UNK]']
+            yield value
+
+    def run(self, tokens, vocab):
+        if self.mxlen < 0:
+            self.mxlen = self.max_seen
+        vec1d = np.zeros(self.mxlen, dtype=np.long)
+        for i, atom in enumerate(self._next_element(tokens, vocab)):
+            if i == self.mxlen:
+                i -= 1
+                break
+            vec1d[i] = atom
+        valid_length = i + 1
+        return vec1d, valid_length
+
+    def get_dims(self):
+        return self.mxlen,
 
 
 @register_vectorizer(name='bpe1d')
 class BPEVectorizer1D(AbstractVectorizer):
     """Define a Baseline Vectorizer for BPE using fastBPE (https://github.com/glample/fastBPE)
 
-    If you use tokens=bpe, this vectorizer is used, and so then there is a
-    dependency on fastBPE
+    this vectorizer has a dependency on fastBPE
 
     To use BPE, we assume that a Dictionary of codes and vocab was already created
 
