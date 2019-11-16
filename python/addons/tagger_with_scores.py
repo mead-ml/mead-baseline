e89eb18527f7d0883b92c6a0b86ad9aa96a0407b
Brian <blester125@gmail.com>
2019-11-15 19:01:51 -0500

    seperate service and tagger

--- python/addons/tagger_with_scores.py
+++ python/addons/tagger_with_scores.py
@@ -2,7 +2,6 @@ import tensorflow as tf
 from baseline.utils import get_version
 from baseline.model import register_model
 from baseline.tf.tagger.model import RNNTaggerModel
-from baseline.services import TaggerService
 
 
 @register_model(task='tagger', name='with-scores')
@@ -18,21 +17,3 @@ class RNNTaggerWithScores(RNNTaggerModel):
         assert self.impl.path_scores is not None, "Sequence scores are not being calculated, you need to use a CRF or constrained decoding to do this."
         scores = self.impl.path_scores
         return bestv, scores.numpy()
-
-
-class TaggerWithScoresService(TaggerService):
-
-    def predict(self, tokens, **kwargs):
-        export_mapping = kwargs.get('export_mapping', {})  # if empty dict argument was passed
-        if not export_mapping:
-            export_mapping = {'tokens': 'text'}
-        label_field = kwargs.get('label', 'label')
-        tokens_batch = self.batch_input(tokens)
-        self.prepare_vectorizers(tokens_batch)
-        examples = self.vectorize(tokens_batch)
-        outcomes = self.model.predict_with_scores(examples)
-        return self.format_output(outcomes, tokens_batch=tokens_batch, label_field=label_field)
-    def format_output(self, predicted, **kwargs):
-        predicted, scores = predicted
-        paths = super().format_output(predicted, **kwargs)
-        return paths, scores
