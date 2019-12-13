fix: bug in masking indices

api-examples/pretrain-transformer-lm.py
@@ -675,11 +675,11 @@ def train():
                 labels[~masked_indices] = 0
                 # Of the masked items, mask 80% of them with [MASK]
                 indices_replaced = torch.bernoulli(torch.full(labels.shape, 0.8)).type(torch.bool) & masked_indices
-                inputs[indices_replaced] = mask_value
+                inputs['x'][indices_replaced] = mask_value
                 # Replace 10% of them with random words, rest preserved for auto-encoding
                 indices_random = torch.bernoulli(torch.full(labels.shape, 0.5)).type(torch.bool) & masked_indices & ~indices_replaced
                 random_words = torch.randint(vocab_size, labels.shape, dtype=torch.long, device=args.device)
-                inputs[indices_random] = random_words[indices_random]
+                inputs['x'][indices_random] = random_words[indices_random]
 
             labels = labels.transpose(0, 1).contiguous()
             logits = model(inputs, None)[0].transpose(0, 1).contiguous()
@@ -724,11 +724,11 @@ def train():
                     labels[~masked_indices] = 0
                     # Of the masked items, mask 80% of them with [MASK]
                     indices_replaced = torch.bernoulli(torch.full(labels.shape, 0.8)).type(torch.bool) & masked_indices
-                    inputs[indices_replaced] = mask_value
+                    inputs['x'][indices_replaced] = mask_value
                     # Replace 10% of them with random work
                     indices_random = torch.bernoulli(torch.full(labels.shape, 0.5)).type(torch.bool) & masked_indices & ~indices_replaced
                     random_words = torch.randint(vocab_size, labels.shape, dtype=torch.long, device=args.device)
-                    inputs[indices_random] = random_words[indices_random]
+                    inputs['x'][indices_random] = random_words[indices_random]
 
                 labels = labels.transpose(0, 1).contiguous()
                 logits = model(inputs, None)[0].transpose(0, 1).contiguous()