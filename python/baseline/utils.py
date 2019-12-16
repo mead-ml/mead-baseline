0b8e2585d70ddb16fa9a6e00d5716f611532fc6f
Brian <blester125@gmail.com>
2019-12-16 13:33:39 -0500

    fix: load addons from files directly to allow for same names as baseline addons

--- python/baseline/utils.py
+++ python/baseline/utils.py
@@ -614,8 +614,12 @@ def import_user_module(module_name):
     addon_path = os.path.dirname(os.path.realpath(addons.__file__))
     _idempotent_append(addon_path, sys.path)
     if module_name.endswith(".py"):
-        module_name, module_dir = _parse_module_as_path(module_name)
-        _idempotent_append(module_dir, sys.path)
+        module_path = module_name
+        module_name, _ = _parse_module_as_path(module_path)
+        spec = importlib.util.spec_from_file_location(module_name, module_path)
+        mod = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(mod)
+        return mod
     mod = importlib.import_module(module_name)
     return mod
 
