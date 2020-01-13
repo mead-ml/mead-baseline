12da3fe3227c6aeacd9c177940091a8c7eb4320e
dpressel <dpressel@gmail.com>
2020-01-13 10:00:55 -0500

    Fixed busted import

--- api-examples/pretrain-transformer-lm.py
+++ api-examples/pretrain-transformer-lm.py
@@ -6,7 +6,7 @@ import tempfile
 import baseline
 from torch.nn.parallel import DistributedDataParallel
 from torch.utils.data import DataLoader, TensorDataset
-from eight_mile.utils import str2bool, write_json
+from eight_mile.utils import str2bool, write_json, Offsets
 import baseline.pytorch.embeddings
 import eight_mile.embeddings
 from eight_mile.optz import *
