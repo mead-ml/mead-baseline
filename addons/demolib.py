cca79d830194dede8eea3d3ecd7d486e2dc70399
dpressel <dpressel@gmail.com>
2020-02-04 15:56:18 -0500

    updates to demolib for v2

--- addons/demolib.py
+++ addons/demolib.py
@@ -1,59 +1,63 @@
-from eight_mile.embeddings import register_embeddings
-from eight_mile.tf.embeddings import TensorFlowEmbeddings
-from eight_mile.tf.optz import optimizer
-from baseline.confusion import ConfusionMatrix
-
+from baseline.embeddings import register_embeddings
+from baseline.tf.embeddings import TensorFlowEmbeddingsModel, TensorFlowEmbeddings, LookupTableEmbeddings
+from eight_mile.tf.layers import TRAIN_FLAG, MeanPool1D
 from baseline.reporting import register_reporting, ReportingHook
 from baseline.train import create_trainer, register_trainer, register_training_func, Trainer
-from baseline.utils import get_metric_cmp, get_model_file, color, Colors
-from baseline.tf.tfy import embed
+from baseline.utils import get_metric_cmp, get_model_file, color, Colors, listify, Offsets
 import tensorflow as tf
 import os
 import numpy as np
 
 
-@register_embeddings(name='cbow')
-class CharBoWEmbeddings(TensorFlowEmbeddings):
-    """Bag of character embeddings, sum char embeds, so in this case `wsz == dsz`
+class CharNBoWEmbeddings(TensorFlowEmbeddings):
+    def __init__(self, trainable=True, name=None, dtype=tf.float32, **kwargs):
+        trainable = kwargs.get("finetune", trainable)
+        super().__init__(trainable=trainable, name=name, dtype=dtype)
+        self.scope = kwargs.get("scope", "CharLUT")
+        self.finetune = kwargs.get("finetune", trainable)
 
-    """
+        self.pdrop = kwargs.get("pdrop", 0.5)
+        self.x = None
+        self.embed = LookupTableEmbeddings(name=f"{self.name}/CharLUT", finetune=self.finetune, **kwargs)
+        #self.pool = MeanPool1D(self.get_dsz(), )
+
+    def encode(self, x):
+        self.x = x
+        shape = tf.shape(x)
+        B = shape[0]
+        T = shape[1]
+        W = shape[2]
+        flat_chars = tf.reshape(x, [-1, W])
+        embed_chars = self.embed(flat_chars)
+
+        # Calculate the lengths of each word
+        # You can use layers.MeanPool1D() for this as well
+        # (BxT)
+        word_lengths = tf.reduce_sum(tf.cast(tf.not_equal(flat_chars, Offsets.PAD), tf.int32), axis=1)
+        #result = self.pool((embed_chars, word_lengths))
+        # (BxT)xD
+        result = tf.reduce_mean(embed_chars, axis=1)
+        return tf.reshape(result, (B, T, -1))
+
+    def call(self, inputs):
+        return self.encode(inputs)
+
+    def get_vsz(self):
+        return self.embed.get_vsz()
+
+    def get_dsz(self):
+        return self.embed.get_dsz()
+
+
+@register_embeddings(name='cbow')
+class CharConvEmbeddingsModel(TensorFlowEmbeddingsModel):
+    def __init__(self, name=None, **kwargs):
+        super().__init__(name, **kwargs)
+        self.embedding_layer = CharNBoWEmbeddings(name=self._name, **kwargs)
+
     @classmethod
     def create_placeholder(cls, name):
         return tf.compat.v1.placeholder(tf.int32, [None, None, None], name=name)
-
-    def __init__(self, name, **kwargs):
-        super(CharBoWEmbeddings, self).__init__()
-        self.vsz = kwargs.get('vsz')
-        self.dsz = kwargs.get('dsz')
-        self.finetune = kwargs.get('finetune', True)
-        self.name = name
-        self.scope = kwargs.get('scope', '{}/CharBoWLUT'.format(self.name))
-        self.weights = kwargs.get('weights')
-        if self.weights is None:
-            unif = kwargs.get('unif', 0.1)
-            self.weights = np.random.uniform(-unif, unif, (self.vsz, self.dsz))
-        self.params = kwargs
-
-    def save_md(self, target):
-        write_json({'vsz': self.get_vsz(), 'dsz': self.get_dsz()}, target)
-
-    def encode(self, x=None):
-        if x is None:
-            x = CharBoWEmbeddings.create_placeholder(self.name)
-        self.x = x
-        return tf.reduce_sum(embed(x,
-                                   self.get_vsz(),
-                                   self.get_dsz(),
-                                   tf.constant_initializer(self.weights, dtype=tf.float32),
-                                   self.finetune,
-                                   self.scope), axis=2, keepdims=False)
-
-    def get_vsz(self):
-        return self.vsz
-
-    # Warning this function is only initialized AFTER encode
-    def get_dsz(self):
-        return self.dsz
 
 
 @register_reporting(name='slack')
@@ -84,11 +88,11 @@ class SlackReporting(ReportingHook):
 
 
 @register_training_func('classify', name='test_every_n_epochs')
-def train(model, ts, vs, es=None, **kwargs):
+def custom_fit_function(model_params, ts, vs, es=None, **kwargs):
     """
     Train a classifier using TensorFlow
 
-    :param model: The model to train
+    :param model_params: The model_params or model to train
     :param ts: A training data set
     :param vs: A validation data set
     :param es: A test data set, can be None
@@ -120,24 +124,20 @@ def train(model, ts, vs, es=None, **kwargs):
 
     reporting_fns = listify(kwargs.get('reporting', []))
     print('reporting', reporting_fns)
+    TRAIN_FLAG()
 
-    trainer = create_trainer(model, **kwargs)
-    tables = tf.compat.v1.tables_initializer()
-    model.sess.run(tables)
-    model.sess.run(tf.compat.v1.global_variables_initializer())
-    model.set_saver(tf.compat.v1.train.Saver())
-
+    trainer = create_trainer(model_params, **kwargs)
     max_metric = 0
     last_improved = 0
 
     for epoch in range(epochs):
 
-        trainer.train(ts, reporting_fns)
-        test_metrics = trainer.test(vs, reporting_fns, phase='Valid')
+        trainer.train(ts, reporting_fns, dataset=False)
+        test_metrics = trainer.test(vs, reporting_fns, phase='Valid', dataset=False)
 
         if epoch > 0 and epoch % n == 0 and epoch < epochs - 1:
             print(color('Running test', Colors.GREEN))
-            trainer.test(es, reporting_fns, phase='Test')
+            trainer.test(es, reporting_fns, phase='Test', dataset=False)
 
         if do_early_stopping is False:
             trainer.checkpoint()
@@ -160,4 +160,4 @@ def train(model, ts, vs, es=None, **kwargs):
     if es is not None:
         print(color('Reloading best checkpoint', Colors.GREEN))
         trainer.recover_last_checkpoint()
-        trainer.test(es, reporting_fns, phase='Test')
+        trainer.test(es, reporting_fns, phase='Test', dataset=False)
