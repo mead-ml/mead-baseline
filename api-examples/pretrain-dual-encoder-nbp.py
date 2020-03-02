50c96a75bb72caab39e1b35918e1f6f00ef4eb0a
dpressel <dpressel@gmail.com>
2020-03-02 13:58:37 -0500

    added paired utils and update single source dual

--- api-examples/pretrain-dual-encoder-nbp.py
+++ api-examples/pretrain-dual-encoder-nbp.py
@@ -5,21 +5,15 @@ from argparse import ArgumentParser
 import tempfile
 import baseline
 from torch.nn.parallel import DistributedDataParallel
-from torch.utils.data import DataLoader, TensorDataset
-from eight_mile.utils import str2bool, write_json, Offsets
+from torch.utils.data import DataLoader
 from eight_mile.pytorch.layers import *
 import baseline.pytorch.embeddings
 import baseline.embeddings
+from paired_utils import *
 from eight_mile.optz import *
 from eight_mile.pytorch.optz import *
-from eight_mile.pytorch.serialize import save_tlm_npz
-from baseline.pytorch.lm import TransformerLanguageModel
-from baseline.vectorizers import Char2DVectorizer, Token1DVectorizer, AbstractVectorizer, BPEVectorizer1D
 from baseline.utils import DataDownloader
-import numpy as np
-import codecs
-from collections import Counter
-from baseline.pytorch.torchy import vec_log_sum_exp
+
 
 logger = logging.getLogger(__file__)
 
@@ -73,210 +67,12 @@ DATASETS = {
 
 
 
-class TripletLoss(nn.Module):
-    """Provide a Triplet Loss using the reversed batch for negatives"""
-    def __init__(self, model):
-        super().__init__()
-        self.score = nn.CosineSimilarity(dim=1)
-        self.model = model
-
-    def forward(self, inputs, targets):
-        # reverse the batch and use as a negative example
-        neg = targets.flip(0)
-        query = self.model.encode_query(inputs)
-        response = self.model.encode_response(targets)
-        neg_response = self.model.encode_response(neg)
-        pos_score = self.score(query, response)
-        neg_score = self.score(query, neg_response)
-        score = neg_score - pos_score
-        score = score.masked_fill(score < 0.0, 0.0).sum(0)
-        return score
-
-
-class AllLoss(nn.Module):
-    def __init__(self, model):
-        r"""Loss from here https://arxiv.org/pdf/1705.00652.pdf see section 4
-
-        We want to minimize the negative log prob of y given x
-
-        -log P(y|x)
-
-        P(y|x) P(x) = P(x, y)                             Chain Rule of Probability
-        P(y|x) = P(x, y) / P(x)                           Algebra
-        P(y|x) = P(x, y) / \sum_\hat(y) P(x, y = \hat(y)) Marginalize over all possible ys to get the probability of x
-        P_approx(y|x) = P(x, y) / \sum_i^k P(x, y_k)      Approximate the Marginalization by just using the ys in the batch
-
-        S(x, y) is the score (cosine similarity between x and y in this case) from our neural network
-        P(x, y) = e^S(x, y)
-
-        P(y|x) = e^S(x, y) / \sum_i^k e^S(x, y_k)
-        log P(y|x) = log( e^S(x, y) / \sum_i^k e^S(x, y_k))
-        log P(y|x) = S(x, y) - log \sum_i^k e^S(x, y_k)
-        -log P(y|x) = -(S(x, y) - log \sum_i^k e^S(x, y_k))
-        """
-        super().__init__()
-        self.score = nn.CosineSimilarity(dim=-1)
-        self.model = model
-
-    def forward(self, inputs, targets):
-        # These will get broadcast to [B, B, H]
-        query = self.model.encode_query(inputs).unsqueeze(1)  # [B, 1, H]
-        response = self.model.encode_response(targets).unsqueeze(0)  # [1, B, H]
-        # all_scores is now a batch x batch matrix where index (i, j) is the score between
-        # the i^th x vector and the j^th y vector
-        all_score = self.score(query, response) # [B, B]
-        # The diagonal has the scores of correct pair, (i, i)
-        pos_score = torch.diag(all_score)
-        # vec_log_sum_exp will calculate the batched log_sum_exp in a numerically stable way
-        # the result is a [B, 1] vector which we squeeze to make it [B] to match the diag
-        # Because we are minimizing the negative log we turned the division into a subtraction here
-        loss = pos_score - vec_log_sum_exp(all_score, -1).squeeze()
-        # Batch loss
-        loss = torch.sum(loss)
-        # minimize the negative loss
-        return -loss
-
-
-class PairedModel(nn.Module):
-
-    def __init__(self, embeddings, d_model, d_ff, dropout, num_heads, num_layers, stacking_layers=None, d_out=512, d_k=64, weight_std=0.02):
-        super().__init__()
-        if stacking_layers is None:
-            stacking_layers = [d_model] * 3
-
-        self.weight_std = weight_std
-        stacking_layers = listify(stacking_layers)
-        transformer = TransformerEncoderStack(num_heads=num_heads, d_model=d_model,
-                                              pdrop=dropout, layers=num_layers, activation='gelu', d_ff=d_ff, d_k=d_k)
-        self.attention_layer = MultiHeadedAttention(2, d_model, dropout, scale=True, d_k=d_k)
-        self.transformer_layers = transformer
-        self.embedding_layers = embeddings
-        self.ff1 = DenseStack(d_model, stacking_layers + [d_out], activation='gelu')
-        self.ff2 = DenseStack(d_model, stacking_layers + [d_out], activation='gelu')
-        self.apply(self.init_layer_weights)
-
-    def init_layer_weights(self, module):
-        if isinstance(module, (nn.Linear, nn.Embedding, nn.LayerNorm)):
-            module.weight.data.normal_(mean=0.0, std=self.weight_std)
-        if isinstance(module, (nn.Linear, nn.LayerNorm)) and module.bias is not None:
-            module.bias.data.zero_()
-
-    def encode_query(self, query):
-        query_mask = (query != Offsets.PAD)
-        query_length = query_mask.sum(-1)
-        query_mask = query_mask.unsqueeze(1).unsqueeze(1)
-        embedded = self.embedding_layers(query)
-        encoded_query = self.transformer_layers((embedded, query_mask))
-        encoded_query = self.attention_layer((encoded_query, encoded_query, encoded_query, query_mask))
-        encoded_query = encoded_query.sum(1) / query_length.float().unsqueeze(1)
-        encoded_query = self.ff1(encoded_query)
-        return encoded_query
-
-    def encode_response(self, response):
-        response_mask = (response != Offsets.PAD)
-        response_length = response_mask.sum(-1)
-        response_mask = response_mask.unsqueeze(1).unsqueeze(1)
-        embedded = self.embedding_layers(response)
-        encoded_response = self.transformer_layers((embedded, response_mask))
-        encoded_response = self.attention_layer((encoded_response, encoded_response, encoded_response, response_mask))
-        encoded_response = encoded_response.sum(1) / response_length.float().unsqueeze(1)
-        encoded_response = self.ff2(encoded_response)
-
-        return encoded_response
-
-    def forward(self, query, response):
-        encoded_query = self.encode_query(query)
-        encoded_response = self.encode_response(response)
-        return encoded_query, encoded_response
-
-    def create_loss(self, loss_type='all'):
-        if loss_type == 'all':
-            return AllLoss(self)
-        return TripletLoss(self)
-
 def create_model(embeddings, d_model, d_ff, dropout, num_heads, num_layers):
 
     model = PairedModel(embeddings, d_model, d_ff, dropout, num_heads, num_layers)
     logger.info(model)
     return model
 
-
-class TensorDatasetReaderBase(object):
-    """Provide a base-class to do operations that are independent of token representation
-    """
-    def __init__(self, nctx, vectorizers):
-        self.vectorizers = vectorizers
-        self.nctx = nctx
-        self.num_words = {}
-
-    def build_vocab(self, files):
-        vocabs = {k: Counter() for k in self.vectorizers.keys()}
-
-        for file in files:
-            if file is None:
-                continue
-            self.num_words[file] = 0
-            with codecs.open(file, encoding='utf-8', mode='r') as f:
-                sentences = []
-                for line in f:
-                    split_sentence = line.split() + ['<EOS>']
-                    self.num_words[file] += len(split_sentence)
-                    sentences += split_sentence
-                for k, vectorizer in self.vectorizers.items():
-                    vocabs[k].update(vectorizer.count(sentences))
-        return vocabs
-
-    def load_features(self, filename, vocabs):
-
-        features = dict()
-        with codecs.open(filename, encoding='utf-8', mode='r') as f:
-            sentences = []
-            for line in f:
-                sentences += line.strip().split() + ['<EOS>']
-            for k, vectorizer in self.vectorizers.items():
-                vec, valid_lengths = vectorizer.run(sentences, vocabs[k])
-                features[k] = vec[:valid_lengths]
-                shp = list(vectorizer.get_dims())
-                shp[0] = valid_lengths
-                features['{}_dims'.format(k)] = tuple(shp)
-        return features
-
-
-class TensorWordDatasetReader(TensorDatasetReaderBase):
-    """Read each word, and produce a tensor of x and y that are identical
-    """
-    def __init__(self, nctx: int, use_subword: bool = False, model_file: str = None, vocab_file: str = None):
-        """Create a reader with a context window that reads words
-
-        :param nctx: The context window length
-        :param use_subword: If true, use BPE, else words
-        """
-        self.use_subword = use_subword
-        if self.use_subword:
-            vectorizer = BPEVectorizer1D(model_file=model_file, vocab_file=vocab_file)
-        else:
-            vectorizer = Token1DVectorizer(transform_fn=baseline.lowercase)
-        super().__init__(nctx, {'x': vectorizer})
-
-    def build_vocab(self, files):
-        """Read the vocab file to get the tokens
-
-        :param files:
-        :return:
-        """
-        if self.use_subword:
-            super().build_vocab(files)
-            return {'x': self.vectorizers['x'].vocab}
-        return super().build_vocab(files)
-
-    def load(self, filename, vocabs):
-        features = self.load_features(filename, vocabs)
-        x_tensor = torch.tensor(features['x'], dtype=torch.long)
-        batch_width = self.nctx * 2
-        num_sequences_word = (x_tensor.size(0) // batch_width) * batch_width
-        x_tensor = x_tensor.narrow(0, 0, num_sequences_word).view(-1, batch_width)
-        # Take the first half for x_tensor, and the second half for y_tensor
-        return TensorDataset(x_tensor[:, :self.nctx], x_tensor[:, self.nctx:])
 
 
 def load_data(token_type, reader, dataset, file_key, vocabs, caching):
@@ -296,10 +92,10 @@ def create_reader(token_type, nctx, subword_model_file, subword_vocab_file):
         raise NotImplementedError("We do not currently support char tokens")
     if token_type == "words":
         logger.info("Using word input")
-        reader = TensorWordDatasetReader(nctx)
+        reader = SingleSourceTensorWordDatasetReader(nctx)
     else:
         logger.info("Using subword ({}) input".format(token_type))
-        reader = TensorWordDatasetReader(nctx, True, subword_model_file, subword_vocab_file)
+        reader = SingleSourceTensorWordDatasetReader(nctx, True, subword_model_file, subword_vocab_file)
     return reader
 
 
@@ -355,7 +151,7 @@ def train():
     parser.add_argument("--dataset_cache", type=str, default=os.path.expanduser('~/.bl-data'),
                         help="Path or url of the dataset cache")
     parser.add_argument("--cache_features", type=str2bool, default=True)
-    parser.add_argument("--embed_type", type=str, default='learned-positional',
+    parser.add_argument("--embed_type", type=str, default='positional',
                         help="register label of the embeddings, so far support positional or learned-positional")
     parser.add_argument("--d_model", type=int, default=410, help="Model dimension (and embedding dsz)")
     parser.add_argument("--d_ff", type=int, default=2100, help="FFN dimension")
@@ -367,8 +163,9 @@ def train():
     parser.add_argument("--subword_model_file", type=str, help="If using subwords, pass this")
     parser.add_argument("--subword_vocab_file", type=str, help="If using subwords with separate vocab file, pass here")
     parser.add_argument("--dropout", type=float, default=0.1, help="Dropout")
-    parser.add_argument("--lr", type=float, default=3e-4, help="Learning rate")
-    parser.add_argument("--clip", type=float, default=0.25, help="Clipping gradient norm")
+    parser.add_argument("--lr", type=float, default=1e-3, help="Learning rate")
+    parser.add_argument("--optim", default="adam")
+    parser.add_argument("--clip", type=float, default=1.0, help="Clipping gradient norm")
     parser.add_argument("--weight_decay", type=float, default=0.0, help="Weight decay")
     parser.add_argument("--epochs", type=int, default=20, help="Num training epochs")
     parser.add_argument("--restart_from", type=str, help="Option allows you to restart from a previous checkpoint")
@@ -480,7 +277,7 @@ def train():
         global_step = (start_epoch+1) * steps_per_epoch
         logger.info("Restarting from a previous checkpoint %s.\n\tStarting at global_step=%d, epoch=%d",
                     args.restart_from, global_step, start_epoch+1)
-    optimizer = OptimizerManager(model, global_step, optim='adam', lr=args.lr, lr_function=lr_sched, weight_decay=args.weight_decay)
+    optimizer = OptimizerManager(model, global_step, optim=args.optim, lr=args.lr, lr_function=lr_sched, weight_decay=args.weight_decay)
     logger.info("Model has {:,} parameters".format(sum(p.numel() for p in model.parameters() if p.requires_grad)))
 
     # Prepare model for distributed training if needed
