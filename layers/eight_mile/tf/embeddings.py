9baee22ad66fa3380a0b4a011a8521ebcd989f65
dpressel <dpressel@gmail.com>
2020-04-17 13:02:28 -0400

    remove old T2T impl since we arent using it

--- layers/eight_mile/tf/embeddings.py
+++ layers/eight_mile/tf/embeddings.py
@@ -309,44 +309,6 @@ class CharTransformerEmbeddings(TensorFlowEmbeddings):
         return self.embed.get_vsz()
 
 
-def get_timing_signal_1d(length, channels, min_timescale=1.0, max_timescale=1.0e4, start_index=0):
-    """Gets a bunch of sinusoids of different frequencies.
-    Each channel of the input Tensor is incremented by a sinusoid of a different
-    frequency and phase.
-    This allows attention to learn to use absolute and relative positions.
-    Timing signals should be added to some precursors of both the query and the
-    memory inputs to attention.
-    The use of relative position is possible because sin(x+y) and cos(x+y) can be
-    expressed in terms of y, sin(x) and cos(x).
-    In particular, we use a geometric sequence of timescales starting with
-    min_timescale and ending with max_timescale.  The number of different
-    timescales is equal to channels / 2. For each timescale, we
-    generate the two sinusoidal signals sin(timestep/timescale) and
-    cos(timestep/timescale).  All of these sinusoids are concatenated in
-    the channels dimension.
-    Args:
-      length: scalar, length of timing signal sequence.
-      channels: scalar, size of timing embeddings to create. The number of
-          different timescales is equal to channels / 2.
-      min_timescale: a float
-      max_timescale: a float
-      start_index: index of first position
-    Returns:
-      a Tensor of timing signals [1, length, channels]
-    """
-    position = tf.cast(tf.range(length) + start_index, tf.float32)
-    num_timescales = channels // 2
-    log_timescale_increment = math.log(float(max_timescale) / float(min_timescale)) / tf.maximum(
-        tf.cast(num_timescales, tf.float32) - 1, 1
-    )
-    inv_timescales = min_timescale * tf.exp(tf.cast(tf.range(num_timescales), tf.float32) * -log_timescale_increment)
-    scaled_time = tf.expand_dims(position, 1) * tf.expand_dims(inv_timescales, 0)
-    signal = tf.concat([tf.sin(scaled_time), tf.cos(scaled_time)], axis=1)
-    signal = tf.pad(signal, [[0, 0], [0, channels % 2]])
-    signal = tf.reshape(signal, [1, length, channels])
-    return signal
-
-
 class PositionalMixin(tf.keras.layers.Layer):
     def positional(self, length):
         pass
@@ -374,20 +336,6 @@ class SinusoidalPositionalMixin(PositionalMixin):
 
     def positional(self, length):
         return self.pe[:, :length]
-
-
-class SinusoidalPositionalMixinT2T(PositionalMixin):
-    def __init__(self, trainable=True, name=None, dtype=tf.float32, **kwargs):
-        super().__init__(trainable=trainable, name=name, dtype=dtype, **kwargs)
-        self.max_timescale = kwargs.get("max_timescale", 1.0e4)
-        # Match the mxlen pytorch has because it precomputes the timing signal
-        self.mxlen = 10000
-        self.min_timescale = kwargs.get("min_timescale", 1.0)
-
-    def positional(self, length):
-        return get_timing_signal_1d(
-            length, self.get_dsz(), min_timescale=self.min_timescale, max_timescale=self.max_timescale, start_index=0
-        )
 
 
 class LearnedPositionalMixin(PositionalMixin):
