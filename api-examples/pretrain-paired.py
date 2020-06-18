91c5aea13effd47a69b48a6fd991c60c26d5966a
Daniel Pressel <dpressel@gmail.com>
2020-06-18 16:36:25 -0400

    updates the pretrain paired to work as expected (#670)

--- api-examples/pretrain-paired.py
+++ api-examples/pretrain-paired.py
@@ -5,75 +5,105 @@ from argparse import ArgumentParser
 import baseline
 from torch.nn.parallel import DistributedDataParallel
 from torch.utils.data import DataLoader
-from eight_mile.utils import str2bool, write_json
-import glob
+from eight_mile.utils import str2bool, write_json, Average, get_num_gpus_multiworker
 from baseline.pytorch.embeddings import *
 import baseline.embeddings
 from eight_mile.optz import *
-from eight_mile.utils import Average, get_num_gpus_multiworker
-from eight_mile.pytorch.optz import *
 from eight_mile.pytorch.layers import save_checkpoint, init_distributed
-from transformer_utils import MultiFileDatasetReader, PairedModel, TripletLoss, AllLoss, TiedEmbeddingsSeq2SeqModel, \
-    create_model, get_lr_decay
+from eight_mile.pytorch.optz import *
+from transformer_utils import (
+    MultiFileDatasetReader,
+    on_demand_mlm_masking,
+    get_lr_decay,
+    PairedModel,
+    TripletLoss,
+    AllLoss,
+    TiedEmbeddingsSeq2SeqModel,
+)
+
 
 logger = logging.getLogger(__file__)
 
 """Pre-train a paired model in PyTorch
 
-This file uses Baseline to train a Transformer-based ConveRT
-model (https://arxiv.org/pdf/1911.03688.pdf) or Seq2Seq with fastBPE with PyTorch on multiple GPUs.
-
+This file uses Baseline to train a Transformer model using fastBPE with query-response input using either
+  - Dual-encoder model (https://arxiv.org/pdf/1911.03688.pdf) with AllLoss or TripletLoss
+  - Seq2Seq model with response generation
+  
 """
+def create_model(embeddings, d_model, d_ff, dropout, num_heads, num_layers, model_type, rpr_k, d_k, reduction_d_k,
+                 stacking_layers, ff_pdrop, logger):
+    if model_type == "encoder-decoder":
+        logger.info("Creating tied encoder decoder model")
+        hps = {"dsz": d_model,
+               "hsz": d_model,
+               "d_ff": d_ff,
+               "dropout": dropout,
+               "num_heads": num_heads,
+               "layers": num_layers,
+               "encoder_type": "transformer",
+               "decoder_type": "transformer",
+               "src_lengths_key": "x_lengths",
+               "d_k": d_k,
+               "rpr_k": rpr_k}
+        model = TiedEmbeddingsSeq2SeqModel(embeddings, **hps)
+    else:
+        model = PairedModel(embeddings, d_model, d_ff, dropout, num_heads, num_layers, rpr_k=rpr_k, d_k=d_k,
+                            reduction_d_k=reduction_d_k, stacking_layers=stacking_layers, ff_pdrop=ff_pdrop)
+
+    logger.info(model)
+    return model
 
 
 def train():
     parser = ArgumentParser()
     parser.add_argument("--basedir", type=str)
-    parser.add_argument("--train_file", type=str, help='Optional file path to use for train file')
-    parser.add_argument("--valid_file", type=str, help='Optional file path to use for valid file')
-    parser.add_argument("--dataset_cache", type=str, default=os.path.expanduser('~/.bl-data'),
-                        help="Path or url of the dataset cache")
+    parser.add_argument("--train_file", type=str, required=True, help='File path to use for train file')
+    parser.add_argument("--valid_file", type=str, required=True, help='File path to use for valid file')
+    parser.add_argument("--dataset_key", default="paired",
+                        help="dataset key for basedir")
+    parser.add_argument("--embed_type", type=str, default='default',
+                        choices=["default", "positional", "learned-positional"],
+                        help="register label of the embeddings")
     parser.add_argument("--d_model", type=int, default=512, help="Model dimension (and embedding dsz)")
     parser.add_argument("--d_ff", type=int, default=2048, help="FFN dimension")
     parser.add_argument("--d_k", type=int, default=None, help="Dimension per head.  Use if num_heads=1 to reduce dims")
+
     parser.add_argument("--num_heads", type=int, default=8, help="Number of heads")
-    parser.add_argument("--reduction_d_k", type=int, default=64, help="Dimensions of Key and Query in the single headed"
-                                                                      "reduction layers")
-    parser.add_argument("--stacking_layers", type=int, nargs='+', default=[1024, 1024, 1024],
-                        help="Hidden sizes of the dense stack (ff2 from the convert paper)")
-    parser.add_argument("--ff_pdrop", type=float, default=0.1, help="Dropout in the dense stack")
+    parser.add_argument("--num_layers", type=int, default=8, help="Number of layers")
     parser.add_argument("--num_train_workers", type=int, default=4, help="Number train workers")
-    parser.add_argument("--num_valid_workers", type=int, default=2, help="Number valid workers")
-    parser.add_argument("--num_layers", type=int, default=6, help="Number of layers")
-    parser.add_argument("--nctx", type=int, default=64, help="Max context length (for both encoder and decoder)")
-    parser.add_argument("--reader_type", type=str, default='ntp', choices=['ntp', 'nsp', 'preprocessed'])
-    parser.add_argument("--embed_type", type=str, default='default',
-                        help="register label of the embeddings, so far support positional or learned-positional")
-    parser.add_argument("--pattern", default='*.txt', help="Glob pattern for data")
+    parser.add_argument("--nctx", type=int, default=256, help="Max input length")
+    parser.add_argument("--pattern", default='*.json', help="Glob pattern for data")
     parser.add_argument("--batch_size", type=int, default=256, help="Batch Size")
-    parser.add_argument("--dataset_key", default="reddit",
-                        help="dataset key for basedir")
-    parser.add_argument("--subword_model_file", type=str, required=True)
-    parser.add_argument("--subword_vocab_file", type=str, required=True)
+    parser.add_argument("--subword_model_file", type=str, help="The BPE model file", required=True)
+    parser.add_argument("--subword_vocab_file", type=str, help="The BPE subword vocab", required=True)
     parser.add_argument("--dropout", type=float, default=0.1, help="Dropout")
     parser.add_argument("--lr_scheduler", type=str, default='cosine', help="The type of learning rate decay scheduler")
     parser.add_argument("--lr_decay_steps", type=int, help="decay steps of lr scheduler")
     parser.add_argument("--lr_decay_rate", type=float, help="decay rate of lr scheduler")
     parser.add_argument("--lr_alpha", type=float, help="parameter alpha for cosine decay scheduler")
-    parser.add_argument("--optim", default="adam", type=str, help="Optimizer to use (defaults to adam)")
-    parser.add_argument("--model_type", default="dual-encoder", choices=["dual-encoder", "encoder-decoder"])
+    parser.add_argument("--optim", default="adamw", type=str, help="Optimizer to use (defaults to adamw)")
     parser.add_argument("--lr", type=float, default=4.0e-4, help="Learning rate")
-    parser.add_argument("--clip", type=float, default=0.25, help="Clipping gradient norm")
-    parser.add_argument("--weight_decay", type=float, default=0.0, help="Weight decay")
-    parser.add_argument("--epochs", type=int, default=20, help="Num training epochs")
+    parser.add_argument("--clip", type=float, default=1.0, help="Clipping gradient norm")
+    parser.add_argument("--weight_decay", type=float, default=1.0e-2, help="Weight decay")
+    parser.add_argument("--epochs", type=int, default=32, help="Num training epochs")
     parser.add_argument("--restart_from", type=str, help="Option allows you to restart from a previous checkpoint")
     parser.add_argument("--restart_tt", type=str, help="Optional param for legacy checkpoints (step|epoch)")
     parser.add_argument("--warmup_steps", type=int, default=10000, help="Num warmup steps")
-    parser.add_argument("--loss", type=str, default='all', choices=['triplet', 'all'])
-    parser.add_argument("--saves_per_epoch", type=int, default=100, help="The number of checkpoints to save per epoch")
-    parser.add_argument('--rpr_k', help='Relative attention positional sizes pass 0 if you dont want relative attention',
-                        type=int, default=[8], nargs='+')
+    parser.add_argument("--saves_per_epoch", type=int, default=10, help="The number of checkpoints to save per epoch")
+    parser.add_argument("--reduction_d_k", type=int, default=64, help="Dimensions of Key and Query in the single headed"
+                                                                      "reduction layers")
+    parser.add_argument("--stacking_layers", type=int, nargs='+', default=[1024, 1024, 1024],
+                        help="Hidden sizes of the dense stack (ff2 from the convert paper)")
+    parser.add_argument("--ff_pdrop", type=float, default=0.1, help="Dropout in the dense stack")
 
+    parser.add_argument("--reader_type", type=str, default='preprocessed', choices=['ntp', 'nsp', 'preprocessed'])
+
+    parser.add_argument("--model_type", default="dual-encoder", choices=["dual-encoder", "encoder-decoder"])
+    parser.add_argument("--loss", type=str, default='all', choices=['triplet', 'all'])
+    parser.add_argument('--rpr_k',
+                        help='Relative attention positional sizes pass 0 if you dont want relative attention',
+                        type=int, default=[8], nargs='+')
     parser.add_argument("--device", type=str,
                         default="cuda" if torch.cuda.is_available() else "cpu",
                         help="Device (cuda or cpu)")
@@ -88,40 +118,28 @@ def train():
 
     args = parser.parse_args()
 
-    if args.train_file and not args.valid_file:
-        logger.error("If you provide a train_file, you must provide a valid_file")
-        return
-
-    if not args.train_file and args.valid_file:
-        logger.error("If you provide a valid_file, you must also provide a train_file")
-        return
-
     if args.basedir is None:
         args.basedir = '{}-{}-paired-{}-bpe-{}'.format(args.model_type, args.reader_type, args.dataset_key, os.getpid())
-    logging.basicConfig(
-        format="%(name)s: %(levelname)s: %(message)s",
-        level=logging.INFO if args.local_rank in [-1, 0] else logging.WARN
-    )
+    logging.basicConfig(level=logging.INFO if args.local_rank in [-1, 0] else logging.WARN)
+
     num_gpus = get_num_gpus_multiworker()
     args.distributed = args.distributed or num_gpus > 1
     logger.info(f"Using {num_gpus} GPUs in this job.")
 
-    if len(args.rpr_k) == 0 or args.rpr_k[0] < 1:
-        rpr_k = None
-    elif len(args.rpr_k) == 1:
-        rpr_k = args.rpr_k[0]
-    else:
-        rpr_k = args.rpr_k
-
     if args.distributed:
-        args.device = init_distributed(args.local_rank)
+        args.device, updated_local_rank = init_distributed(args.local_rank)
+        args.local_rank = updated_local_rank
 
-    reader = MultiFileDatasetReader(args.nctx, args.subword_model_file, args.subword_vocab_file, args.pattern, args.reader_type)
+    reader = MultiFileDatasetReader(args.nctx, args.subword_model_file, args.subword_vocab_file, args.pattern,
+                                    reader_type=args.reader_type)
+
     vocab = reader.build_vocab()
-
     # If we are not using chars, then use 'x' for both input and output
-    preproc_data = baseline.embeddings.load_embeddings('x', dsz=args.d_model, known_vocab=vocab['x'], embed_type=args.embed_type)
+    preproc_data = baseline.embeddings.load_embeddings('x', dsz=args.d_model, known_vocab=vocab['x'],
+                                                       preserve_vocab_indices=True,
+                                                       embed_type=args.embed_type)
     vocabs = preproc_data['vocab']
+
     os.makedirs(args.basedir, exist_ok=True)
     # We want to make sure to save our input vocab into the basedir for reuse later
     write_json(vocabs, os.path.join(args.basedir, 'vocabs.json'))
@@ -129,11 +147,19 @@ def train():
     logger.info("Loaded embeddings")
 
     train_set = reader.load(args.train_file, vocabs)
-    valid_set = reader.load(args.valid_file, vocabs)
+    valid_set = reader.load(args.valid_file, vocabs, distribute=False, shuffle=False)
 
     train_loader = DataLoader(train_set, batch_size=args.batch_size, num_workers=args.num_train_workers)
-    valid_loader = DataLoader(valid_set, batch_size=args.batch_size, num_workers=args.num_valid_workers)
+    valid_loader = DataLoader(valid_set, batch_size=args.batch_size)
     logger.info("Loaded datasets")
+    logger.info("Using embedding type [%s]", args.embed_type)
+
+    if len(args.rpr_k) == 0 or args.rpr_k[0] < 1:
+        rpr_k = None
+    elif len(args.rpr_k) == 1:
+        rpr_k = args.rpr_k[0]
+    else:
+        rpr_k = args.rpr_k
 
     model = create_model(embeddings, d_model=args.d_model, d_ff=args.d_ff, dropout=args.dropout,
                          num_heads=args.num_heads, num_layers=args.num_layers,
@@ -147,9 +173,10 @@ def train():
 
     # according to pytorch, len(train_loader) will return len(train_set) when train_set is IterableDataset, so manually
     # correct it here
-    steps_per_epoch = len(train_loader) // (args.batch_size*num_gpus)
+    steps_per_epoch = len(train_loader) // (args.batch_size * num_gpus)
+    valid_steps = len(valid_loader) // args.batch_size
     update_on = steps_per_epoch // args.saves_per_epoch
-    report_on = update_on // 10
+    report_on = max(10, update_on) // 10
     logger.info(f"Steps per epoch per GPU: {steps_per_epoch}. Saving checkpoint every {update_on} steps.")
     lr_decay = get_lr_decay(args.lr_scheduler, args.lr, steps_per_epoch, args.epochs, logger,
                             decay_steps=args.lr_decay_steps, decay_rate=args.lr_decay_rate, alpha=args.lr_alpha)
@@ -158,6 +185,7 @@ def train():
 
     global_step = 0
     start_epoch = 0
+
     if args.restart_from:
         model.load_state_dict(torch.load(args.restart_from))
         vec = args.restart_from.split("-")
@@ -186,16 +214,17 @@ def train():
         logger.info("Model located on %d", args.local_rank)
 
     model_base = os.path.join(args.basedir, 'checkpoint')
-
-    # This is the training loop
     steps = global_step
+
     for epoch in range(start_epoch, args.epochs):
         avg_loss = Average('average_train_loss')
         metrics = {}
         optimizer.zero_grad()
         start = time.time()
         model.train()
-        for i, batch in enumerate(train_loader):
+        train_itr = iter(train_loader)
+        for i in range(steps_per_epoch):
+            batch = next(train_itr)
             steps += 1
             x, y = batch
             inputs = x.to(args.device)
@@ -222,25 +251,26 @@ def train():
         # This is the token-level training perplexity
         metrics['train_elapsed_min'] = elapsed
         metrics['average_train_loss'] = train_avg_loss
-        avg_valid_loss = Average('average_valid_loss')
-        start = time.time()
-        model.eval()
-        for batch in valid_loader:
-            with torch.no_grad():
-                x, y = batch
-                inputs = x.to(args.device)
-                labels = y.to(args.device)
-                loss = loss_function(inputs, labels)
-                avg_valid_loss.update(loss.item())
-
-        valid_avg_loss = avg_valid_loss.avg
-
-        elapsed = (time.time() - start)/60
-        metrics['valid_elapsed_min'] = elapsed
-
-        metrics['average_valid_loss'] = valid_avg_loss
-        logger.info(metrics)
         if args.local_rank < 1:
+            avg_valid_loss = Average('average_valid_loss')
+            start = time.time()
+            model.eval()
+            valid_itr = iter(valid_loader)
+            for j in range(valid_steps):
+                with torch.no_grad():
+                    x, y = batch
+                    inputs = x.to(args.device)
+                    labels = y.to(args.device)
+                    loss = loss_function(inputs, labels)
+                    avg_valid_loss.update(loss.item())
+
+            valid_avg_loss = avg_valid_loss.avg
+
+            elapsed = (time.time() - start)/60
+            metrics['valid_elapsed_min'] = elapsed
+
+            metrics['average_valid_loss'] = valid_avg_loss
+            logger.info(metrics)
             save_checkpoint(model, model_base, epoch, tick_type='epoch')
 
 
