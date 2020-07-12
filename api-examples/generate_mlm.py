5d0464c39d231213fa1bf61aae5cf92889dc7850
Daniel Pressel <dpressel@gmail.com>
2020-07-12 17:51:45 -0400

    Feature/finetuning and MLM testing utils (#692)
    * make easier to finetune on LM
    
    * name cleanup
    
    * stop using the vocabs.json, use the vectorizer
    
    * fix weird vectorizer stuff

--- api-examples/generate_mlm.py
+++ api-examples/generate_mlm.py
@@ -15,7 +15,7 @@ from transformer_utils import find_latest_checkpoint
 logger = logging.getLogger(__file__)
 
 
-def decode_sentence(model, vectorizer, query, word2index, index2word, device, sample=True):
+def decode_sentence(model, vectorizer, query, word2index, index2word, device, sample=True, y_only=False):
     vec, length = vectorizer.run(query, word2index)
     UNK = word2index.get('<UNK>')
     MASK = word2index.get('[MASK]')
@@ -23,8 +23,8 @@ def decode_sentence(model, vectorizer, query, word2index, index2word, device, sa
         if vec[i] == UNK:
             vec[i] = MASK
 
-    detok = [index2word[v] for v in vec if v != 0]
-    print('[De-tok] ' + ' '.join(detok))
+    bpe = [index2word[v] for v in vec if v != 0]
+    print('[BPE] ' + ' '.join(bpe))
     toks = torch.from_numpy(vec).unsqueeze(0).to(device=device)
 
     length = torch.from_numpy(np.array(length)).unsqueeze(0).to(device=device)
@@ -32,11 +32,10 @@ def decode_sentence(model, vectorizer, query, word2index, index2word, device, sa
     with torch.no_grad():
         predictions, _ = model({'x': toks}, None)
         predictions = predictions.squeeze(0)
-        #predictions = model({'x': toks, 'src_len': length, 'h': None}).squeeze(0)
         words = []
         for i in range(length):
 
-            if vec[i] == MASK:
+            if vec[i] == MASK or y_only:
                 if not sample:
                     output = torch.argmax(predictions[i], -1).item()
                     word = index2word[output]
@@ -76,6 +75,7 @@ def create_model(embeddings, d_model, d_ff, num_heads, num_layers, rpr_k, d_k, c
                                                   src_keys=['x'], tgt_key='x')
     if checkpoint_name.endswith('npz'):
         load_tlm_npz(model, checkpoint_name, lm_head=True)
+        #model.output_layer = WeightTieDense(model.embeddings['x'])
     else:
         tlm_load_state_dict(model, checkpoint_name)
     #model.load_state_dict(torch.load(checkpoint_name))
@@ -89,7 +89,6 @@ def run():
     parser.add_argument("--basedir", type=str)
     parser.add_argument("--checkpoint", type=str, help='Checkpoint name or directory to load')
     parser.add_argument("--sample", type=str2bool, help='Sample from the decoder?  Defaults to `false`', default=0)
-    parser.add_argument("--vocab", type=str, help='Vocab file to load', required=False)
     parser.add_argument("--query", type=str, default='hello , <unk> are you today ?')
     parser.add_argument("--dataset_cache", type=str, default=os.path.expanduser('~/.bl-data'),
                         help="Path or url of the dataset cache")
@@ -104,10 +103,11 @@ def run():
     parser.add_argument("--subword_model_file", type=str, required=True)
     parser.add_argument("--subword_vocab_file", type=str, required=True)
     parser.add_argument("--use_cls", type=str2bool, default=False)
+    parser.add_argument('--end_token', default='<EOU>')
     parser.add_argument("--activation", type=str, default='gelu')
     parser.add_argument('--rpr_k', help='Relative attention positional sizes pass 0 if you dont want relative attention',
                         type=int, default=[8], nargs='+')
-
+    parser.add_argument("--y_only", type=str2bool, default=False)
     parser.add_argument("--device", type=str,
                         default="cuda" if torch.cuda.is_available() else "cpu",
                         help="Device (cuda or cpu)")
@@ -118,30 +118,28 @@ def run():
         args.device = torch.device("cuda", 0)
 
 
-    vocab_file = args.vocab
-
     if os.path.isdir(args.checkpoint):
-        vocab_file = os.path.join(args.checkpoint, 'vocabs.json')
         checkpoint, _ = find_latest_checkpoint(args.checkpoint)
         logger.warning("Found latest checkpoint %s", checkpoint)
     else:
         checkpoint = args.checkpoint
-        vocab_file = os.path.join(os.path.dirname(checkpoint), 'vocabs.json')
 
-    vocab = read_json(vocab_file)
-
+    cls = None if not args.use_cls else '[CLS]'
+    end = args.end_token
+    vectorizer = BPEVectorizer1D(model_file=args.subword_model_file, vocab_file=args.subword_vocab_file, mxlen=args.nctx, emit_begin_tok=cls, emit_end_tok=end)
+    vocab = vectorizer.vocab.copy()
     # If we are not using chars, then use 'x' for both input and output
-    preproc_data = baseline.embeddings.load_embeddings('x', dsz=args.d_model, counts=False, known_vocab=vocab, embed_type=args.embed_type)
+    preproc_data = baseline.embeddings.load_embeddings('x', dsz=args.d_model, counts=False, known_vocab=vocab, embed_type=args.embed_type, preserve_vocab_indices=True)
     embeddings = preproc_data['embeddings']
     vocab = preproc_data['vocab']
     model = create_model(embeddings, d_model=args.d_model, d_ff=args.d_ff, num_heads=args.num_heads, num_layers=args.num_layers,
                          rpr_k=args.rpr_k, d_k=args.d_k, checkpoint_name=checkpoint, activation=args.activation)
     model.to(args.device)
 
-    cls = None if not args.use_cls else '[CLS]'
-    vectorizer = BPEVectorizer1D(model_file=args.subword_model_file, vocab_file=args.subword_vocab_file, mxlen=args.nctx, emit_begin_tok=cls)
     index2word = revlut(vocab)
     print('[Query]', args.query)
-    print('[Response]', ' '.join(decode_sentence(model, vectorizer, args.query.split(), vocab, index2word, args.device, sample=args.sample)))
+    bpe_out = decode_sentence(model, vectorizer, args.query.split(), vocab, index2word, args.device, sample=args.sample, y_only=args.y_only)
+
+    print('[Response]', ' '.join(bpe_out))
 
 run()
