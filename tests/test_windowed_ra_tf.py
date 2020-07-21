a7f73fca2a499e739da84298cce41ed0ea8ddce5
wliu <wliu@interactions.com>
2020-07-21 12:19:14 -0400

    fix test pytorch

--- tests/test_windowed_ra_tf.py
+++ tests/test_windowed_ra_tf.py
@@ -1,7 +1,16 @@
-import math
-import torch
+import pytest
+import os
+import numpy as np
 import tensorflow as tf
+from eight_mile.utils import get_version
 from eight_mile.tf.layers import SeqScaledDotProductRelativeAttention, SeqScaledWindowedRelativeAttention, SET_TRAIN_FLAG, masked_fill
+
+
+@pytest.fixture(scope="module")
+def set_cpu():
+    os.environ["CUDA_VISIBLE_DEVICES"] = ""
+    yield
+    del os.environ["CUDA_VISIBLE_DEVICES"]
 
 
 def make_rpr(rpr_key_emb, rpr_value_emb, rpr_k, seq_len):
@@ -20,42 +29,48 @@ def unfold_rpr(rpr_key_emb, rpr_value_emb, rpr_k):
     return rpr_key_emb(window), rpr_value_emb(window)
 
 
-num_heads = 4
-d_model = 64
-rpr_k = 1
-batchsize = 2
-nctx = 256
-d_k = d_model // num_heads
+def test_windowed_ra():
+    num_heads = 4
+    d_model = 64
+    rpr_k = 1
+    batchsize = 2
+    nctx = 256
+    d_k = d_model // num_heads
 
-old = SeqScaledDotProductRelativeAttention(pdrop=0.)
-new = SeqScaledWindowedRelativeAttention(pdrop=0.)
+    with tf.device("/cpu:0"):
+        old = SeqScaledDotProductRelativeAttention(pdrop=0.)
+        new = SeqScaledWindowedRelativeAttention(pdrop=0.)
 
-rpr_key_emb = tf.keras.layers.Embedding(2 * rpr_k + 1, d_k)
-rpr_value_emb = tf.keras.layers.Embedding(2 * rpr_k + 1, d_k)
+        rpr_key_emb = tf.keras.layers.Embedding(2 * rpr_k + 1, d_k)
+        rpr_value_emb = tf.keras.layers.Embedding(2 * rpr_k + 1, d_k)
 
-Q = tf.random.normal([batchsize, num_heads, nctx, d_k])
-K = tf.random.normal([batchsize, num_heads, nctx, d_k])
-V = tf.random.normal([batchsize, num_heads, nctx, d_k])
-lengths = tf.random.uniform([batchsize, ], 0, nctx, dtype=tf.int32)
-seq_mask = tf.sequence_mask(lengths, maxlen=nctx, dtype=tf.float32)
-in_mask = tf.expand_dims(tf.expand_dims(seq_mask, 1), 1)
-out_mask = tf.expand_dims(tf.expand_dims(seq_mask, 1), -1)
+        Q = tf.random.normal([batchsize, num_heads, nctx, d_k])
+        K = tf.random.normal([batchsize, num_heads, nctx, d_k])
+        V = tf.random.normal([batchsize, num_heads, nctx, d_k])
+        lengths = tf.random.uniform([batchsize, ], 0, nctx, dtype=tf.int32)
+        seq_mask = tf.sequence_mask(lengths, maxlen=nctx, dtype=tf.float32)
+        in_mask = tf.expand_dims(tf.expand_dims(seq_mask, 1), 1)
+        out_mask = tf.expand_dims(tf.expand_dims(seq_mask, 1), -1)
 
-# manually create a ra_mask to prevent attention beyond rpr_k
-ones = tf.ones([nctx, nctx])
-ra_mask = tf.linalg.band_part(ones, rpr_k, rpr_k)
-mask = in_mask * tf.expand_dims(tf.expand_dims(ra_mask, 0), 0)
-rpr_key_old, rpr_value_old = make_rpr(rpr_key_emb, rpr_value_emb, rpr_k, nctx)
-SET_TRAIN_FLAG(False)
-out_old = old((Q, K, V, rpr_key_old, rpr_value_old, mask))
-out_old = masked_fill(out_old, tf.equal(out_mask, 0), 1)
-print(out_old.shape)
+        # manually create a ra_mask to prevent attention beyond rpr_k
+        ones = tf.ones([nctx, nctx])
+        ra_mask = tf.linalg.band_part(ones, rpr_k, rpr_k)
+        mask = in_mask * tf.expand_dims(tf.expand_dims(ra_mask, 0), 0)
+        rpr_key_old, rpr_value_old = make_rpr(rpr_key_emb, rpr_value_emb, rpr_k, nctx)
+        SET_TRAIN_FLAG(False)
+        out_old = old((Q, K, V, rpr_key_old, rpr_value_old, mask))
+        out_old = masked_fill(out_old, tf.equal(out_mask, 0), 1)
+        print(out_old.shape)
 
-# using the windowed relative attention with the original sequence mask
-rpr_key_new, rpr_value_new = unfold_rpr(rpr_key_emb, rpr_value_emb, rpr_k)
-out_new = new((Q, K, V, rpr_key_new, rpr_value_new, in_mask))
-out_new = masked_fill(out_new, tf.equal(out_mask, 0), 1)
-print(out_new.shape)
+        # using the windowed relative attention with the original sequence mask
+        rpr_key_new, rpr_value_new = unfold_rpr(rpr_key_emb, rpr_value_emb, rpr_k)
+        out_new = new((Q, K, V, rpr_key_new, rpr_value_new, in_mask))
+        out_new = masked_fill(out_new, tf.equal(out_mask, 0), 1)
+        print(out_new.shape)
+        if get_version(tf) < 2:
+            with tf.compat.v1.Session() as sess:
+                out_old, out_new = sess.run([out_old, out_new])
+        else:
+            out_old, out_new = out_old.numpy(), out_new.numpy()
 
-print(tf.math.reduce_max(tf.abs(out_old - out_new)))
-print(tf.math.reduce_max((out_old - out_new)/tf.abs(out_old)))
+        assert np.allclose(out_old, out_new, atol=1e-6)
