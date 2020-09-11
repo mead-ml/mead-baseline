d5a5ebbec298a79ebe907d7df9243c0f696d8cd1
dpressel <dpressel@gmail.com>
2020-09-10 21:19:35 -0400

    oops name

--- baseline/pytorch/deps/train.py
+++ baseline/pytorch/deps/train.py
@@ -150,7 +150,7 @@ def fit(model_params, ts, vs, es, **kwargs):
     do_early_stopping = bool(kwargs.get('do_early_stopping', True))
     verbose = kwargs.get('verbose', {'console': kwargs.get('verbose_console', False), 'file': kwargs.get('verbose_file', None)})
     epochs = int(kwargs.get('epochs', 20))
-    model_file = get_model_file('classify', 'pytorch', kwargs.get('basedir'))
+    model_file = get_model_file('deps', 'pytorch', kwargs.get('basedir'))
     output = kwargs.get('output')
     txts = kwargs.get('txts')
 
