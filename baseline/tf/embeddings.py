c899d7c5bd3d23a4847e15eff7e2e24ddd4cbd16
Wenshuo Liu <wliu@interactions.com>
2020-09-25 11:17:50 -0400

    initialize token type embed with given scale (#744)
    * default zero init token type embed
    
    * add option to init tt embedding with different scale
    
    * update tf version

--- baseline/tf/embeddings.py
+++ baseline/tf/embeddings.py
@@ -215,7 +215,9 @@ class TransformerLMEmbeddings(TensorFlowEmbeddings):
         # This is for BERT support when we are using 2 features
         token_type_vsz = kwargs.get('token_type_vsz')
         if token_type_vsz:
-            tt_embedding = LookupTableEmbeddings(vsz=token_type_vsz, dsz=self.d_model, name='tt', padding_idx=None)
+            tt_unif = float(kwargs.get('token_type_embed_unif', 0.001))
+            tt_embedding = LookupTableEmbeddings(vsz=token_type_vsz, dsz=self.d_model, name='tt', padding_idx=None,
+                                                 unif=tt_unif)
             embeddings['tt'] = tt_embedding
         # For bert, make sure this is `sum-layer-norm`
         reduction = kwargs.get('embeddings_reduction', kwargs.get('reduction', 'sum'))
