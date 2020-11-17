6e73eb6e268bdc878926eff29574ad0fc237541c
dpressel <dpressel@gmail.com>
2020-11-17 13:56:18 -0500

    move pytorch lengths in cls to cpu

--- baseline/pytorch/classify/model.py
+++ baseline/pytorch/classify/model.py
@@ -215,7 +215,7 @@ class EmbedPoolStackClassifier(ClassifierModelBase):
         :param inputs: An input dictionary containing the features and the primary key length
         :return: A tensor
         """
-        lengths = inputs.get("lengths")
+        lengths = inputs.get("lengths").cpu()
         embedded = self.embeddings(inputs)
         embedded = (embedded, lengths)
         pooled = self.pool_model(embedded)
