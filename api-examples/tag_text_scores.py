e005bb101981051688f124fdda6fd5de51761c48
dpressel <dpressel@gmail.com>
2021-01-29 10:19:34 -0500

    clean up a bit in scoring print, rebased

--- api-examples/tag_text_scores.py
+++ api-examples/tag_text_scores.py
@@ -6,6 +6,10 @@ import os
 from eight_mile.utils import str2bool, read_conll
 from addons import tagger_score_services as tss
 
+def sorted_label_scores(label_scores, top_n):
+    kv = [(k, v) for k, v in label_scores.items()]
+    kv = sorted(kv, key=lambda t: -t[-1])
+    return kv[:top_n]
 
 parser = argparse.ArgumentParser(description='Tag text with a model')
 parser.add_argument('--model', help='A tagger model with extended features', required=True, type=str)
@@ -14,6 +18,7 @@ parser.add_argument('--conll', help='is file type conll?', type=str2bool, defaul
 parser.add_argument('--features', help='(optional) features in the format feature_name:index (column # in conll) or '
                                        'just feature names (assumed sequential)', default=[], nargs='+')
 parser.add_argument('--backend', help='backend', default='tf')
+parser.add_argument('--top_n', help='Show only top N scores', default=5, type=int)
 parser.add_argument('--device', help='device')
 parser.add_argument('--remote', help='(optional) remote endpoint', type=str) # localhost:8500
 parser.add_argument('--name', help='(optional) signature name', type=str)
@@ -24,7 +29,7 @@ parser.add_argument('--export_mapping', help='mapping between features and the f
                     default=[], nargs='+')
 parser.add_argument('--prefer_eager', help="If running in TensorFlow, should we prefer eager model", type=str2bool)
 parser.add_argument('--batchsz', default=64, help="How many examples to run through the model at once", type=int)
-parser.add_argument('--score_type', default='sentence', choices=['sentence', 'dist', 'posterior'])
+parser.add_argument('--score_type', default='posterior', choices=['sentence', 'dist', 'posterior'])
 parser.add_argument('--labels_only', type=str2bool, default=False)
 args = parser.parse_args()
 
@@ -93,12 +98,12 @@ for texts in batched:
         for sen, score in zip(batch_sen, batch_score):
             score = score[:len(sen)]
             for word_tag, word_score in zip(sen, score):
+                label_scores = {m.label_vocab[i]: w for i, w in enumerate(word_score.detach().cpu().numpy())}
+                label_scores = sorted_label_scores(label_scores, args.top_n)
                 if args.labels_only:
-                    print(f"{word_tag['label']}")
+                    print(f"{word_tag['label']}, {label_scores}")
                 else:
-                    print(f"{word_tag['text']} {word_tag['label']}")
-                print({m.label_vocab[i]: w for i, w in enumerate(word_score.detach().cpu().numpy())})
-
+                    print(f"{word_tag['text']} {word_tag['label']}, {label_scores}")
             print()
     else:
 
@@ -106,10 +111,10 @@ for texts in batched:
         for sen, score in zip(batch_sen, batch_score):
 
             for word_tag, word_score in zip(sen, score):
+                label_scores = {m.label_vocab[i]: w for i, w in enumerate(word_score.detach().cpu().numpy())}
+                label_scores = sorted_label_scores(label_scores, args.top_n)
                 if args.labels_only:
-                    print(f"{word_tag['label']}")
+                    print(f"{word_tag['label']}, {label_scores}")
                 else:
-                    print(f"{word_tag['text']} {word_tag['label']}")
-                print({m.label_vocab[i]: w for i, w in enumerate(word_score.detach().cpu().numpy())})
-
+                    print(f"{word_tag['text']} {word_tag['label']}, {label_scores}")
             print()
