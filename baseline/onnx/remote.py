e93173e75c59a2a15d45b3e5fb499d9122c3c61a
dpressel <dpressel@gmail.com>
2021-05-23 21:08:04 -0400

    rm old pbuf stubs (for ORT serving) use triton
    switch to gRPC proto support only for now

--- baseline/onnx/remote.py
+++ baseline/onnx/remote.py
@@ -1,20 +1,24 @@
 import numpy as np
+import logging
 from urllib.parse import urlparse
 import json
-from baseline.onnx.apis import predict_pb2, onnx_ml_pb2
-from http.client import HTTPConnection
+from baseline.onnx.apis import model_config_pb2, grpc_service_pb2, grpc_service_pb2_grpc
+#from http.client import HTTPConnection
+import grpc
 
 from baseline.remote import (
     RemoteModel,
     register_remote,
 )
+logger = logging.getLogger('baseline')
 
 
 class RemoteONNXPredictModel(RemoteModel):
     def __init__(
             self,
             remote,
-            name, signature,
+            name,
+            signature,
             labels=None,
             beam=None,
             lengths_key=None,
@@ -22,11 +26,11 @@ class RemoteONNXPredictModel(RemoteModel):
             version=1,
             return_labels=None,
     ):
-        """A remote model with REST transport
+        """A remote model with gRPC transport
 
-        :param remote: The remote endpoint
-        :param name:  The name of the model
-        :param signature: The model signature
+        :param remote: The remote endpoint (eg. 0.0.0.0:8000)
+        :param name:  The name of the model (this is the same name as the sub-dir under the triton model dir
+        :param signature: The model signature (this is deprecated)
         :param labels: The labels (defaults to None)
         :param beam: The beam width (defaults to None)
         :param lengths_key: Which key is used for the length of the input vector (defaults to None)
@@ -38,17 +42,22 @@ class RemoteONNXPredictModel(RemoteModel):
         super().__init__(
             remote, name, signature, labels, beam, lengths_key, inputs, version, return_labels
         )
-        url = urlparse(self.remote)
-        if len(url.netloc.split(":")) != 2:
+        if len(self.remote.split(":")) != 2:
             raise ValueError("remote has to have the form <host_name>:<port>")
-        self.hostname, self.port = url.netloc.split(":")
-        #v_str = '/versions/{}:'.format(self.version) if self.version is not None else ''
-        # TODO: this a hack
-        v_str = 'versions/1:'
-        path = url.path if url.path.endswith("/") else "{}/".format(url.path)
-        self.path = f'{path}v1/models/{self.name}/{v_str}predict'
-        self.headers = {'Content-type': 'application/x-protobuf',
-                        'Accept': 'application/x-protobuf'}
+
+        channel = grpc.insecure_channel(self.remote)
+        self.grpc_stub = grpc_service_pb2_grpc.GRPCInferenceServiceStub(channel)
+        self.version = version
+        request = grpc_service_pb2.ModelMetadataRequest(name=self.name, version=self.version)
+
+
+
+        response = self.grpc_stub.ModelMetadata(request)
+        self.inputs = {i.name: i.datatype for i in response.inputs}
+        self.outputs = {o.name: o.datatype for o in response.outputs}
+        if self.version is None:
+            self.version = response.versions[-1]
+            logger.info("Selected version %s of model %s", self.version, self.name)
 
     def predict(self, examples, **kwargs):
         """Run prediction over HTTP/REST.
@@ -58,70 +67,59 @@ class RemoteONNXPredictModel(RemoteModel):
         """
 
         request = self.create_request(examples)
-        conn = HTTPConnection(self.hostname, self.port)
-        conn.request('POST', self.path, request.SerializeToString(), self.headers)
-        response = conn.getresponse()
-        if response.status != 200:
-            raise ValueError(f"remote server returns error: {response.reason}")
-        response = response.read()
-        response_message = predict_pb2.PredictResponse()
-        response_message.ParseFromString(response)
-        #if "error" in outcomes_list:
-        #    raise ValueError("remote server returns error: {0}".format(outcomes_list["error"]))
-        outcomes_list = response_message.outputs
-        outcomes_list = self.deserialize_response(examples, outcomes_list)
-        return outcomes_list
+        response = self.grpc_stub.ModelInfer(request)
+        response = self.deserialize_response(examples, response)
+        return response
 
-@register_remote('http-classify')
+    def create_request(self, inputs):
+
+        request = grpc_service_pb2.ModelInferRequest()
+        request.model_name = self.name
+        request.model_version = str(self.version)
+        request.id = "ID"  # ???
+        require_length = False
+        have_length = False
+        if 'lengths' in self.inputs:
+            require_length = True
+        for k, v in inputs.items():
+            # TODO: fix this hack
+            if k not in self.inputs and not k.endswith('_lengths'):
+                logger.warning("Unexpected input: %s, ignoring", k)
+
+            if k.endswith('_lengths'):
+                if k not in self.inputs:
+                    if not require_length or have_length:
+                        continue
+                    else:
+                        k = 'lengths'
+                        have_length = True
+            input = grpc_service_pb2.ModelInferRequest().InferInputTensor()
+            input.name = k
+            input.shape.extend(v.shape)
+            input.datatype = self.inputs[k]
+            request.inputs.extend([input])
+            request.raw_input_contents.extend([v.tobytes()])
+
+        for k, v in self.outputs.items():
+            output = grpc_service_pb2.ModelInferRequest().InferRequestedOutputTensor()
+            output.name = k
+            #output.datatype = v
+            request.outputs.extend([output])
+
+        return request
+
+@register_remote('grpc-classify')
 class RemoteONNXClassifier(RemoteONNXPredictModel):
     def deserialize_response(self, examples, predict_response):
         """Convert the response into a standard format."""
-        labels = np.frombuffer(predict_response['output'].raw_data, dtype=np.float32)
-        labels = [(np.array([i], np.int32), np.array([l], np.float32)) for i, l in enumerate(labels)]
+        label_values = np.frombuffer(predict_response.raw_output_contents[0], dtype=np.float32)
+        labels = [(np.array([i], np.int32), np.array([l], np.float32)) for i, l in enumerate(label_values)]
         return [labels]
 
-
-    def create_request(self, inputs):
-        request_message = predict_pb2.PredictRequest()
-        for k, v in inputs.items():
-            # TODO: fix this hack
-            k_name = 'lengths' if k.endswith('_lengths') else k
-            if 'lengths' in k:
-                continue
-            input_tensor = onnx_ml_pb2.TensorProto()
-            input_tensor.dims.extend(v.shape)
-            input_tensor.data_type = 7
-            input_tensor.raw_data = v.tobytes()
-            request_message.inputs[k_name].data_type = input_tensor.data_type
-            request_message.inputs[k_name].dims.extend(input_tensor.dims)
-            request_message.inputs[k_name].raw_data = input_tensor.raw_data
-
-        return request_message
-
-@register_remote('http-tagger')
+@register_remote('grpc-tagger')
 class RemoteONNXTagger(RemoteONNXPredictModel):
     def deserialize_response(self, examples, predict_response):
         """Convert the response into a standard format."""
-        labels = np.frombuffer(predict_response['output'].raw_data, dtype=np.int64)
+        label_values = np.frombuffer(predict_response.raw_output_contents[0], dtype=np.int32)
 
-        return [labels]
-
-    def create_request(self, inputs):
-        request_message = predict_pb2.PredictRequest()
-        have_lengths = False
-        for k, v in inputs.items():
-            if k.endswith('_lengths'):
-                if have_lengths:
-                    continue
-                else:
-                    k = 'lengths'
-                    have_lengths = True
-            input_tensor = onnx_ml_pb2.TensorProto()
-            input_tensor.dims.extend(v.shape)
-            input_tensor.data_type = 7
-            input_tensor.raw_data = v.tobytes()
-            request_message.inputs[k].data_type = input_tensor.data_type
-            request_message.inputs[k].dims.extend(input_tensor.dims)
-            request_message.inputs[k].raw_data = input_tensor.raw_data
-
-        return request_message
+        return [label_values]
