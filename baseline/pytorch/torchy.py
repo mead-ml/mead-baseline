946b67aa71dee8faaa16fada96234bc8c9eab0e5
Daniel Pressel <dpressel@gmail.com>
2021-06-17 08:23:12 -0400

    Feature/nbest vecs (#831)
    * adds NBest vectorizers and weighted losses
    
    * add adapter for iterable dataset

--- baseline/pytorch/torchy.py
+++ baseline/pytorch/torchy.py
@@ -7,6 +7,7 @@ import torch
 import torch.autograd
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.utils.data import IterableDataset
 from baseline.utils import lookup_sentence, get_version, Offsets
 from eight_mile.pytorch.layers import *
 
@@ -15,3 +16,19 @@ PYT_MAJOR_VERSION = get_version(torch)
 BaseLayer = nn.Module
 TensorDef = torch.Tensor
 
+
+class IterableDatasetAdapter(IterableDataset):
+    def __init__(self, example_list, shuffle=False):
+        super().__init__()
+        self.examples = example_list
+        self.shuffle = shuffle
+
+    def __len__(self):
+        return len(self.examples)
+
+    def __iter__(self):
+        while True:
+            if self.shuffle:
+                np.random.shuffle(self.examples)
+            for example in self.examples:
+                yield example
