b4876f6373873edbc8ac110f93a010bcf62f9606
dpressel <dpressel@gmail.com>
2021-08-16 15:30:14 -0400

    add senteval script

---
+++ mead/api_examples/run_senteval.py
@@ -0,0 +1,119 @@
+import argparse
+import baseline
+import sys
+from baseline.embeddings import load_embeddings_overlay
+from eight_mile.utils import read_config_stream
+from baseline.pytorch.embeddings import *
+from baseline.embeddings import *
+from baseline.vectorizers import *
+from mead.utils import convert_path, parse_extra_args, configure_logger, parse_and_merge_overrides, read_config_file_or_json, index_by_label
+import senteval
+logging.basicConfig(format='%(asctime)s : %(message)s', level=logging.DEBUG)
+DEFAULT_SETTINGS_LOC = 'config/mead-settings.json'
+DEFAULT_DATASETS_LOC = 'config/datasets.json'
+DEFAULT_EMBEDDINGS_LOC = 'config/embeddings.json'
+DEFAULT_VECTORIZERS_LOC = 'config/vecs.json'
+SUBWORD_EXTRA = 30
+
+def main():
+    parser = argparse.ArgumentParser(description='Run senteval harness')
+    parser.add_argument('--nctx', default=512, type=int)
+    parser.add_argument('--batchsz', default=20, type=int)
+    parser.add_argument('--pool', help='Should a reduction be applied on the embeddings?  Only use if your embeddings arent already pooled', type=str)
+    parser.add_argument('--vec_id', help='Reference to a specific embedding type')
+    parser.add_argument('--embed_id', help='What type of embeddings to use')
+    parser.add_argument("--device", type=str, default="cuda" if torch.cuda.is_available() else "cpu", help="Device (cuda or cpu)")
+    parser.add_argument('--max_len1d', type=int, default=100)
+    parser.add_argument('--embeddings', help='index of embeddings: local file, remote URL or mead-ml/hub ref', type=convert_path)
+    parser.add_argument('--vecs', help='index of vectorizers: local file, remote URL or hub mead-ml/ref', type=convert_path)
+    parser.add_argument('--fast', help="Run fast, but not necessarily as accurate", action='store_true')
+    parser.add_argument('--data', help="Path to senteval data",
+                        default=os.path.expanduser("~/dev/work/SentEval/data"))
+    args = parser.parse_args()
+
+    args.embeddings = convert_path(DEFAULT_EMBEDDINGS_LOC) if args.embeddings is None else args.embeddings
+    args.embeddings = read_config_stream(args.embeddings)
+
+    args.vecs = convert_path(DEFAULT_VECTORIZERS_LOC) if args.vecs is None else args.vecs
+
+    vecs_index = read_config_stream(args.vecs)
+    vecs_set = index_by_label(vecs_index)
+    vec_params = vecs_set[args.vec_id]
+    vec_params['mxlen'] = args.nctx
+
+    if 'transform' in vec_params:
+        vec_params['transform_fn'] = vec_params['transform']
+
+    if 'transform_fn' in vec_params and isinstance(vec_params['transform_fn'], str):
+        vec_params['transform_fn'] = eval(vec_params['transform_fn'])
+
+    vectorizer = create_vectorizer(**vec_params)
+    if not isinstance(vectorizer, HasPredefinedVocab):
+        raise Exception("We currently require a vectorizer with a pre-defined vocab to run this script")
+    embeddings_index = read_config_stream(args.embeddings)
+    embeddings_set = index_by_label(embeddings_index)
+    embeddings_params = embeddings_set[args.embed_id]
+    embeddings = load_embeddings_overlay(embeddings_set, embeddings_params, vectorizer.vocab)
+
+    embedder = embeddings['embeddings']
+    embedder.to(args.device).eval()
+
+    def _mean_pool(inputs, embeddings):
+        mask = (inputs != 0)
+        seq_lengths = mask.sum(1).unsqueeze(-1)
+        return embeddings.sum(1)/seq_lengths
+
+    def _max_pool(inputs, embeddings):
+        mask = (inputs != 0)
+        embeddings = embeddings.masked_fill(mask.unsqueeze(-1) == False, -1e8)
+        return torch.max(embeddings, 1, False)[0]
+
+
+    if args.pool:
+        pool = _max_pool if args.pool == 'max' else _mean_pool
+    else:
+        pool = lambda x, y: y
+
+    params_senteval = {'task_path': args.data, 'usepytorch': True, 'kfold': 10}
+    params_senteval['classifier'] = {'nhid': 0, 'optim': 'adam', 'batch_size': 64,
+                                     'tenacity': 5, 'epoch_size': 4}
+    if args.fast:
+        logging.info("Setting fast params")
+        params_senteval['kfold'] = 5
+        params_senteval['classifier']['epoch_size'] = 2
+        params_senteval['classifier']['tenacity'] = 3
+        params_senteval['classifier']['batch_size'] = 128
+
+    # SentEval prepare and batcher
+    def prepare(params, samples):
+        max_sample = max(len(s) for s in samples)
+        vectorizer.mxlen = min(args.nctx, max_sample + SUBWORD_EXTRA)
+        logging.info('num_samples %d, mxlen set to %d', max_sample, vectorizer.mxlen)
+
+    def batcher(params, batch):
+        batch = [sent if sent != [] else ['.'] for sent in batch]
+        vs = []
+        for sent in batch:
+            v, l = vectorizer.run(sent, vectorizer.vocab)
+            vs.append(v)
+        vs = np.stack(vs)
+        with torch.no_grad():
+            inputs = torch.tensor(vs, device=args.device)
+            encoding = embedder(inputs)
+            encoding = pool(inputs, encoding)
+            encoding = encoding.cpu().numpy()
+        return encoding
+
+    se = senteval.engine.SE(params_senteval, batcher, prepare)
+    transfer_tasks = ['STS12', 'STS13', 'STS14', 'STS15', 'STS16',
+                      'MR', 'CR', 'MPQA', 'SUBJ', 'SST2', 'SST5', 'TREC', 'MRPC',
+                      'SICKEntailment', 'SICKRelatedness', 'STSBenchmark',
+                      'Length', 'WordContent', 'Depth', 'TopConstituents',
+                      'BigramShift', 'Tense', 'SubjNumber', 'ObjNumber',
+                      'OddManOut', 'CoordinationInversion']
+    results = se.eval(transfer_tasks)
+    print(results)
+
+
+if __name__ == '__main__':
+    main()
