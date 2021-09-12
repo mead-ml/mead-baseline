38adbd9c97dcc0e8d5ecf0d0672dc45ed5283e5b
Daniel Pressel <dpressel@gmail.com>
2021-09-12 12:03:55 -0400

    custom tokenizers, better CLI, FAISS (#868)
    * Add option to build a FAISS index for embeddings
    
    Also adds tokenizer options and makes the CLI
    friendlier
    
    * move the faiss portion down and add L2 norm

--- mead/api_examples/text2embeddings.py
+++ mead/api_examples/text2embeddings.py
@@ -6,6 +6,7 @@ from eight_mile.utils import read_config_stream
 from baseline.pytorch.embeddings import *
 from baseline.embeddings import *
 from baseline.vectorizers import *
+from mead.api_examples.preproc_utils import create_tokenizer
 from mead.utils import convert_path, parse_extra_args, configure_logger, parse_and_merge_overrides, read_config_file_or_json, index_by_label
 import pandas as pd
 logging.basicConfig(format='%(asctime)s : %(message)s', level=logging.DEBUG)
@@ -23,16 +24,26 @@ def main():
     parser.add_argument('--vec_id', default='bert-base-uncased', help='Reference to a specific embedding type')
     parser.add_argument('--embed_id', default='bert-base-uncased', help='What type of embeddings to use')
     parser.add_argument('--file', required=True)
-    parser.add_argument('--column', required=True)
+    parser.add_argument('--column', type=str)
     parser.add_argument('--output', default='embeddings.npz')
-    parser.add_argument('--pool', help='Should a reduction be applied on the embeddings?', choices=['max', 'mean'], default='mean')
+    parser.add_argument('--pool', help='Should a reduction be applied on the embeddings?  Only use if your embeddings arent already pooled', type=str)
     parser.add_argument('--embeddings', help='index of embeddings: local file, remote URL or mead-ml/hub ref', type=convert_path)
     parser.add_argument('--vecs', help='index of vectorizers: local file, remote URL or hub mead-ml/ref', type=convert_path)
     parser.add_argument('--cuda', type=baseline.str2bool, default=True)
-    parser.add_argument('--has_header', type=baseline.str2bool, default=True)
+    parser.add_argument('--has_header', action="store_true")
+    parser.add_argument("--tokenizer_type", type=str, help="Optional tokenizer, default is to use string split")
+    parser.add_argument('--faiss_index', help="If provided, we will build a FAISS index and store it here")
+    parser.add_argument('--quoting', default=3, help='0=QUOTE_MINIMAL 1=QUOTE_ALL 2=QUOTE_NONNUMERIC 3=QUOTE_NONE', type=int)
     parser.add_argument('--sep', default='\t')
 
     args = parser.parse_args()
+
+    if not args.has_header:
+        if not args.column:
+            args.column = 0
+        column = int(args.column)
+    else:
+        column = args.column
 
     args.embeddings = convert_path(DEFAULT_EMBEDDINGS_LOC) if args.embeddings is None else args.embeddings
     args.embeddings = read_config_stream(args.embeddings)
@@ -49,7 +60,7 @@ def main():
 
     if 'transform_fn' in vec_params and isinstance(vec_params['transform_fn'], str):
         vec_params['transform_fn'] = eval(vec_params['transform_fn'])
-
+    tokenizer = create_tokenizer(args.tokenizer_type)
     vectorizer = create_vectorizer(**vec_params)
     if not isinstance(vectorizer, HasPredefinedVocab):
         raise Exception("We currently require a vectorizer with a pre-defined vocab to run this script")
@@ -70,13 +81,24 @@ def main():
         seq_lengths = mask.sum(1).unsqueeze(-1)
         return embeddings.sum(1)/seq_lengths
 
+    def _zero_tok_pool(_, embeddings):
+        pooled = embeddings[:, 0]
+        return pooled
+
     def _max_pool(inputs, embeddings):
         mask = (inputs != 0)
         embeddings = embeddings.masked_fill(mask.unsqueeze(-1) == False, -1e8)
         return torch.max(embeddings, 1, False)[0]
 
-
-    pool = _max_pool if args.pool == 'max' else _mean_pool
+    if args.pool:
+        if args.pool == 'max':
+            pool = _max_pool
+        elif args.pool == 'zero' or args.pool == 'cls':
+            pool = _zero_tok_pool
+        else:
+            pool = _mean_pool
+    else:
+        pool = lambda x, y: y
 
     def chunks(lst, n):
         """Yield successive n-sized chunks from lst."""
@@ -84,28 +106,35 @@ def main():
             yield lst[i:i + n]
 
     df = pd.read_csv(args.file, header='infer' if args.has_header else None, sep=args.sep)
-    if not args.has_header:
-        col = df[int(args.column)]
-    else:
-        col = df[args.column]
+    col = df[column]
     batches = []
     as_list = col.tolist()
     num_batches = math.ceil(len(as_list) / args.batchsz)
     pg = baseline.create_progress_bar(num_batches, name='tqdm')
+    embed_dsz = 0
     for i, batch in enumerate(chunks(as_list, args.batchsz)):
         pg.update()
         with torch.no_grad():
             vecs = []
             for line in batch:
-                vec, l = vectorizer.run(line.split(), vocabs['x'])
+                tokenized = tokenizer(line)
+                vec, l = vectorizer.run(tokenized, vocabs['x'])
                 vecs.append(vec)
             vecs = torch.tensor(np.stack(vecs))
             if args.cuda:
                 vecs = vecs.cuda()
             embedding = embedder(vecs)
-            batches += pool(vecs, embedding).cpu().numpy().tolist()
+            pooled_batch = pool(vecs, embedding).cpu().numpy()
+            batches += [x for x in pooled_batch]
 
     np.savez(args.output, embeddings=batches, text=as_list)
+    if args.faiss_index:
+        import faiss
+        index = faiss.IndexFlatIP(batches[0].shape[-1])
+        batches = np.stack(batches)
+        faiss.normalize_L2(batches)
+        index.add(batches)
+        faiss.write_index(index, args.faiss_index)
 
 
 if __name__ == '__main__':
