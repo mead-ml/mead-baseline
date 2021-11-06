311b9742bfed78039f3f16331568250fe897af10
Daniel Pressel <dpressel@gmail.com>
2021-11-06 10:26:36 -0400

    Chore/sep paired training (#886)
    * remove s2s from pretrain_paired, make standalone

--- mead/api_examples/pretrain_transformer_seq2seq_pytorch.py
+++ mead/api_examples/pretrain_transformer_seq2seq_pytorch.py
@@ -10,48 +10,35 @@ from eight_mile.utils import str2bool, write_json, Average, Timer, get_num_gpus_
 from baseline.pytorch.embeddings import *
 import baseline.embeddings
 from eight_mile.optz import *
-from eight_mile.pytorch.serialize import load_seq2seq_enc_from_tlm_npz, load_transformer_seq2seq_npz, load_transformer_de_npz
+from eight_mile.pytorch.serialize import load_seq2seq_enc_from_tlm_npz, load_transformer_seq2seq_npz, \
+    load_transformer_de_npz
 from eight_mile.pytorch.layers import (
     save_checkpoint, init_distributed,
     PairedModel,
     TransformerBoWPairedModel,
 )
+from baseline.pytorch.seq2seq.model import TiedEmbeddingsSeq2SeqModel
 from eight_mile.pytorch.optz import *
 from mead.api_examples.transformer_utils import MultiFileDatasetReader, get_lr_decay
 
-
 logger = logging.getLogger(__file__)
-
 
 """Pre-train a paired model in PyTorch
 
-This file uses Baseline to train a Transformer model using fastBPE with query-response input using either
-  - Dual-encoder model (https://arxiv.org/pdf/1911.03688.pdf) with AllLoss or TripletLoss
-  - Seq2Seq model with response generation
+This file uses Baseline to train a Transformer model using fastBPE with query-response input using Seq2Seq
   
 """
-def create_model(embeddings, d_model, d_ff, dropout, num_heads, num_layers, model_type, rpr_k, d_k, reduction_d_k,
-                 stacking_layers, ff_pdrop, windowed_ra, reduction_type, shared_output, logger, layer_norms_after=False):
 
-    if model_type == 'transformer-bow':
-        model = TransformerBoWPairedModel(embeddings, d_model, d_ff, dropout, num_heads, num_layers, rpr_k=rpr_k, d_k=d_k,
-                                          reduction_d_k=reduction_d_k, stacking_layers=stacking_layers, ffn_pdrop=ff_pdrop, windowed_ra=windowed_ra,
-                                          reduction_type_1=reduction_type, freeze_encoders=True, layer_norms_after=layer_norms_after)
+def run_step(x, y, model, loss_function, distributed):
+    x_lengths = torch.sum(x != Offsets.PAD, 1)
+    y_lengths = torch.sum(y != Offsets.PAD, 1)
+    if distributed:
+        inputs = model.module.make_input({'x': x, 'x_lengths': x_lengths, 'tgt': y, 'tgt_lengths': y_lengths})
+        pred = model(inputs)
     else:
-        model = PairedModel(embeddings, d_model, d_ff, dropout, num_heads, num_layers, rpr_k=rpr_k, d_k=d_k,
-                            reduction_d_k=reduction_d_k, stacking_layers=stacking_layers, ffn_pdrop=ff_pdrop,
-                            windowed_ra=windowed_ra, reduction_type=reduction_type, freeze_encoders=True,
-                            output_shared=shared_output, output_layed=shared_output,
-                            layer_norms_after=layer_norms_after)
-
-    logger.info(model)
-    return model
-
-
-def run_step(x, y, loss_function, device):
-    inputs = x.to(device)
-    labels = y.to(device)
-    loss = loss_function(inputs, labels)
+        inputs = model.make_input({'x': x, 'x_lengths': x_lengths, 'tgt': y, 'tgt_lengths': y_lengths})
+        pred = model(inputs)
+    loss = loss_function(pred, inputs['tgt'])
     return loss
 
 
@@ -87,7 +74,8 @@ def parse_args(argv):
     parser.add_argument("--subword_model_file", type=str, help="The BPE model file")
     parser.add_argument("--subword_vocab_file", type=str, help="The BPE subword vocab", required=True)
     parser.add_argument("--dropout", type=float, default=0.1, help="Dropout")
-    parser.add_argument("--layer_norms_after", type=str2bool, default=False, help="Layer norms after (set True for BERT)")
+    parser.add_argument("--layer_norms_after", type=str2bool, default=False,
+                        help="Layer norms after (set True for BERT)")
     parser.add_argument("--lr_scheduler", type=str, default='cosine', help="The type of learning rate decay scheduler")
     parser.add_argument("--lr_decay_steps", type=int, help="decay steps of lr scheduler")
     parser.add_argument("--lr_decay_rate", type=float, help="decay rate of lr scheduler")
@@ -103,29 +91,14 @@ def parse_args(argv):
     parser.add_argument("--saves_per_epoch", type=int, default=10, help="The number of checkpoints to save per epoch")
     parser.add_argument("--reduction_d_k", type=int, default=64, help="Dimensions of Key and Query in the single headed"
                                                                       "reduction layers")
-    parser.add_argument("--reduction_type", type=str, default="2ha",
-                        help="If using a dual encoder, specifies the reduction type")
-    parser.add_argument("--unfreeze_after_step", default=0, type=int,
-                        help="Unfreeze encoders after step, ignored if we dont have a checkpoint")
-    parser.add_argument("--stacking_layers", type=int, nargs='+', default=[],
-                        help="Hidden sizes of the dense stack (ff2 from the convert paper)")
     parser.add_argument("--layer_drop", type=float, default=0.0, help="LayerDrop to apply")
-    parser.add_argument("--ff_pdrop", type=float, default=0.1, help="Dropout in the dense stack")
     parser.add_argument("--reader_type", type=str, default='preprocessed',
                         choices=['ntp', 'nsp', 'preprocessed', 'tfrecord'])
-    parser.add_argument("--model_type", default="dual-encoder",
-                        choices=["dual-encoder", "transformer-bow"])
     parser.add_argument("--src_begin_tok", type=str, nargs='+', default=[])
     parser.add_argument("--src_end_tok", type=str, nargs='+', default=['<EOS>'])
     parser.add_argument("--tgt_begin_tok", type=str, nargs='+', default=['<GO>'])
     parser.add_argument("--tgt_end_tok", type=str, nargs='+', default=['<EOS>'])
     parser.add_argument('--lower', type=baseline.str2bool, default=False)
-    parser.add_argument("--loss", type=str, default='symmetric',
-                        choices=['triplet', 'all', 'all_mean', 'contrastive', 'symmetric'])
-    parser.add_argument("--learn_temp", type=str2bool, default=True,
-                        help="If 'constrastive' or 'symmetric' loss, should we learn the temperature scaling")
-    parser.add_argument("--init_temp", type=float,
-                        help="Initialize the temperature for 'contrastive' or 'symmetric' loss")
     parser.add_argument('--rpr_k',
                         help='Relative attention positional sizes pass 0 if you dont want relative attention',
                         type=int, default=[8], nargs='+')
@@ -140,26 +113,25 @@ def parse_args(argv):
                         type=int,
                         default=-1,
                         help="Local rank for distributed training (-1 means use the environment variables to find)")
-    parser.add_argument("--shared_output", action='store_true')
-    parser.add_argument("--extra_tokens", help="What extra tokens should we use", nargs="+", default=["[CLS]", "[MASK]"])
+    parser.add_argument("--extra_tokens", help="What extra tokens should we use", nargs="+",
+                        default=["[CLS]", "[MASK]"])
     parser.add_argument("--save_npz", type=str2bool, default=False, help="Whether save npz checkpoint")
     args = parser.parse_args(argv)
     return args
 
 
 def run(basedir=None, train_file=None, valid_file=None, dataset_key='paired', embed_type='default',
-        d_model=512, d_ff=2048, d_k=None, num_heads=8, num_layers=8, windowed_ra=False, num_train_workers=4,
+        d_model=512, d_ff=2048, d_k=None, num_heads=8, num_layers=8, num_train_workers=4,
         nctx=256, tgt_nctx=None, file_type='json', record_keys=['x', 'y'], batch_size=256,
         subword_model_file=None, subword_vocab_file=None, dropout=0.1, lr_scheduler='cosine',
         lr_decay_steps=None, lr_decay_rate=None, lr_alpha=None, optim='adamw', lr=4.0e-4, clip=1.0,
         weight_decay=1.0e-2, epochs=32, restart_from=None, restart_tt=None, warmup_steps=10000, saves_per_epoch=10,
-        reduction_d_k=64, reduction_type='2ha', unfreeze_after_step=0, stacking_layers=[], layer_drop=0.0, ff_pdrop=0.1,
-        reader_type='preprocessed', model_type='dual-encoder', src_begin_tok=[], src_end_tok=['<EOS>'],
-        tgt_begin_tok=['<GO>'], tgt_end_tok=['<EOS>'], lower=False, loss='symmetric', learn_temp=True, init_temp=None,
-        rpr_k=[8], device='cuda', distributed=False, local_rank=-1, save_npz=False, layer_norms_after=False,
-        extra_tokens=["[CLS]", "[MASK]"], subword_type='bpe', shared_output=False, **kwargs):
+        layer_drop=0.0, reader_type='preprocessed', src_begin_tok=[], src_end_tok=['<EOS>'],
+        tgt_begin_tok=['<GO>'], tgt_end_tok=['<EOS>'], lower=False,
+        rpr_k=[8], device='cuda', distributed=False, local_rank=-1, save_npz=False,
+        extra_tokens=["[CLS]", "[MASK]"], subword_type='bpe', label_smoothing=None, **kwargs):
     if basedir is None:
-        basedir = '{}-{}-paired-{}-bpe-{}'.format(model_type, reader_type, dataset_key, os.getpid())
+        basedir = f's2s-{reader_type}-paired-{dataset_key}-bpe-{os.getpid()}'
     logging.basicConfig(level=logging.INFO if local_rank in [-1, 0] else logging.WARN)
     num_gpus = get_num_gpus_multiworker()
     distributed = distributed or num_gpus > 1
@@ -196,18 +168,23 @@ def run(basedir=None, train_file=None, valid_file=None, dataset_key='paired', em
         rpr_k = rpr_k[0]
     else:
         rpr_k = rpr_k
-    model = create_model(embeddings, d_model=d_model, d_ff=d_ff, dropout=dropout,
-                         num_heads=num_heads, num_layers=num_layers,
-                         model_type=model_type, rpr_k=rpr_k, d_k=d_k, reduction_d_k=reduction_d_k,
-                         stacking_layers=stacking_layers, ff_pdrop=ff_pdrop, windowed_ra=windowed_ra,
-                         reduction_type=reduction_type,
-                         shared_output=shared_output,
-                         layer_norms_after=layer_norms_after,
-                         logger=logger)
+
+    hps = {"dsz": d_model,
+           "hsz": d_model,
+           "d_ff": d_ff,
+           "dropout": dropout,
+           "num_heads": num_heads,
+           "layers": num_layers,
+           "encoder_type": "transformer",
+           "decoder_type": "transformer",
+           "src_lengths_key": "x_lengths",
+           "d_k": d_k,
+           "layer_drop": layer_drop,
+           "rpr_k": rpr_k}
+    model = TiedEmbeddingsSeq2SeqModel({'x': embeddings}, None, **hps)
     model.to(device)
 
-    logger.info(f"Creating {loss}, init temperature: {init_temp}, learnable: {learn_temp}")
-    loss_function = model.create_loss(loss_type=loss, init_temp=init_temp, learn_temp=learn_temp)
+    loss_function = model.create_loss(label_smoothing=label_smoothing)
     loss_function.to(device)
     logger.info("Created model and loss")
     steps_per_epoch = len(train_loader) // num_gpus
@@ -223,15 +200,13 @@ def run(basedir=None, train_file=None, valid_file=None, dataset_key='paired', em
     start_epoch = 0
     if restart_from:
 
-        if unfreeze_after_step > 0 and model_type == "dual-encoder":
-            logger.info(f"Encoders will be frozen until step %d", unfreeze_after_step)
         global_step, start_epoch = reload_from_checkpoint(restart_from, restart_tt, model,
                                                           steps_per_epoch)
         logger.info("Restarting from a previous checkpoint %s.\n\tStarting at global_step=%d, epoch=%d",
                     restart_from, global_step, start_epoch + 1)
-    optimizer = OptimizerManager(loss_function, global_step, optim=optim, lr=lr, lr_function=lr_sched,
+    optimizer = OptimizerManager(model, global_step, optim=optim, lr=lr, lr_function=lr_sched,
                                  weight_decay=weight_decay)
-    logger.info("Model has {:,} parameters".format(sum(p.numel() for p in loss_function.parameters() if p.requires_grad)))
+    logger.info("Model has {:,} parameters".format(sum(p.numel() for p in model.parameters() if p.requires_grad)))
     # Prepare model for distributed training if needed
     if distributed:
         model = DistributedDataParallel(model, device_ids=[device], output_device=device)
@@ -248,14 +223,10 @@ def run(basedir=None, train_file=None, valid_file=None, dataset_key='paired', em
         train_itr = iter(train_loader)
         for i in range(steps_per_epoch):
             batch = next(train_itr)
-
-            if steps > unfreeze_after_step and hasattr(model, 'freeze') and model.freeze:
-                logging.info("Unfreezing encoders at step %d", steps)
-                model.freeze = False
             steps += 1
 
             x, y = batch
-            loss = run_step(x, y, loss_function, device)
+            loss = run_step(x, y, model, loss_function, distributed)
             loss.backward()
             avg_loss.update(loss.item())
 
@@ -287,7 +258,7 @@ def run(basedir=None, train_file=None, valid_file=None, dataset_key='paired', em
                 with torch.no_grad():
                     batch = next(valid_itr)
                     x, y = batch
-                    loss = run_step(x, y, model, loss_function, device)
+                    loss = run_step(x, y, model, loss_function, distributed)
                 avg_valid_loss.update(loss.item())
 
             valid_avg_loss = avg_valid_loss.avg
@@ -314,16 +285,14 @@ def reload_from_checkpoint(restart_from, restart_tick_type, model, steps_per_epo
     else:
         tick_type = vec[-2]
     if restart_from.endswith('.npz'):
+        # If its a seq2seq load either from a seq2seq or from a TLM encoder
         try:
-            load_transformer_de_npz(model, restart_from)
-        # If its a dual-encoder, assuming we have model.transformer and model.embeddings, we can load directly
-        # from a Transformer Language Model
+            load_transformer_seq2seq_npz(model, restart_from)
         except:
-            print('Model file not recognized as a dual encoder model, attempting to load as LM for encoder, reset step')
-            load_tlm_npz(model, restart_from)
+            print('Model file not recognized as seq2seq model, attempting to load as LM for encoder, reset step')
+            load_seq2seq_enc_from_tlm_npz(model, restart_from)
             step_num = 0
             tick_type = 'ignore'
-
     else:
         model.load_state_dict(torch.load(restart_from))
     if tick_type == 'epoch':
